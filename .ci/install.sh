17bc12a1ea26f738aca300c7a3233b6f2b267c9d
stnolting <stnolting@gmail.com>
2020-07-23 21:13:24 +0200

    update to use most recent pre-built toolchain

--- .ci/install.sh
+++ .ci/install.sh
@@ -3,12 +3,16 @@
 # Abort if any command returns != 0
 set -e
 
+# Toolchain to be used 
+TOOLCHAIN_V=riscv32-unknown-elf.gcc-10.1.0.rv32gc.ilp32.newlib
+
 # Download toolchain
-wget https://github.com/stnolting/riscv_gcc_prebuilt/raw/master/data/riscv32-unknown-elf.gcc-9.2.0.rv32i.ilp32.tar.gz
+echo "Downloading prebuilt RISC-V GCC toolchain ($TOOLCHAIN_V)..."
+wget https://github.com/stnolting/riscv_gcc_prebuilt/raw/master/data/$TOOLCHAIN_V.tar.gz
 
 # Decompress
 mkdir riscv
-tar -xzf riscv32-unknown-elf.gcc-9.2.0.rv32i.ilp32.tar.gz -C riscv/
+tar -xzf $TOOLCHAIN_V.tar.gz -C riscv/
 pwd
 ls -al
 ls -al riscv/
