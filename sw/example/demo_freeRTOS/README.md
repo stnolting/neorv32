976b5581446e0919626c649d41872c14268bde60
stnolting <stnolting@gmail.com>
2020-08-24 18:53:52 +0200

    added simple FreeRTOS demo

---
+++ sw/example/demo_freeRTOS/README.md
@@ -0,0 +1,45 @@
+# FreeRTOS Demo for the NEORV32 Processor
+
+This is simple example shows the usage of [FreeRTOS](https://www.freertos.org/) on the NEORV32 processor. It uses the default *blink*
+demo application (`blinky_demo/main_blinky.c`). See the comments in that source file for more information.
+
+The chip-specific extensions folder (`chip_specific_extensions/neorv32`) should be in `$(FREERTOS_HOME)/Source/portable/GCC/RISC-V/chip_specific_extensions`,
+but is placed in this source directory for simplicity.
+
+
+## Hardware Requirements
+
+* 8kB DMEM and 16kB IMEM
+* MTIME (machine timer)
+* `Zicsr` CPU extension
+
+
+## Instructions
+
+Download FreeRTOS from the [official GitHub repository](https://github.com/FreeRTOS/FreeRTOS).
+
+    $ git clone https://github.com/FreeRTOS/FreeRTOS.git
+
+Open the makefile from this example folder and configure the `FREERTOS_HOME` variable to point to the `FreeRTOS/FreeRTOS` home folder.
+
+    FREERTOS_HOME ?= /mnt/n/Projects/FreeRTOS/FreeRTOS
+
+Compile the NEORV32 executable. Do not forget the `RUN_FREERTOS_DEMO` switch.
+
+    $ make USER_FLAGS+=-DRUN_FREERTOS_DEMO clean_all exe
+
+Note: The *.c sources and the FreeRTOS-specific part of the makefile have (include) guards that test if `RUN_FREERTOS_DEMO` is defined.
+This has no pratical usage for the user - it is just a work-around for the NEORV32 CI environment.
+
+Upload the executable (`neorv32_exe.bin`) to the processor via the bootloader and execute it.
+
+```
+Awaiting neorv32_exe.bin... OK
+CMD:> e
+Booting...
+
+FreeRTOS V10.3.1
+Blink
+Blink
+Blink
+```
---
+++ sw/example/demo_freeRTOS/main.c
@@ -0,0 +1,196 @@
+/*
+ * FreeRTOS Kernel V10.3.0
+ * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * http://www.FreeRTOS.org
+ * http://aws.amazon.com/freertos
+ *
+ * 1 tab == 4 spaces!
+ */
+
+/*
+ * Modified for the NEORV32 processor by Stephan Nolting.
+ */
+
+#ifdef RUN_FREERTOS_DEMO
+
+
+/* FreeRTOS kernel includes. */
+#include <FreeRTOS.h>
+#include <task.h>
+
+/* NEORV32 includes. */
+#include <neorv32.h>
+
+
+#define BAUD_RATE 19200
+
+
+/******************************************************************************
+ * This project provides two demo applications.  A simple blinky style project,
+ * and a more comprehensive test and demo application.  The
+ * mainCREATE_SIMPLE_BLINKY_DEMO_ONLY setting (defined in this file) is used to
+ * select between the two.  The simply blinky demo is implemented and described
+ * in main_blinky.c.  The more comprehensive test and demo application is
+ * implemented and described in main_full.c.
+ *
+ * This file implements the code that is not demo specific, including the
+ * hardware setup and standard FreeRTOS hook functions.
+ *
+ * ENSURE TO READ THE DOCUMENTATION PAGE FOR THIS PORT AND DEMO APPLICATION ON
+ * THE http://www.FreeRTOS.org WEB SITE FOR FULL INFORMATION ON USING THIS DEMO
+ * APPLICATION, AND ITS ASSOCIATE FreeRTOS ARCHITECTURE PORT!
+ *
+ */
+
+extern void main_blinky( void );
+extern void freertos_risc_v_trap_handler( void );
+
+/* Prototypes for the standard FreeRTOS callback/hook functions implemented
+within this file.  See https://www.freertos.org/a00016.html */
+void vApplicationMallocFailedHook( void );
+void vApplicationIdleHook( void );
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName );
+void vApplicationTickHook( void );
+
+/* Prepare hardware to run the demo. */
+static void prvSetupHardware( void );
+
+/* Send a message to the UART initialised in prvSetupHardware. */
+void vSendString( const char * const pcString );
+
+/*-----------------------------------------------------------*/
+
+int main( void )
+{
+	prvSetupHardware();
+
+  neorv32_uart_printf("FreeRTOS %s\n", tskKERNEL_VERSION_NUMBER);
+
+	main_blinky();
+}
+
+/*-----------------------------------------------------------*/
+
+static void prvSetupHardware( void )
+{
+  // configure trap handler entry point
+  neorv32_cpu_csr_write(CSR_MTVEC, (uint32_t)&freertos_risc_v_trap_handler);
+
+  // configure UART for default baud rate, no rx interrupt, no tx interrupt
+  neorv32_uart_setup(BAUD_RATE, 0, 0);
+}
+
+/*-----------------------------------------------------------*/
+
+void vToggleLED( void )
+{
+	neorv32_gpio_pin_toggle(0);
+}
+
+/*-----------------------------------------------------------*/
+
+void vSendString( const char * const pcString )
+{
+	neorv32_uart_print( (char *)pcString );
+}
+
+/*-----------------------------------------------------------*/
+
+void vApplicationMallocFailedHook( void )
+{
+	/* vApplicationMallocFailedHook() will only be called if
+	configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
+	function that will get called if a call to pvPortMalloc() fails.
+	pvPortMalloc() is called internally by the kernel whenever a task, queue,
+	timer or semaphore is created.  It is also called by various parts of the
+	demo application.  If heap_1.c or heap_2.c are used, then the size of the
+	heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
+	FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
+	to query the size of free heap space that remains (although it does not
+	provide information on how the remaining heap might be fragmented). */
+	taskDISABLE_INTERRUPTS();
+  neorv32_uart_print("FreeRTOS_FAULT: vApplicationMallocFailedHook\n");
+	__asm volatile( "ebreak" );
+	for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationIdleHook( void )
+{
+	/* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
+	to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
+	task.  It is essential that code added to this hook function never attempts
+	to block in any way (for example, call xQueueReceive() with a block time
+	specified, or call vTaskDelay()).  If the application makes use of the
+	vTaskDelete() API function (as this demo application does) then it is also
+	important that vApplicationIdleHook() is permitted to return to its calling
+	function, because it is the responsibility of the idle task to clean up
+	memory allocated by the kernel to any task that has since been deleted. */
+  neorv32_cpu_sleep();
+}
+
+/*-----------------------------------------------------------*/
+
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
+{
+	( void ) pcTaskName;
+	( void ) pxTask;
+
+	/* Run time stack overflow checking is performed if
+	configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+	function is called if a stack overflow is detected. */
+	taskDISABLE_INTERRUPTS();
+  neorv32_uart_print("FreeRTOS_FAULT: vApplicationStackOverflowHook\n");
+	__asm volatile( "ebreak" );
+	for( ;; );
+}
+
+/*-----------------------------------------------------------*/
+
+void vApplicationTickHook( void )
+{
+
+}
+
+/*-----------------------------------------------------------*/
+
+/* This handler is responsible for handling all interrupts. Only the machine timer interrupt is handled by the kernel. */
+void SystemIrqHandler( uint32_t mcause )
+{
+  neorv32_uart_printf("freeRTOS: Unknown interrupt (0x%x)\n", mcause);
+}
+
+
+
+
+
+// ---------- Primitive main in case this demo is not enabled (i.e. RUN_FREERTOS_DEMO is not defined) ----------
+#else
+  #warning FREERTOS DEMO HAS NOT BEEN COMPILED! Use >>make USER_FLAGS+=-DRUN_FREERTOS_DEMO clean_all exe<< to compile it.
+
+#include <neorv32.h>
+int main() {
+
+  neorv32_uart_setup(19200, 0, 0);
+  neorv32_uart_print("ERROR! FreeRTOS has not been compiled. Use >>make USER_FLAGS+=-DRUN_FREERTOS_DEMO clean_all exe<< to compile it.\n");
+  return 0;
+}
+#endif
