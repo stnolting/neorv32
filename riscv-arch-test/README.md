00efb6e6bfceff83280fa9d2b0bf2a04b341db16
stnolting <stnolting@gmail.com>
2021-03-04 11:22:31 +0100

    removed riscv-compliance folder; replaced by up-to-date riscv-arch-test folder
    Adapted to new name of the RISC-V architecture test repository

--- riscv-arch-test/README.md
+++ riscv-arch-test/README.md
@@ -1,9 +1,9 @@
-# NEORV32 RISC-V-Compliance Test Framework
+# NEORV32 RISC-V ARchitecture Test Framework
 
 ## Overview
 
-This sub-project folder tests the [NEORV32 Processor Core](https://github.com/stnolting/neorv32) for **RISC-V compliance** by
-using the [official RISC-V compliance test suite v2+](https://github.com/riscv/riscv-compliance). The core's HDL sources are *simulated* using
+This sub-project folder tests the [NEORV32 Processor Core](https://github.com/stnolting/neorv32)
+using the [official RISC-V architecture test suite v2+](https://github.com/riscv/riscv-arch-test). The core's HDL sources are *simulated* using
 `GHDL` to provide a virtual execution platform for the test framework.
 
 The following tests are supported yet:
@@ -23,28 +23,28 @@ The following tests are supported yet:
 
 To **execute all the supported tests** open a terminal an run (:warning: simulating everything takes quite some time):
 
-    $ sh run_compliance_test.sh
+    $ sh run_riscv_arch_test.sh
 
 
 ## Details
 
-The [`run_compliance_test.sh`](https://github.com/stnolting/neorv32/blob/master/riscv-compliance/run_compliance_test.sh)
+The [`run_riscv_arch_test.sh`](https://github.com/stnolting/neorv32/blob/master/riscv-arch-test/run_riscv_arch_test.sh)
 bash script does the following:
 
 * Copy the `rtl`, `sim` and `sw` folders of the NEORV32 into `work/neorv32/` to keep the project's core files clean
-* Clone (if not already there) the [riscv-compliance repository](https://github.com/riscv/riscv-compliance) into `work/`
-* Install (copy) the custom `neorv32` test target from the `port-neorv32/framework_v2.0/riscv-target` folder to the compliance test suite's target folder
+* Clone (if not already there) the [riscv-arch-test repository](https://github.com/riscv/riscv-arch-test) into `work/`
+* Install (copy) the custom `neorv32` test target from the `port-neorv32/framework_v2.0/riscv-target` folder to the riscv-arch-test suite's target folder
 * Replace the original DMEM VHDL source file of the processor (in `work/neorv32/rtl/core/neorv32_dmem.vhd`) by the simulation-optimized file (`neorv32/sim/rtl_modules/neorv32_dmem.vhd` to allow faster simulation)
 * Make a copy of the original IMEM VHDL source file of the processor (in `work/neorv32/rtl/core/neorv32_imem.vhd`) by the simulation-optimized file (`work/neorv32/rtl/core/neorv32_imem.ORIGINAL`); the original IMEM will be overriden by the device makefiles with a simulation-optimized one (`neorv32/sim/rtl_modules/neorv32_imem.vhd`); the original IMEM is required for certain tests that use self-modifying code
-* Run the actual compliance tests
+* Run the actual riscv-arch-test tests
 
 More datails regarding the actual simulation process can be found in the [target's
-`README`](https://github.com/stnolting/neorv32/blob/master/riscv-compliance/port-neorv32/framework_v2.0/riscv-target/neorv32/README.md).
+`README`](https://github.com/stnolting/neorv32/blob/master/riscv-arch-test/port-neorv32/framework_v2.0/riscv-target/neorv32/README.md).
 For more information regarding the NEORV32 Processor see the :page_facing_up:
 [NEORV32 data sheet](https://raw.githubusercontent.com/stnolting/neorv32/master/docs/NEORV32.pdf).
 
-:information_source: The port files for the *old framework (v1.0)* can be found in
+:information_source: The port files for the *old framework (v1.0)* (riscv-compliance) can be found in
 [`port-neorv32/framework_v1.0`](https://github.com/stnolting/neorv32/tree/master/riscv-compliance/port-neorv32/framework_v1.0/riscv-target).
 
 :information_source: If the simulation of a test does not generate any signature output at all or if the signature is truncated
-try increasing the simulation time by modiying the `SIM_TIME` variable when calling the test makefiles in `run_compliance_test.sh`.
+try increasing the simulation time by modiying the `SIM_TIME` variable when calling the test makefiles in `run_riscv_arch_test.sh`.
--- riscv-arch-test/run_riscv_arch_test.sh
+++ riscv-arch-test/run_riscv_arch_test.sh
@@ -21,11 +21,11 @@ ghdl -v
 
 # Clone RISC-V Compliance Test Suite GitHub repository if it not exists already
 echo "--------------------------------------------------------------------------"
-echo "> Checking 'RISC-V Compliance Test Suite' GitHub repository..."
+echo "> Checking 'riscv-arch-test' GitHub repository..."
 echo "--------------------------------------------------------------------------"
 
-REPOSRC=https://github.com/riscv/riscv-compliance.git
-LOCALREPO=$homedir/riscv-compliance/work/riscv-compliance
+REPOSRC=https://github.com/riscv/riscv-arch-test.git
+LOCALREPO=$homedir/riscv-arch-test/work/riscv-arch-test
 LOCALREPO_VC_DIR=$LOCALREPO/.git
 
 if [ ! -d $LOCALREPO_VC_DIR ]
@@ -34,36 +34,36 @@ then
     git clone $REPOSRC $LOCALREPO
 else
     echo ">>> Repository already exists. Checking for updates..."
-    (cd $homedir/riscv-compliance/work/riscv-compliance ; git status -uno)
+    (cd $homedir/riscv-arch-test/work/riscv-arch-test ; git status -uno)
 fi
 
 # Copy NEORV32 files
 echo "--------------------------------------------------------------------------"
 echo "> Making local copy of NEORV32 'rtl', 'sim' & 'sw' folders..."
 echo "--------------------------------------------------------------------------"
-(cd $homedir/riscv-compliance/work ; rm -rf neorv32 ; mkdir neorv32)
-cp -r $homedir/rtl/ $homedir/riscv-compliance/work/neorv32/.
-cp -r $homedir/sim/ $homedir/riscv-compliance/work/neorv32/.
-cp -r $homedir/sw/ $homedir/riscv-compliance/work/neorv32/.
+(cd $homedir/riscv-arch-test/work ; rm -rf neorv32 ; mkdir neorv32)
+cp -r $homedir/rtl/ $homedir/riscv-arch-test/work/neorv32/.
+cp -r $homedir/sim/ $homedir/riscv-arch-test/work/neorv32/.
+cp -r $homedir/sw/ $homedir/riscv-arch-test/work/neorv32/.
 
 # Copy neorv32 target folder into test suite
 echo "--------------------------------------------------------------------------"
 echo "> Copying neorv32 test-target into compliance framework..."
 echo "--------------------------------------------------------------------------"
-cp -rf $homedir/riscv-compliance/port-neorv32/framework_v2.0/riscv-target/neorv32 $homedir/riscv-compliance/work/riscv-compliance/riscv-target/.
+cp -rf $homedir/riscv-arch-test/port-neorv32/framework_v2.0/riscv-target/neorv32 $homedir/riscv-arch-test/work/riscv-arch-test/riscv-target/.
 
 # Use simulation-optimized DMEM
 echo ""
 echo ">>> Replacing default DMEM *vhd file with simulation-optimized one..."
 echo ""
-rm -rf $homedir/riscv-compliance/work/neorv32/rtl/core/neorv32_dmem.vhd
-cp  $homedir/riscv-compliance/work/neorv32/sim/rtl_modules/neorv32_dmem.vhd $homedir/riscv-compliance/work/neorv32/rtl/core/.
+rm -rf $homedir/riscv-arch-test/work/neorv32/rtl/core/neorv32_dmem.vhd
+cp  $homedir/riscv-arch-test/work/neorv32/sim/rtl_modules/neorv32_dmem.vhd $homedir/riscv-arch-test/work/neorv32/rtl/core/.
 
 # Make a local copy of the original IMEM rtl file
 echo ""
 echo ">>> Making local backup of original IMEM rtl file (work/neorv32/rtl/core/neorv32_imem.ORIGINAL)..."
 echo ""
-cp $homedir/riscv-compliance/work/neorv32/rtl/core/neorv32_imem.vhd $homedir/riscv-compliance/work/neorv32/rtl/core/neorv32_imem.ORIGINAL
+cp $homedir/riscv-arch-test/work/neorv32/rtl/core/neorv32_imem.vhd $homedir/riscv-arch-test/work/neorv32/rtl/core/neorv32_imem.ORIGINAL
 
 # Component installation done
 ls -al
@@ -74,21 +74,21 @@ echo ""
 
 
 # neorv32 home folder
-NEORV32_LOCAL_HOME=$homedir/riscv-compliance/work/neorv32
+NEORV32_LOCAL_HOME=$homedir/riscv-arch-test/work/neorv32
 
 echo "--------------------------------------------------------------------------"
-echo "> Starting RISC-V compliance tests..."
+echo "> Starting RISC-V architecture tests..."
 echo "--------------------------------------------------------------------------"
 
 # Clean everything
-make -C $homedir/riscv-compliance/work/riscv-compliance NEORV32_LOCAL_COPY=$NEORV32_LOCAL_HOME XLEN=32 RISCV_TARGET=neorv32 clean
+make -C $homedir/riscv-arch-test/work/riscv-arch-test NEORV32_LOCAL_COPY=$NEORV32_LOCAL_HOME XLEN=32 RISCV_TARGET=neorv32 clean
 
 # Run tests and check results
-make --silent -C $homedir/riscv-compliance/work/riscv-compliance NEORV32_LOCAL_COPY=$NEORV32_LOCAL_HOME SIM_TIME=600us XLEN=32 RISCV_TARGET=neorv32 RISCV_DEVICE=I build run verify
-make --silent -C $homedir/riscv-compliance/work/riscv-compliance NEORV32_LOCAL_COPY=$NEORV32_LOCAL_HOME SIM_TIME=400us XLEN=32 RISCV_TARGET=neorv32 RISCV_DEVICE=C build run verify
-make --silent -C $homedir/riscv-compliance/work/riscv-compliance NEORV32_LOCAL_COPY=$NEORV32_LOCAL_HOME SIM_TIME=800us XLEN=32 RISCV_TARGET=neorv32 RISCV_DEVICE=M build run verify
-make --silent -C $homedir/riscv-compliance/work/riscv-compliance NEORV32_LOCAL_COPY=$NEORV32_LOCAL_HOME SIM_TIME=200us XLEN=32 RISCV_TARGET=neorv32 RISCV_DEVICE=privilege build run verify
-make --silent -C $homedir/riscv-compliance/work/riscv-compliance NEORV32_LOCAL_COPY=$NEORV32_LOCAL_HOME SIM_TIME=200us XLEN=32 RISCV_TARGET=neorv32 RISCV_DEVICE=Zifencei RISCV_TARGET_FLAGS=-DNEORV32_NO_DATA_INIT build run verify
+make --silent -C $homedir/riscv-arch-test/work/riscv-arch-test NEORV32_LOCAL_COPY=$NEORV32_LOCAL_HOME SIM_TIME=600us XLEN=32 RISCV_TARGET=neorv32 RISCV_DEVICE=I build run verify
+make --silent -C $homedir/riscv-arch-test/work/riscv-arch-test NEORV32_LOCAL_COPY=$NEORV32_LOCAL_HOME SIM_TIME=400us XLEN=32 RISCV_TARGET=neorv32 RISCV_DEVICE=C build run verify
+make --silent -C $homedir/riscv-arch-test/work/riscv-arch-test NEORV32_LOCAL_COPY=$NEORV32_LOCAL_HOME SIM_TIME=800us XLEN=32 RISCV_TARGET=neorv32 RISCV_DEVICE=M build run verify
+make --silent -C $homedir/riscv-arch-test/work/riscv-arch-test NEORV32_LOCAL_COPY=$NEORV32_LOCAL_HOME SIM_TIME=200us XLEN=32 RISCV_TARGET=neorv32 RISCV_DEVICE=privilege build run verify
+make --silent -C $homedir/riscv-arch-test/work/riscv-arch-test NEORV32_LOCAL_COPY=$NEORV32_LOCAL_HOME SIM_TIME=200us XLEN=32 RISCV_TARGET=neorv32 RISCV_DEVICE=Zifencei RISCV_TARGET_FLAGS=-DNEORV32_NO_DATA_INIT build run verify
 
 echo ""
 echo "Compliance tests completed"
