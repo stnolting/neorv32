c06dd34a98747ef6a06688d7a7e945a7069e8f32
stnolting <stnolting@gmail.com>
2021-04-25 18:27:16 +0200

    [.ci/hw_check.sh] removed simulation time override

--- .ci/hw_check.sh
+++ .ci/hw_check.sh
@@ -8,7 +8,7 @@ homedir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 homedir=$homedir/..
 
 # Run simulation
-sh $homedir/sim/ghdl/ghdl_sim.sh --stop-time=7ms
+sh $homedir/sim/ghdl/ghdl_sim.sh
 
 # Check if reference can be found in output (UART0 primary UART simulation output)
 grep -qf $homedir/check_reference.out neorv32.uart0.sim_mode.text.out && echo "Hardware test completed successfully!"
