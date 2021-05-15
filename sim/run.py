4d1496f68ead166b4ef96b6a95eb5e4149b0f8e6
Lars Asplund <lars.anders.asplund@gmail.com>
2021-05-15 12:28:10 +0200

    Create VUnit run script and make testbench recognized by VUnit

---
+++ sim/run.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from vunit import VUnit
+
+root = Path(__file__).parent
+prj = VUnit.from_argv()
+
+neorv32 = prj.add_library("neorv32")
+neorv32.add_source_files(root / "*.vhd")
+neorv32.add_source_files(root / "../rtl/**/*.vhd")
+
+prj.set_sim_option("disable_ieee_warnings", True)
+
+prj.main()
