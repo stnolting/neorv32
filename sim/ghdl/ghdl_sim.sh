b46958ad612208aea4d83d5e21de767c1f8ba335
umarcor <unai.martinezcorral@ehu.eus>
2021-06-03 04:44:06 +0200

    [sim/ghdl] use import and make, instead of analyze and elaborate

--- sim/ghdl/ghdl_sim.sh
+++ sim/ghdl/ghdl_sim.sh
@@ -36,47 +36,9 @@ echo "Tip: Compile application with USER_FLAGS+=-DUART[0/1]_SIM_MODE to auto-ena
 echo ""
 
 # Analyse sources; libs and images at first!
-ghdl -a --work=neorv32 rtl/core/neorv32_package.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_application_image.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_bootloader_image.vhd
-#
-ghdl -a --work=neorv32 rtl/core/neorv32_boot_rom.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_busswitch.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_bus_keeper.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_icache.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_cfs.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_cpu.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_cpu_alu.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_cpu_bus.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_cpu_control.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_cpu_cp_bitmanip.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_cpu_cp_fpu.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_cpu_cp_muldiv.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_cpu_decompressor.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_cpu_regfile.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_debug_dm.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_debug_dtm.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_dmem.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_gpio.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_imem.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_mtime.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_nco.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_neoled.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_pwm.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_spi.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_sysinfo.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_top.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_trng.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_twi.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_uart.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_wdt.vhd
-ghdl -a --work=neorv32 rtl/core/neorv32_wishbone.vhd
-#
-ghdl -a --work=neorv32 rtl/top_templates/neorv32_test_setup.vhd
-ghdl -a --work=neorv32 rtl/top_templates/neorv32_top_axi4lite.vhd
-ghdl -a --work=neorv32 rtl/top_templates/neorv32_top_stdlogic.vhd
-#
-ghdl -a --work=neorv32 sim/neorv32_tb.vhd
+ghdl -i --work=neorv32 rtl/core/*.vhd
+ghdl -i --work=neorv32 rtl/top_templates/*.vhd
+ghdl -i --work=neorv32 sim/*.vhd
 
 # Prepare simulation output files for UART0
 # Testbench receiver log file
@@ -99,7 +61,7 @@ touch neorv32.uart1.sim_mode.data.out
 chmod 777 neorv32.uart1.sim_mode.data.out
 
 # Run simulation
-ghdl -e --work=neorv32 neorv32_tb
+ghdl -m --work=neorv32 neorv32_tb
 ghdl -r --work=neorv32 neorv32_tb --max-stack-alloc=0 --ieee-asserts=disable --assert-level=error $SIM_CONFIG
 
 cat neorv32.uart0.sim_mode.text.out | grep "CPU TEST COMPLETED SUCCESSFULLY!"
