6bca3b75d9be8d1f254fbdd2b7f67ad555c42cf1
umarcor <unai.martinezcorral@ehu.eus>
2021-06-05 00:52:05 +0200

    [boards/osflow] split 'synthesis.mk' from UPduino_v3 Makefile

---
+++ boards/osflow/synthesis.mk
@@ -0,0 +1,17 @@
+ice40up-obj08.cf: ${ICE40_SRC}
+	ghdl -a $(GHDL_FLAGS) --work=iCE40UP ${ICE40_SRC}
+
+neorv32-obj08.cf: ice40up-obj08.cf ${NEORV32_PKG} ${NEORV32_SRC}
+	ghdl -a $(GHDL_FLAGS) --work=neorv32 ${NEORV32_PKG} ${NEORV32_SRC}
+
+work-obj08.cf: neorv32-obj08.cf ${BOARD_SRC}
+	ghdl -a $(GHDL_FLAGS) --work=work ${BOARD_SRC}
+
+${IMPL}.json: work-obj08.cf
+	$(YOSYS) $(YOSYSFLAGS) \
+	  -p \
+	  "$(GHDLSYNTH) $(GHDL_FLAGS) --no-formal $(TOP); \
+	  synth_ice40 \
+	  -top $(TOP) \
+	  -dsp \
+	  -json $@" 2>&1 | tee yosys-report.txt
