9989cd765470b3fc60b403fa3ba807ce3229f4e4
stnolting <stnolting@gmail.com>
2021-06-05 10:55:29 +0200

    removed debug mode `stepie` flag
    used to allow interrupts during single-stepping. stepie support in GCC/gdb is very poor + the debugger can emulate interrupt behaviour

--- rtl/core/neorv32_package.vhd
+++ rtl/core/neorv32_package.vhd
@@ -87,7 +87,7 @@ package neorv32_package is
   -- Architecture Constants (do not modify!) ------------------------------------------------
   -- -------------------------------------------------------------------------------------------
   constant data_width_c   : natural := 32; -- native data path width - do not change!
-  constant hw_version_c   : std_ulogic_vector(31 downto 0) := x"01050604"; -- no touchy!
+  constant hw_version_c   : std_ulogic_vector(31 downto 0) := x"01050605"; -- no touchy!
   constant archid_c       : natural := 19; -- official NEORV32 architecture ID - hands off!
   constant rf_r0_is_reg_c : boolean := true; -- x0 is a *physical register* that has to be initialized to zero by the CPU
   constant def_rst_val_c  : std_ulogic := cond_sel_stdulogic_f(dedicated_reset_c, '0', '-');
