ebd298da11c71eb4f4f071213ca0f7e862efd810
stnolting <stnolting@gmail.com>
2021-06-11 19:38:17 +0200

    [sw/example/blink_led] simplification
    executable is now less than 4kB

--- sw/example/blink_led/main.c
+++ sw/example/blink_led/main.c
@@ -79,15 +79,12 @@ int main() {
   // check if GPIO unit is implemented at all
   if (neorv32_gpio_available() == 0) {
     neorv32_uart_print("Error! No GPIO unit synthesized!\n");
-    return 0; // nope, no GPIO unit synthesized
+    return 1; // nope, no GPIO unit synthesized
   }
 
   // capture all exceptions and give debug info via UART
   // this is not required, but keeps us safe
   neorv32_rte_setup();
-
-  // check available hardware extensions and compare with compiler flags
-  neorv32_rte_check_isa(0); // silent = 0 -> show message if isa mismatch
 
   // say hello
   neorv32_uart_print("Blinking LED demo program\n");
