560d3a00ffc2b0954b2e6ca689a072bf68c3baa6
umarcor <unai.martinezcorral@ehu.eus>
2021-06-12 20:13:44 +0200

    [ci] move Windows/MSYS2 jobs to a new workflow named 'Windows'

---
+++ .github/generate-job-matrix.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+print('::set-output name=matrix::' + str([
+{
+  'board': 'UPduino_v3',
+  'design': 'MinimalBoot',
+  'bitstream': 'neorv32_UPduino_v3_MinimalBoot.bin'
+}, {
+  'board': 'UPduino_v3',
+  'design': 'UP5KDemo',
+  'bitstream': 'neorv32_UPduino_v3_UP5KDemo.bin'
+}, {
+  'board': 'Fomu',
+  'design': 'Minimal',
+  'bitstream': 'neorv32_Fomu_pvt_Minimal.bit'
+}, {
+  'board': 'Fomu',
+  'design': 'MinimalBoot',
+  'bitstream': 'neorv32_Fomu_pvt_MinimalBoot.bit'
+}, {
+  'board': 'Fomu',
+  'design': 'UP5KDemo',
+  'bitstream': 'neorv32_Fomu_pvt_UP5KDemo.bit'
+}]))
