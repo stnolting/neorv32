5a5f4e1ba253f9287ccd62e8f8dbcbe09d4c83a1
stnolting <stnolting@gmail.com>
2021-06-14 15:59:29 +0200

    [core/rtl] minor clean-up

--- rtl/core/neorv32_imem.vhd
+++ rtl/core/neorv32_imem.vhd
@@ -109,7 +109,6 @@ begin
   assert not (IMEM_AS_IROM = true)  report "NEORV32 PROCESSOR CONFIG NOTE: Implementing processor-internal IMEM as ROM (" & natural'image(IMEM_SIZE) & " bytes), pre-initialized with application." severity note;
   assert not (IMEM_AS_IROM = false) report "NEORV32 PROCESSOR CONFIG NOTE: Implementing processor-internal IMEM as blank RAM (" & natural'image(IMEM_SIZE) & " bytes)." severity note;
   --
-  assert false report "imem app size: " & natural'image(imem_app_size_c) & " bytes." severity note;
   assert not ((IMEM_AS_IROM = true) and (imem_app_size_c > IMEM_SIZE)) report "NEORV32 PROCESSOR CONFIG ERROR: Application (image, " & natural'image(imem_app_size_c) & " bytes) does not fit into processor-internal IMEM (ROM, " & natural'image(IMEM_SIZE) & " bytes)!" severity error;
 
 
