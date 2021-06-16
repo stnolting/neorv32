c8d614a773c1c70255e6dc8ce8a61b9a1f971cec
umarcor <unai.martinezcorral@ehu.eus>
2021-06-16 02:36:00 +0200

    [setups/examples] add iCESugar Minimal

--- setups/osflow/devices/ice40/sb_ice40_components.vhd
+++ setups/osflow/devices/ice40/sb_ice40_components.vhd
@@ -36,8 +36,8 @@ package components is
     DIVF                           : std_logic_vector(6 downto 0) := "0000000";
     DIVQ                           : std_logic_vector(2 downto 0) := "000";
     FILTER_RANGE                   : std_logic_vector(2 downto 0) := "000";
-    ENABLE_ICEGATE                 : std_logic := '0';
-    TEST_MODE                      : std_logic := '0';
+    ENABLE_ICEGATE                 : bit := '0';
+    TEST_MODE                      : bit := '0';
     EXTERNAL_DIVIDE_FACTOR         : integer := 1
   );
   port (
@@ -56,7 +56,40 @@ package components is
   );
   end component;
 
-  component  SB_RGBA_DRV
+  component SB_PLL40_PAD
+  generic (
+    FEEDBACK_PATH                   : string := "SIMPLE";
+    DELAY_ADJUSTMENT_MODE_FEEDBACK  : string := "FIXED";
+    DELAY_ADJUSTMENT_MODE_RELATIVE  : string := "FIXED";
+    SHIFTREG_DIV_MODE               : bit_vector(1 downto 0) := "00";
+    FDA_FEEDBACK                    : bit_vector(3 downto 0) := "0000";
+    FDA_RELATIVE                    : bit_vector(3 downto 0) := "0000";
+    PLLOUT_SELECT                   : string := "GENCLK";
+    DIVR                            : bit_vector(3 downto 0) := x"0";
+    DIVF                            : bit_vector(6 downto 0) := "0000000";
+    DIVQ                            : bit_vector(2 downto 0) := "000";
+    FILTER_RANGE                    : bit_vector(2 downto 0) := "000";
+    ENABLE_ICEGATE                  : bit := '0';
+    TEST_MODE                       : bit := '0';
+    EXTERNAL_DIVIDE_FACTOR          : integer := 1
+  );
+  port (
+    PACKAGEPIN      : in  std_logic;
+    PLLOUTCORE      : out std_logic;
+    PLLOUTGLOBAL    : out std_logic;
+    EXTFEEDBACK     : in  std_logic;
+    DYNAMICDELAY    : in  std_logic_vector(7 downto 0);
+    LOCK            : out std_logic;
+    BYPASS          : in  std_logic;
+    RESETB          : in  std_logic;
+    LATCHINPUTVALUE : in  std_logic;
+    SDO             : out std_logic;
+    SDI             : in  std_logic;
+    SCLK            : in  std_logic
+  );
+  end component;
+
+  component SB_RGBA_DRV
   generic (
     CURRENT_MODE : string := "0b0";
     RGB0_CURRENT : string := "0b000000";
