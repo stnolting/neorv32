a356c24124dcf094ecf6a870fd39f64b42f69dc6
umarcor <unai.martinezcorral@ehu.eus>
2021-07-04 13:34:37 +0200

    [setups/osflow] prepare for supporting ECP5

--- setups/osflow/filesets.mk
+++ setups/osflow/filesets.mk
@@ -48,4 +48,6 @@ NEORV32_SRC := ${NEORV32_PKG} ${NEORV32_APP_SRC} ${NEORV32_MEM_SRC} ${NEORV32_CO
 ICE40_SRC := \
   ../devices/ice40/sb_ice40_components.vhd
 
+DEVICE_SRC := ${ICE40_SRC}
+
 # Optionally NEORV32_VERILOG_SRC can be set to a list of Verilog sources
