eb3ffbabb319c845c70f3c0fdb7f58b610550f06
umarcor <unai.martinezcorral@ehu.eus>
2021-07-08 04:29:01 +0200

    [doit] add task BuildAndInstallSoftwareFrameworkTests

--- dodo.py
+++ dodo.py
@@ -138,3 +138,21 @@ def task_BuildAndInstallCheckSoftware():
         ],
         "doc": "Build and install Processor Check software",
     }
+
+
+def task_BuildAndInstallSoftwareFrameworkTests():
+    return {
+        "actions": [
+            # Check toolchain
+            "make -C sw/example/processor_check check",
+            # Generate executables for all example projects
+            "make -C sw/example clean_all exe",
+            # Compile and install bootloader
+            "make -C sw/bootloader clean_all info bootloader",
+            # Compile and install test application
+            # Redirect UART0 TX to text.io simulation output via <UART0_SIM_MODE> user flag
+            "echo 'Compiling and installing CPU (/Processor) test application'",
+            "make -C sw/example/processor_check clean_all USER_FLAGS+=-DRUN_CHECK USER_FLAGS+=-DUART0_SIM_MODE USER_FLAGS+=-DUART1_SIM_MODE MARCH=-march=rv32imac info all",
+        ],
+        "doc": "Build all sw/example/*; install bootloader and processor check",
+    }
