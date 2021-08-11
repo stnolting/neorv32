8903de5d830c74adff8122614d4f85d67377053e
stnolting <stnolting@gmail.com>
2021-08-11 17:18:42 +0200

    8kB IMEM; 4kB DMEM

--- setups/examples/neorv32_AlhambraII_BoardTop_MinimalBoot.vhd
+++ setups/examples/neorv32_AlhambraII_BoardTop_MinimalBoot.vhd
@@ -113,7 +113,9 @@ begin
   -- -------------------------------------------------------------------------------------------
   neorv32_inst: entity work.neorv32_ProcessorTop_MinimalBoot
   generic map (
-    CLOCK_FREQUENCY => f_clock_c  -- clock frequency of clk_i in Hz
+    CLOCK_FREQUENCY   => f_clock_c, -- clock frequency of clk_i in Hz
+    MEM_INT_IMEM_SIZE => 8*1024,    -- size of processor-internal instruction memory in bytes
+    MEM_INT_DMEM_SIZE => 4*1024     -- size of processor-internal data memory in bytes
   )
   port map (
     -- Global control --
