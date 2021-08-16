a972b2cf15d385a8ebe33cc8470cf743120e915b
stnolting <stnolting@gmail.com>
2021-08-16 20:31:00 +0200

    [sim] adapted new rtl folders

--- sim/ghdl.setup.sh
+++ sim/ghdl.setup.sh
@@ -8,8 +8,8 @@ mkdir -p build
 
 ghdl -i --work=neorv32 --workdir=build \
   ../rtl/core/*.vhd \
-  ../rtl/templates/processor/*.vhd \
-  ../rtl/templates/system/*.vhd \
+  ../rtl/processor_templates/*.vhd \
+  ../rtl/system_integration/*.vhd \
   ../rtl/test_setups/*.vhd \
   neorv32_tb.simple.vhd \
   uart_rx.simple.vhd
