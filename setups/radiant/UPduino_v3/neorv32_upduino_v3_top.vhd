77d38c3552f144b7e5ce9e058d1bf6d6e4d2935d
stnolting <stnolting@gmail.com>
2021-10-30 13:12:13 +0200

    [setups] minor update

--- setups/radiant/UPduino_v3/neorv32_upduino_v3_top.vhd
+++ setups/radiant/UPduino_v3/neorv32_upduino_v3_top.vhd
@@ -160,6 +160,7 @@ begin
     CPU_EXTENSION_RISCV_U        => false,       -- implement user mode extension?
     CPU_EXTENSION_RISCV_Zfinx    => false,       -- implement 32-bit floating-point extension (using INT regs!)
     CPU_EXTENSION_RISCV_Zicsr    => true,        -- implement CSR system?
+    CPU_EXTENSION_RISCV_Zicntr   => true,        -- implement base counters?
     CPU_EXTENSION_RISCV_Zifencei => false,       -- implement instruction stream sync.?
 
     -- Extension Options --
