bc99338281d7569e6837d70035fb8b4af29e9ee1
zipotron <zipotron@gmail.com>
2021-11-23 21:25:57 +0200

    Compilation fix regarding mixed language

--- setups/osflow/synthesis.mk
+++ setups/osflow/synthesis.mk
@@ -7,10 +7,10 @@ neorv32-obj08.cf: ${DEVICE_LIB}-obj08.cf ${NEORV32_SRC}
 work-obj08.cf: neorv32-obj08.cf ${DESIGN_SRC} ${BOARD_SRC}
 	ghdl -a $(GHDL_FLAGS) --work=work ${DESIGN_SRC} ${BOARD_SRC}
 
-${IMPL}.json: work-obj08.cf $(NEORV32_VERILOG_SRC)
+${IMPL}.json: work-obj08.cf $(NEORV32_VERILOG_ALL)
 	$(YOSYS) $(YOSYSFLAGS) \
 	  -p \
 	  "$(GHDLSYNTH) $(GHDL_FLAGS) --no-formal $(TOP); \
 	  synth_${YOSYSSYNTH} \
 	  -top $(TOP) $(YOSYSPIPE) \
-	  -json $@" $(NEORV32_VERILOG_SRC) 2>&1 | tee yosys-report.txt
+	  -json $@" $(NEORV32_VERILOG_ALL) 2>&1 | tee yosys-report.txt
