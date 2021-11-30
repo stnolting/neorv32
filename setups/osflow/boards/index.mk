cba04c8def4c81a5360d2a099ee54a3aaf0478fc
stnolting <stnolting@gmail.com>
2021-11-30 06:47:31 +0100

    ignore comb. loops on FOMU

--- setups/osflow/boards/index.mk
+++ setups/osflow/boards/index.mk
@@ -13,23 +13,23 @@ FOMU_REV ?= pvt
 
 ifeq ($(FOMU_REV),evt1)
 YOSYSFLAGS  ?= -D EVT=1 -D EVT1=1 -D HAVE_PMOD=1
-PNRFLAGS    ?= --up5k --package sg48
+PNRFLAGS    ?= --up5k --package sg48 --ignore-loops
 CONSTRAINTS ?= $(PCF_PATH)/$(BOARD)-evt2.pcf
 else ifeq ($(FOMU_REV),evt2)
 YOSYSFLAGS  ?= -D EVT=1 -D EVT2=1 -D HAVE_PMOD=1
-PNRFLAGS    ?= --up5k --package sg48
+PNRFLAGS    ?= --up5k --package sg48 --ignore-loops
 CONSTRAINTS ?= $(PCF_PATH)/$(BOARD)-$(FOMU_REV).pcf
 else ifeq ($(FOMU_REV),evt3)
 YOSYSFLAGS  ?= -D EVT=1 -D EVT3=1 -D HAVE_PMOD=1
-PNRFLAGS    ?= --up5k --package sg48
+PNRFLAGS    ?= --up5k --package sg48 --ignore-loops
 CONSTRAINTS ?= $(PCF_PATH)/$(BOARD)-$(FOMU_REV).pcf
 else ifeq ($(FOMU_REV),hacker)
 YOSYSFLAGS  ?= -D HACKER=1
-PNRFLAGS    ?= --up5k --package uwg30
+PNRFLAGS    ?= --up5k --package uwg30 --ignore-loops
 CONSTRAINTS ?= $(PCF_PATH)/$(BOARD)-$(FOMU_REV).pcf
 else ifeq ($(FOMU_REV),pvt)
 YOSYSFLAGS  ?= -D PVT=1
-PNRFLAGS    ?= --up5k --package uwg30
+PNRFLAGS    ?= --up5k --package uwg30 --ignore-loops
 CONSTRAINTS ?= $(PCF_PATH)/$(BOARD)-$(FOMU_REV).pcf
 else
 $(error Unrecognized FOMU_REV value. must be "evt1", "evt2", "evt3", "pvt", or "hacker")
