f6a72d5b6c5c6e1808d20282467ce351b707c86b
stnolting <stnolting@gmail.com>
2022-01-22 07:29:19 +0100

    [sw/example] minor helper script edits

--- sw/example/dhrystone/dhrystone.sh
+++ sw/example/dhrystone/dhrystone.sh
@@ -1,1 +1,2 @@
-make USER_FLAGS+=-DRUN_DHRYSTONE USER_FLAGS+=-DDHRY_ITERS=2000000 USER_FLAGS+=-DNOENUM MARCH=rv32imc EFFORT=-O3 clean_all exe
+echo "Generating dhrystone executable..."
+make USER_FLAGS+="-DRUN_DHRYSTONE -DDHRY_ITERS=2000000 -DNOENUM" MARCH=rv32imc EFFORT=-O3 clean_all exe
--- sw/example/processor_check/run_check.sh
+++ sw/example/processor_check/run_check.sh
@@ -1,1 +1,2 @@
+echo "Starting processor check simulation..."
 make USER_FLAGS+="-DRUN_CHECK -DUART0_SIM_MODE -DUART1_SIM_MODE -g" MARCH=rv32imac clean_all sim
