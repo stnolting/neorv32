56ab241d5943cf7288df7f8a09a027a6a46ba697
stnolting <stnolting@gmail.com>
2022-01-26 06:17:29 +0100

    [sw/example/bitmanip_test] added Zbs & Zbc test cases

--- sw/example/bitmanip_test/main.c
+++ sw/example/bitmanip_test/main.c
@@ -3,7 +3,7 @@
 // # ********************************************************************************************* #
 // # BSD 3-Clause License                                                                          #
 // #                                                                                               #
-// # Copyright (c) 2021, Stephan Nolting. All rights reserved.                                     #
+// # Copyright (c) 2022, Stephan Nolting. All rights reserved.                                     #
 // #                                                                                               #
 // # Redistribution and use in source and binary forms, with or without modification, are          #
 // # permitted provided that the following conditions are met:                                     #
@@ -51,6 +51,14 @@
 #define BAUD_RATE      (19200)
 //** Number of test cases for each instruction */
 #define NUM_TEST_CASES (1000000)
+//** Enable Zbb tests */
+#define ENABLE_ZBB     (1)
+//** Enable Zba tests */
+#define ENABLE_ZBA     (1)
+//** Enable Zbs tests */
+#define ENABLE_ZBS     (1)
+//** Enable Zbc tests */
+#define ENABLE_ZBC     (1)
 /**@}*/
 
 
@@ -61,7 +69,7 @@ void print_report(int num_err, int num_tests);
 
 
 /**********************************************************************//**
- * Main function; test all available operations of the NEORV32 'Zbb' extensions
+ * Main function; test all available operations of the NEORV32 'B' extension
  * using bit manipulation intrinsics and software-only reference functions (emulation).
  *
  * @note This program requires the bit-manipulation CPU extension.
@@ -77,7 +85,7 @@ int main() {
   // capture all exceptions and give debug info via UART
   neorv32_rte_setup();
 
-  // init UART at default baud rate, no parity bits, ho hw flow control
+  // init UART at default baud rate, no parity bits, no hw flow control
   neorv32_uart0_setup(BAUD_RATE, PARITY_NONE, FLOW_CONTROL_NONE);
 
 // Disable compilation by default
@@ -91,22 +99,23 @@ int main() {
 #endif
 
   // intro
-  neorv32_uart0_printf("NEORV32 Bit-Manipulation Extension Test (Zba, Zbb)\n\n");
+  neorv32_uart0_printf("<<< NEORV32 Bit-Manipulation Extension ('B') Test >>>\n\n");
 
   // check available hardware extensions and compare with compiler flags
   neorv32_rte_check_isa(0); // silent = 0 -> show message if isa mismatch
 
-  // check if Zbb extension is implemented at all
+  // check if B extension is implemented at all
   if ((neorv32_cpu_csr_read(CSR_MISA) & (1<<CSR_MISA_B)) == 0) {
-    neorv32_uart0_print("Error! <B> extension not synthesized!\n");
+    neorv32_uart0_print("Error! B extension not synthesized!\n");
     return 1;
   }
 
   neorv32_uart0_printf("Starting bit-manipulation extension tests (%i test cases per instruction)...\n\n", num_tests);
 
-  neorv32_uart0_printf("-----------------------------------------\n");
+#if (ENABLE_ZBB != 0)
+  neorv32_uart0_printf("--------------------------------------------\n");
   neorv32_uart0_printf("Zbb - Basic bit-manipulation instructions\n");
-  neorv32_uart0_printf("-----------------------------------------\n");
+  neorv32_uart0_printf("--------------------------------------------\n");
 
   // ANDN
   neorv32_uart0_printf("\nANDN:\n");
@@ -328,13 +337,14 @@ int main() {
     err_cnt += check_result(i, opa, 0, res_sw, res_hw);
   }
   print_report(err_cnt, num_tests);
+#endif
 
 
-
+#if (ENABLE_ZBA != 0)
   neorv32_uart0_printf("\n\n");
-  neorv32_uart0_printf("-----------------------------------------\n");
-  neorv32_uart0_printf("Zba - Address generation instructions\n");
-  neorv32_uart0_printf("-----------------------------------------\n");
+  neorv32_uart0_printf("--------------------------------------------\n");
+  neorv32_uart0_printf("Zba - Address-generation instructions\n");
+  neorv32_uart0_printf("--------------------------------------------\n");
 
   // SH1ADD
   neorv32_uart0_printf("\nSH1ADD:\n");
@@ -370,16 +380,169 @@ int main() {
     err_cnt += check_result(i, opa, opb, res_sw, res_hw);
   }
   print_report(err_cnt, num_tests);
+#endif
 
 
-  neorv32_uart0_printf("\nBit manipulation extension tests done.\n");
+#if (ENABLE_ZBS != 0)
+  neorv32_uart0_printf("\n\n");
+  neorv32_uart0_printf("--------------------------------------------\n");
+  neorv32_uart0_printf("Zbs - Single-bit instructions\n");
+  neorv32_uart0_printf("--------------------------------------------\n");
 
+  // BCLR
+  neorv32_uart0_printf("\nBCLR:\n");
+  err_cnt = 0;
+  for (i=0;i<num_tests; i++) {
+    opa = xorshift32();
+    opb = xorshift32();
+    res_sw = riscv_emulate_bclr(opa, opb);
+    res_hw = riscv_intrinsic_bclr(opa, opb);
+    err_cnt += check_result(i, opa, opb, res_sw, res_hw);
+  }
+  print_report(err_cnt, num_tests);
+
+  // BCLRI
+  neorv32_uart0_printf("\nBCLRI (imm=20):\n"); // FIXME: static immediate
+  err_cnt = 0;
+  for (i=0;i<num_tests; i++) {
+    opa = xorshift32();
+    res_sw = riscv_emulate_bclr(opa, 20);
+    res_hw = riscv_intrinsic_bclri20(opa);
+    err_cnt += check_result(i, opa, opb, res_sw, res_hw);
+  }
+  print_report(err_cnt, num_tests);
+
+
+
+  // BEXT
+  neorv32_uart0_printf("\nBEXT:\n");
+  err_cnt = 0;
+  for (i=0;i<num_tests; i++) {
+    opa = xorshift32();
+    opb = xorshift32();
+    res_sw = riscv_emulate_bext(opa, opb);
+    res_hw = riscv_intrinsic_bext(opa, opb);
+    err_cnt += check_result(i, opa, opb, res_sw, res_hw);
+  }
+  print_report(err_cnt, num_tests);
+
+  // BEXTI
+  neorv32_uart0_printf("\nBEXTI (imm=20):\n"); // FIXME: static immediate
+  err_cnt = 0;
+  for (i=0;i<num_tests; i++) {
+    opa = xorshift32();
+    res_sw = riscv_emulate_bext(opa, 20);
+    res_hw = riscv_intrinsic_bexti20(opa);
+    err_cnt += check_result(i, opa, opb, res_sw, res_hw);
+  }
+  print_report(err_cnt, num_tests);
+
+
+
+  // BINV
+  neorv32_uart0_printf("\nBINV:\n");
+  err_cnt = 0;
+  for (i=0;i<num_tests; i++) {
+    opa = xorshift32();
+    opb = xorshift32();
+    res_sw = riscv_emulate_binv(opa, opb);
+    res_hw = riscv_intrinsic_binv(opa, opb);
+    err_cnt += check_result(i, opa, opb, res_sw, res_hw);
+  }
+  print_report(err_cnt, num_tests);
+
+  // BINVI
+  neorv32_uart0_printf("\nBINVI (imm=20):\n"); // FIXME: static immediate
+  err_cnt = 0;
+  for (i=0;i<num_tests; i++) {
+    opa = xorshift32();
+    res_sw = riscv_emulate_binv(opa, 20);
+    res_hw = riscv_intrinsic_binvi20(opa);
+    err_cnt += check_result(i, opa, opb, res_sw, res_hw);
+  }
+  print_report(err_cnt, num_tests);
+
+
+
+  // BSET
+  neorv32_uart0_printf("\nBSET:\n");
+  err_cnt = 0;
+  for (i=0;i<num_tests; i++) {
+    opa = xorshift32();
+    opb = xorshift32();
+    res_sw = riscv_emulate_bset(opa, opb);
+    res_hw = riscv_intrinsic_bset(opa, opb);
+    err_cnt += check_result(i, opa, opb, res_sw, res_hw);
+  }
+  print_report(err_cnt, num_tests);
+
+  // BSETI
+  neorv32_uart0_printf("\nBSETI (imm=20):\n"); // FIXME: static immediate
+  err_cnt = 0;
+  for (i=0;i<num_tests; i++) {
+    opa = xorshift32();
+    res_sw = riscv_emulate_bset(opa, 20);
+    res_hw = riscv_intrinsic_bseti20(opa);
+    err_cnt += check_result(i, opa, opb, res_sw, res_hw);
+  }
+  print_report(err_cnt, num_tests);
+#endif
+
+
+#if (ENABLE_ZBC != 0)
+  neorv32_uart0_printf("\n\n");
+  neorv32_uart0_printf("--------------------------------------------\n");
+  neorv32_uart0_printf("Zbc - Carry-less multiplication instructions\n");
+  neorv32_uart0_printf("--------------------------------------------\n");
+
+  neorv32_uart0_printf("\nWARNING: The NEORV32 CPU hardware does NOT support the Zbc extension yet!");
+  neorv32_uart0_printf("\n         Hence, illegal instruction exceptions should be triggered here.\n");
+
+  // CLMUL
+  neorv32_uart0_printf("\nCLMUL:\n");
+  err_cnt = 0;
+  for (i=0;i<1; i++) {
+    opa = xorshift32();
+    opb = xorshift32();
+    res_sw = riscv_emulate_clmul(opa, opb);
+    res_hw = riscv_intrinsic_clmul(opa, opb);
+    err_cnt += check_result(i, opa, opb, res_sw, res_hw);
+  }
+  print_report(err_cnt, num_tests);
+
+  // CLMULH
+  neorv32_uart0_printf("\nCLMULH:\n");
+  err_cnt = 0;
+  for (i=0;i<1; i++) {
+    opa = xorshift32();
+    opb = xorshift32();
+    res_sw = riscv_emulate_clmulh(opa, opb);
+    res_hw = riscv_intrinsic_clmulh(opa, opb);
+    err_cnt += check_result(i, opa, opb, res_sw, res_hw);
+  }
+  print_report(err_cnt, num_tests);
+
+  // CLMULR
+  neorv32_uart0_printf("\nCLMULR:\n");
+  err_cnt = 0;
+  for (i=0;i<1; i++) {
+    opa = xorshift32();
+    opb = xorshift32();
+    res_sw = riscv_emulate_clmulr(opa, opb);
+    res_hw = riscv_intrinsic_clmulr(opa, opb);
+    err_cnt += check_result(i, opa, opb, res_sw, res_hw);
+  }
+  print_report(err_cnt, num_tests);
+#endif
+
+
+  neorv32_uart0_printf("\n\nB extension tests completed.\n");
   return 0;
 }
 
 
 /**********************************************************************//**
- * Pseudo-Random Number Generator (to generate test vectors).
+ * Pseudo-Random Number Generator (to generate deterministic test vectors).
  *
  * @return Random data (32-bit).
  **************************************************************************/
@@ -434,4 +597,18 @@ void print_report(int num_err, int num_tests) {
   else {
     neorv32_uart0_printf("%c[1m[FAILED]%c[0m\n", 27, 27);
   }
+}
+
+
+/**********************************************************************//**
+ * "after-main" handler that is executed after the application's
+ * main function returns (called by crt0.S start-up code)
+ **************************************************************************/
+int __neorv32_crt0_after_main(int32_t return_code) {
+
+  if (return_code) {
+    neorv32_uart0_printf("\n<RTE> main function returned with exit code (%i) </RTE>\n", return_code);
+  }
+
+  return 0;
 }
