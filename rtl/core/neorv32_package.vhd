a3ca9a30d5f1fad1754ca1646dde8a2548670a6c
stnolting <stnolting@gmail.com>
2022-03-02 05:05:40 +0100

    :bug: [rtl] fix pmpaddr CSR layout

--- rtl/core/neorv32_package.vhd
+++ rtl/core/neorv32_package.vhd
@@ -65,7 +65,7 @@ package neorv32_package is
   -- Architecture Constants (do not modify!) ------------------------------------------------
   -- -------------------------------------------------------------------------------------------
   constant data_width_c : natural := 32; -- native data path width - do not change!
-  constant hw_version_c : std_ulogic_vector(31 downto 0) := x"01060807"; -- no touchy!
+  constant hw_version_c : std_ulogic_vector(31 downto 0) := x"01060808"; -- no touchy!
   constant archid_c     : natural := 19; -- official NEORV32 architecture ID - hands off!
 
   -- Check if we're inside the Matrix -------------------------------------------------------
@@ -90,7 +90,7 @@ package neorv32_package is
   -- Internal Interface Types ---------------------------------------------------------------
   -- -------------------------------------------------------------------------------------------
   type pmp_ctrl_if_t is array (0 to 15) of std_ulogic_vector(07 downto 0);
-  type pmp_addr_if_t is array (0 to 15) of std_ulogic_vector(31 downto 0);
+  type pmp_addr_if_t is array (0 to 15) of std_ulogic_vector(33 downto 2); -- bits 33:2 of phys. address
 
   -- Internal Memory Types Configuration Types ----------------------------------------------
   -- -------------------------------------------------------------------------------------------
