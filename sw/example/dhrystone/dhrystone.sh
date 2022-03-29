dcf77141ebe0bafe4058e301378ccb2505fcf56b
stnolting <stnolting@gmail.com>
2022-03-29 21:08:21 +0200

    minor bash script edits
    shebang added

--- sw/example/dhrystone/dhrystone.sh
+++ sw/example/dhrystone/dhrystone.sh
@@ -1,2 +1,6 @@
+#!/usr/bin/env bash
+
+set -e
+
 echo "Generating dhrystone executable..."
 make USER_FLAGS+="-DRUN_DHRYSTONE -DDHRY_ITERS=2000000 -DNOENUM" MARCH=rv32imc EFFORT=-O3 clean_all exe
--- sw/example/processor_check/run_check.sh
+++ sw/example/processor_check/run_check.sh
@@ -1,2 +1,6 @@
+#!/usr/bin/env bash
+
+set -e
+
 echo "Starting processor check simulation..."
 make USER_FLAGS+="-DRUN_CHECK -DUART0_SIM_MODE -DUART1_SIM_MODE -g" MARCH=rv32imac clean_all sim
