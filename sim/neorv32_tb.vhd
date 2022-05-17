41e1651d97cdadbeeabe2fe52cf1f7d8731a2c7a
stnolting <22944758+stnolting@users.noreply.github.com>
2022-05-17 16:35:18 +0200

    add IO_TRNG_FIFO generic to testbenches

--- sim/neorv32_tb.vhd
+++ sim/neorv32_tb.vhd
@@ -339,6 +339,7 @@ begin
     IO_PWM_NUM_CH                => 30,            -- number of PWM channels to implement (0..60); 0 = disabled
     IO_WDT_EN                    => true,          -- implement watch dog timer (WDT)?
     IO_TRNG_EN                   => true,          -- implement true random number generator (TRNG)?
+    IO_TRNG_FIFO                 => 4,             -- TRNG fifo depth, has to be a power of two, min 1
     IO_CFS_EN                    => true,          -- implement custom functions subsystem (CFS)?
     IO_CFS_CONFIG                => (others => '0'), -- custom CFS configuration generic
     IO_CFS_IN_SIZE               => 32,            -- size of CFS input conduit in bits
