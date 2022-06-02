17504e24875dd45f2cca3df20b3bac0687a7317d
stnolting <22944758+stnolting@users.noreply.github.com>
2022-06-02 18:38:15 +0200

    add UART HW reset

--- rtl/core/neorv32_uart.vhd
+++ rtl/core/neorv32_uart.vhd
@@ -74,6 +74,7 @@ entity neorv32_uart is
   port (
     -- host access --
     clk_i       : in  std_ulogic; -- global clock line
+    rstn_i      : in  std_ulogic; -- global reset line, low-active
     addr_i      : in  std_ulogic_vector(31 downto 0); -- address
     rden_i      : in  std_ulogic; -- read enable
     wren_i      : in  std_ulogic; -- write enable
@@ -256,9 +257,13 @@ begin
 
   -- Read/Write Access ----------------------------------------------------------------------
   -- -------------------------------------------------------------------------------------------
-  rw_access: process(clk_i)
+  rw_access: process(rstn_i, clk_i)
   begin
-    if rising_edge(clk_i) then
+    if (rstn_i = '0') then
+      ctrl   <= (others => '0');
+      ack_o  <= '-';
+      data_o <= (others => '0');
+    elsif rising_edge(clk_i) then
       -- bus access acknowledge --
       ack_o <= wren or rden;
 
