7a0e5073cf5de29baa54867285f4c68fc2397491
stnolting <22944758+stnolting@users.noreply.github.com>
2022-12-01 20:02:19 +0100

    [sw/lib] update CFU instruction wrappers
    add compatibility layer

--- sw/lib/include/neorv32_cpu_cfu.h
+++ sw/lib/include/neorv32_cpu_cfu.h
@@ -46,25 +46,35 @@ int neorv32_cpu_cfu_available(void);
 
 
 /**********************************************************************//**
- * @name CFU custom instructions ("intrinsics")
+ * @name Low-level CFU custom instruction wrappers ("intrinsics")
  **************************************************************************/
 /**@{*/
-/** CFU custom instruction 0 (funct3 = 000) */
-#define neorv32_cfu_cmd0(funct7, rs1, rs2) CUSTOM_INSTR_R2_TYPE(funct7, rs2, rs1, 0, RISCV_OPCODE_CUSTOM0)
-/** CFU custom instruction 1 (funct3 = 001) */
-#define neorv32_cfu_cmd1(funct7, rs1, rs2) CUSTOM_INSTR_R2_TYPE(funct7, rs2, rs1, 1, RISCV_OPCODE_CUSTOM0)
-/** CFU custom instruction 2 (funct3 = 010) */
-#define neorv32_cfu_cmd2(funct7, rs1, rs2) CUSTOM_INSTR_R2_TYPE(funct7, rs2, rs1, 2, RISCV_OPCODE_CUSTOM0)
-/** CFU custom instruction 3 (funct3 = 011) */
-#define neorv32_cfu_cmd3(funct7, rs1, rs2) CUSTOM_INSTR_R2_TYPE(funct7, rs2, rs1, 3, RISCV_OPCODE_CUSTOM0)
-/** CFU custom instruction 4 (funct3 = 100) */
-#define neorv32_cfu_cmd4(funct7, rs1, rs2) CUSTOM_INSTR_R2_TYPE(funct7, rs2, rs1, 4, RISCV_OPCODE_CUSTOM0)
-/** CFU custom instruction 5 (funct3 = 101) */
-#define neorv32_cfu_cmd5(funct7, rs1, rs2) CUSTOM_INSTR_R2_TYPE(funct7, rs2, rs1, 5, RISCV_OPCODE_CUSTOM0)
-/** CFU custom instruction 6 (funct3 = 110) */
-#define neorv32_cfu_cmd6(funct7, rs1, rs2) CUSTOM_INSTR_R2_TYPE(funct7, rs2, rs1, 6, RISCV_OPCODE_CUSTOM0)
-/** CFU custom instruction 7 (funct3 = 111) */
-#define neorv32_cfu_cmd7(funct7, rs1, rs2) CUSTOM_INSTR_R2_TYPE(funct7, rs2, rs1, 7, RISCV_OPCODE_CUSTOM0)
+/** R3-type CFU custom instruction prototype */
+#define neorv32_cfu_r3_instr(funct7, funct3, rs1, rs2) CUSTOM_INSTR_R3_TYPE(funct7, rs2, rs1, funct3, RISCV_OPCODE_CUSTOM0)
+/**@}*/
+
+
+/**********************************************************************//**
+ * @name Backward-compatibility layer (before version v1.7.8.x)
+ * do not use for new designs!
+ **************************************************************************/
+/**@{*/
+/** R3-type CFU custom instruction 0 (funct3 = 000) */
+#define neorv32_cfu_cmd0(funct7, rs1, rs2) neorv32_cfu_r3_instr(funct7, 0, rs1, rs2)
+/** R3-type CFU custom instruction 1 (funct3 = 001) */
+#define neorv32_cfu_cmd1(funct7, rs1, rs2) neorv32_cfu_r3_instr(funct7, 1, rs1, rs2)
+/** R3-type CFU custom instruction 2 (funct3 = 010) */
+#define neorv32_cfu_cmd2(funct7, rs1, rs2) neorv32_cfu_r3_instr(funct7, 2, rs1, rs2)
+/** R3-type CFU custom instruction 3 (funct3 = 011) */
+#define neorv32_cfu_cmd3(funct7, rs1, rs2) neorv32_cfu_r3_instr(funct7, 3, rs1, rs2)
+/** R3-type CFU custom instruction 4 (funct3 = 100) */
+#define neorv32_cfu_cmd4(funct7, rs1, rs2) neorv32_cfu_r3_instr(funct7, 4, rs1, rs2)
+/** R3-type CFU custom instruction 5 (funct3 = 101) */
+#define neorv32_cfu_cmd5(funct7, rs1, rs2) neorv32_cfu_r3_instr(funct7, 5, rs1, rs2)
+/** R3-type CFU custom instruction 6 (funct3 = 110) */
+#define neorv32_cfu_cmd6(funct7, rs1, rs2) neorv32_cfu_r3_instr(funct7, 6, rs1, rs2)
+/** R3-type CFU custom instruction 7 (funct3 = 111) */
+#define neorv32_cfu_cmd7(funct7, rs1, rs2) neorv32_cfu_r3_instr(funct7, 7, rs1, rs2)
 /**@}*/
 
 #endif // neorv32_cpu_cfu_h
