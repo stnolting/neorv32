2925f399d4db4e498b60f4a0bf8272a43a062ee0
stnolting <22944758+stnolting@users.noreply.github.com>
2022-12-08 22:47:18 +0100

    [sw/lib] add R5-type CFU prototypes

--- sw/lib/include/neorv32_cpu_cfu.h
+++ sw/lib/include/neorv32_cpu_cfu.h
@@ -53,13 +53,17 @@ int neorv32_cpu_cfu_available(void);
 #define neorv32_cfu_r3_instr(funct7, funct3, rs1, rs2) CUSTOM_INSTR_R3_TYPE(funct7, rs2, rs1, funct3, RISCV_OPCODE_CUSTOM0)
 /** R4-type CFU custom instruction prototype */
 #define neorv32_cfu_r4_instr(funct3, rs1, rs2, rs3) CUSTOM_INSTR_R4_TYPE(rs3, rs2, rs1, funct3, RISCV_OPCODE_CUSTOM1)
+/** R5-type CFU custom instruction A prototype  */
+#define neorv32_cfu_r5_instr_a(rs1, rs2, rs3, rs4) CUSTOM_INSTR_R5_TYPE(rs4, rs3, rs2, rs1, RISCV_OPCODE_CUSTOM2)
+/** R5-type CFU custom instruction B prototype */
+#define neorv32_cfu_r5_instr_b(rs1, rs2, rs3, rs4) CUSTOM_INSTR_R5_TYPE(rs4, rs3, rs2, rs1, RISCV_OPCODE_CUSTOM3)
 /**@}*/
 
 
 /**********************************************************************//**
  * @name Backward-compatibility layer (before version v1.7.8.2)
  * @note DO NOT USE FOR NEW DESIGNS!
- * @note THESE WRAPPERS WILL BE REMOVED IN THE FUTURE!
+ * @warning THESE WRAPPERS WILL BE REMOVED SOON!
  **************************************************************************/
 /**@{*/
 /** R3-type CFU custom instruction 0 (funct3 = 000) */
