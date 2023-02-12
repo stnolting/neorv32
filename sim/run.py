b1cfaadace17e08145f846fb126b168472cc6a59
Olof Kraigher <olof.kraigher@gmail.com>
2023-02-12 19:23:36 +0100

    Add run.py dump of VHDL-LS library mapping

--- sim/run.py
+++ sim/run.py
@@ -33,4 +33,27 @@ NEORV32.test_bench("neorv32_tb").set_generic("ci_mode", args.ci_mode)
 PRJ.set_sim_option("disable_ieee_warnings", True)
 PRJ.set_sim_option("ghdl.sim_flags", ["--max-stack-alloc=256"])
 
+def _gen_vhdl_ls(vu):
+    """
+    Generate the vhdl_ls.toml file required by VHDL-LS language server.
+    """
+    import toml
+
+    # Repo root
+    parent = Path(__file__).parent.parent
+
+    proj = vu._project
+    libs = proj.get_libraries()
+    vhdl_ls = {"libraries": {}}
+    for lib in libs:
+        files = [str(file).replace('\\', '/') for file in lib._source_files
+            # Conflicts with *.default.vhd
+            if not any(exclude in file for exclude in ('neorv32_imem.simple.vhd', 'neorv32_imem.legacy.vhd', 'neorv32_dmem.legacy.vhd'))
+        ]
+        vhdl_ls["libraries"][lib.name] = {"files": files}
+    with open(parent / 'vhdl_ls.toml', "w") as f:
+        toml.dump(vhdl_ls, f)
+
+_gen_vhdl_ls(PRJ)
+
 PRJ.main()
