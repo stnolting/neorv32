67de37e3af9f884a5634c1f93efadb79a45e0d19
stnolting <22944758+stnolting@users.noreply.github.com>
2023-03-31 11:07:56 +0200

    :rocket: preparing new release 1.8.3

--- rtl/core/neorv32_package.vhd
+++ rtl/core/neorv32_package.vhd
@@ -60,7 +60,7 @@ package neorv32_package is
 
   -- Architecture Constants (do not modify!) ------------------------------------------------
   -- -------------------------------------------------------------------------------------------
-  constant hw_version_c : std_ulogic_vector(31 downto 0) := x"01080209"; -- hardware version
+  constant hw_version_c : std_ulogic_vector(31 downto 0) := x"01080300"; -- hardware version
   constant archid_c     : natural := 19; -- official RISC-V architecture ID
 
   -- Check if we're inside the Matrix -------------------------------------------------------
