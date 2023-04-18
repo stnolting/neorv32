56811120e83a3dbe8adb01096373cc129729a525
Alvaro Gamez Machado <alvaro.gamez@hazent.com>
2023-04-18 10:46:11 +0200

    [rtl] NEOLED: do not use VHDL'08 features

--- rtl/core/neorv32_neoled.vhd
+++ rtl/core/neorv32_neoled.vhd
@@ -85,8 +85,8 @@ architecture neorv32_neoled_rtl of neorv32_neoled is
   constant lo_abb_c : natural := index_size_f(neoled_size_c); -- low address boundary bit
 
   -- interface configuration
-  constant neoled_ctrl_offset_c   : std_ulogic_vector(lo_abb_c-1 downto 0) := 3x"0";
-  constant neoled_data_offset_c   : std_ulogic_vector(lo_abb_c-1 downto 0) := 3x"4";
+  constant neoled_ctrl_offset_c   : std_ulogic_vector(lo_abb_c-1 downto 0) := std_ulogic_vector(to_signed(0 * 4, lo_abb_c));
+  constant neoled_data_offset_c   : std_ulogic_vector(lo_abb_c-1 downto 0) := std_ulogic_vector(to_signed(1 * 4, lo_abb_c));
 
   -- access control --
   signal acc_en : std_ulogic; -- module access enable
