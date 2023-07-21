bec057a7a883f6bbb21d6a9d385814a05bd8fe30
stnolting <22944758+stnolting@users.noreply.github.com>
2023-07-21 10:42:33 +0200

    [rtl] add LR/SC control signal to package
    -> SoC bus request bus: add "is reservation set operation" signal
    -> CPU control bus: add "is reservation set operation" signal

--- rtl/core/neorv32_package.vhd
+++ rtl/core/neorv32_package.vhd
@@ -56,7 +56,7 @@ package neorv32_package is
 
   -- Architecture Constants -----------------------------------------------------------------
   -- -------------------------------------------------------------------------------------------
-  constant hw_version_c : std_ulogic_vector(31 downto 0) := x"01080606"; -- hardware version
+  constant hw_version_c : std_ulogic_vector(31 downto 0) := x"01080607"; -- hardware version
   constant archid_c     : natural := 19; -- official RISC-V architecture ID
   constant XLEN         : natural := 32; -- native data path width, do not change!
 
@@ -160,6 +160,7 @@ package neorv32_package is
     re   : std_ulogic; -- read request (single-shot)
     src  : std_ulogic; -- access source (1=instruction fetch, 0=data access)
     priv : std_ulogic; -- set if privileged (machine-mode) access
+    rvso : std_ulogic; -- set if reservation set operation (atomic LR/SC)
   end record;
 
   -- bus response --
@@ -177,7 +178,8 @@ package neorv32_package is
     we   => '0',
     re   => '0',
     src  => '0',
-    priv => '0'
+    priv => '0',
+    rvso => '0'
   );
 
   -- endpoint termination --
@@ -663,6 +665,7 @@ package neorv32_package is
     bus_fence     : std_ulogic;                     -- fence operation
     bus_fencei    : std_ulogic;                     -- fence.i operation
     bus_priv      : std_ulogic;                     -- effective privilege level for load/store
+    bus_rvso      : std_ulogic;                     -- reservation set operation (atomic LR/SC)
     -- instruction word --
     ir_funct3     : std_ulogic_vector(02 downto 0); -- funct3 bit field
     ir_funct12    : std_ulogic_vector(11 downto 0); -- funct12 bit field
@@ -695,6 +698,7 @@ package neorv32_package is
     bus_fence    => '0',
     bus_fencei   => '0',
     bus_priv     => '0',
+    bus_rvso     => '0',
     ir_funct3    => (others => '0'),
     ir_funct12   => (others => '0'),
     ir_opcode    => (others => '0'),
