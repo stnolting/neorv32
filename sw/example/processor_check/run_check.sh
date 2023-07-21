246359b08dddc2bb125ddbcb58a1d444135ac2e6
stnolting <22944758+stnolting@users.noreply.github.com>
2023-07-21 10:40:05 +0200

    [sw] update processor check program
    clean-up & add LR/SC test cases

--- sw/example/processor_check/run_check.sh
+++ sw/example/processor_check/run_check.sh
@@ -3,4 +3,4 @@
 set -e
 
 echo "Starting processor check simulation..."
-make USER_FLAGS+="-DRUN_CHECK -DUART0_SIM_MODE -DUART1_SIM_MODE -g -flto" MARCH=rv32imc clean_all all sim
+make USER_FLAGS+="-DRUN_CHECK -DUART0_SIM_MODE -DUART1_SIM_MODE -g -flto" EFFORT=-Os MARCH=rv32imac clean_all all sim
