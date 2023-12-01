52c6f22e5d317ce797f3fa98acfb09d25e56db89
stnolting <22944758+stnolting@users.noreply.github.com>
2023-12-01 21:40:17 +0100

    [rtl] add Zicond co-processor VHDL source

---
+++ rtl/core/neorv32_cpu_cp_cond.vhd
@@ -0,0 +1,86 @@
+-- #################################################################################################
+-- # << NEORV32 CPU - Co-Processor: RISC-V Conditional Operations ('Zicond') ISA Extension     >>  #
+-- # ********************************************************************************************* #
+-- # BSD 3-Clause License                                                                          #
+-- #                                                                                               #
+-- # Copyright (c) 2023, Stephan Nolting. All rights reserved.                                     #
+-- #                                                                                               #
+-- # Redistribution and use in source and binary forms, with or without modification, are          #
+-- # permitted provided that the following conditions are met:                                     #
+-- #                                                                                               #
+-- # 1. Redistributions of source code must retain the above copyright notice, this list of        #
+-- #    conditions and the following disclaimer.                                                   #
+-- #                                                                                               #
+-- # 2. Redistributions in binary form must reproduce the above copyright notice, this list of     #
+-- #    conditions and the following disclaimer in the documentation and/or other materials        #
+-- #    provided with the distribution.                                                            #
+-- #                                                                                               #
+-- # 3. Neither the name of the copyright holder nor the names of its contributors may be used to  #
+-- #    endorse or promote products derived from this software without specific prior written      #
+-- #    permission.                                                                                #
+-- #                                                                                               #
+-- # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS   #
+-- # OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF               #
+-- # MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE    #
+-- # COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,     #
+-- # EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE #
+-- # GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED    #
+-- # AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     #
+-- # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED  #
+-- # OF THE POSSIBILITY OF SUCH DAMAGE.                                                            #
+-- # ********************************************************************************************* #
+-- # The NEORV32 RISC-V Processor - https://github.com/stnolting/neorv32       (c) Stephan Nolting #
+-- #################################################################################################
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library neorv32;
+use neorv32.neorv32_package.all;
+
+entity neorv32_cpu_cp_cond is
+  port (
+    -- global control --
+    clk_i   : in  std_ulogic; -- global clock, rising edge
+    rstn_i  : in  std_ulogic; -- global reset, low-active, async
+    ctrl_i  : in  ctrl_bus_t; -- main control bus
+    start_i : in  std_ulogic; -- trigger operation
+    -- data input --
+    rs1_i   : in  std_ulogic_vector(XLEN-1 downto 0); -- rf source 1
+    rs2_i   : in  std_ulogic_vector(XLEN-1 downto 0); -- rf source 2
+    -- result and status --
+    res_o   : out std_ulogic_vector(XLEN-1 downto 0); -- operation result
+    valid_o : out std_ulogic -- data output valid
+  );
+end neorv32_cpu_cp_cond;
+
+architecture neorv32_cpu_cp_cond_rtl of neorv32_cpu_cp_cond is
+
+  signal rs2_zero, condition : std_ulogic;
+
+begin
+
+  -- conditional output --
+  cond_out: process(rstn_i, clk_i)
+  begin
+    if (rstn_i = '0') then
+      res_o <= (others => '0');
+    elsif rising_edge(clk_i) then
+      if (start_i = '1') and (condition = '1') then -- unit triggered and condition true
+        res_o <= rs1_i;
+      else
+        res_o <= (others => '0');
+      end if;
+    end if;
+  end process cond_out;
+
+  -- condition check --
+  rs2_zero  <= '1' when (or_reduce_f(rs2_i) = '0') else '0';
+  condition <= rs2_zero xnor ctrl_i.ir_funct3(1); -- equal zero / non equal zero
+
+  -- processing done --
+  valid_o <= start_i;
+
+
+end neorv32_cpu_cp_cond_rtl;
