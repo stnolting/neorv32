0e08dc328c6f4faef53e20809a3f57141fe28152
stnolting <22944758+stnolting@users.noreply.github.com>
2024-02-09 11:47:22 +0100

    [sw] rename XIP-available flag in SYSINFO

--- sw/lib/source/neorv32_xip.c
+++ sw/lib/source/neorv32_xip.c
@@ -51,7 +51,7 @@
  **************************************************************************/
 int neorv32_xip_available(void) {
 
-  if (NEORV32_SYSINFO->SOC & (1 << SYSINFO_SOC_IO_XIP)) {
+  if (NEORV32_SYSINFO->SOC & (1 << SYSINFO_SOC_XIP)) {
     return 1;
   }
   else {
