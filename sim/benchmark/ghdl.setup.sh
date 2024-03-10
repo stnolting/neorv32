3a93c5e141c158ef53c6118ed160b6c3be58ee87
Mikael Mortensen <mikael.mortensen@skyworksinc.com>
2024-03-10 15:40:38 +0000

    Updated Performance test
    Now with functional makefiles and tests

---
+++ sim/benchmark/ghdl.setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd $(dirname "$0")
+
+NEORV32_LOCAL_RTL=${NEORV32_LOCAL_RTL:-../../rtl}
+
+mkdir -p build
+
+ghdl -i --std=08 --work=neorv32 --workdir=build \
+  "$NEORV32_LOCAL_RTL"/core/*.vhd \
+  "$NEORV32_LOCAL_RTL"/core/mem/*.vhd \
+  "$NEORV32_LOCAL_RTL"/processor_templates/*.vhd \
+  "$NEORV32_LOCAL_RTL"/system_integration/*.vhd \
+  "$NEORV32_LOCAL_RTL"/test_setups/*.vhd \
+  neorv32_tb.benchmark.vhd \
+  uart_rx.simple.vhd
