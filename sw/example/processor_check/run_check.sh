8cb7e9d9da57715d95e1936f27200ae552345ebb
stnolting <22944758+stnolting@users.noreply.github.com>
2024-04-04 19:59:07 +0200

    minor cleanups
    trim Zbc

--- sw/example/processor_check/run_check.sh
+++ sw/example/processor_check/run_check.sh
@@ -3,4 +3,4 @@
 set -e
 
 echo "Starting processor check simulation..."
-make USER_FLAGS+="-DUART0_SIM_MODE -DUART1_SIM_MODE -g -flto" EFFORT=-Os MARCH=rv32ima_zba_zbb_zbc_zbs_zicsr_zifencei_zicond clean_all all sim-check
+make USER_FLAGS+="-DUART0_SIM_MODE -DUART1_SIM_MODE -g -flto" EFFORT=-Os MARCH=rv32ima_zba_zbb_zbs_zicsr_zifencei_zicond clean_all all sim-check
