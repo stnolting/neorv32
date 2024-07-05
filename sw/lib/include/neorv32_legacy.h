d1cdf81107eb34210b4da2da5793fac908494fea
stnolting <22944758+stnolting@users.noreply.github.com>
2024-07-05 12:56:00 +0200

    [gpio] rename GPIO interface registers
    backwards compatibility aliases added to neorv32_legacy.h

--- sw/lib/include/neorv32_legacy.h
+++ sw/lib/include/neorv32_legacy.h
@@ -67,4 +67,16 @@
 #define neorv32_uart1_scan(buffer, max_size, echo) neorv32_uart_scan(NEORV32_UART1, buffer, max_size, echo)
 /**@}*/
 
+
+/**********************************************************************//**
+ * @name GPIO aliases
+ **************************************************************************/
+/**@{*/
+#define INPUT_LO  INPUT[0]
+#define INPUT_HI  INPUT[1]
+#define OUTPUT_LO OUTPUT[0]
+#define OUTPUT_HI OUTPUT[1]
+/**@}*/
+
+
 #endif // neorv32_legacy_h
