ab6191bce50ec9791d96142034036ed7d9d11f66
stnolting <stnolting@gmail.com>
2024-07-19 20:58:23 +0200

    :warning: [sw/lib] re-arrange SPI control register

--- sw/lib/include/neorv32_spi.h
+++ sw/lib/include/neorv32_spi.h
@@ -28,7 +28,7 @@
 /** SPI module prototype */
 typedef volatile struct __attribute__((packed,aligned(4))) {
   uint32_t CTRL;  /**< offset 0: control register (#NEORV32_SPI_CTRL_enum) */
-  uint32_t DATA;  /**< offset 4: data register */
+  uint32_t DATA;  /**< offset 4: data register  (#NEORV32_SPI_DATA_enum) */
 } neorv32_spi_t;
 
 /** SPI module hardware access (#neorv32_spi_t) */
@@ -39,18 +39,14 @@ enum NEORV32_SPI_CTRL_enum {
   SPI_CTRL_EN           =  0, /**< SPI control register(0)  (r/w): SPI unit enable */
   SPI_CTRL_CPHA         =  1, /**< SPI control register(1)  (r/w): Clock phase */
   SPI_CTRL_CPOL         =  2, /**< SPI control register(2)  (r/w): Clock polarity */
-  SPI_CTRL_CS_SEL0      =  3, /**< SPI control register(3)  (r/w): Direct chip select bit 1 */
-  SPI_CTRL_CS_SEL1      =  4, /**< SPI control register(4)  (r/w): Direct chip select bit 2 */
-  SPI_CTRL_CS_SEL2      =  5, /**< SPI control register(5)  (r/w): Direct chip select bit 2 */
-  SPI_CTRL_CS_EN        =  6, /**< SPI control register(6)  (r/w): Chip select enable (selected CS line output is low when set) */
-  SPI_CTRL_PRSC0        =  7, /**< SPI control register(7)  (r/w): Clock prescaler select bit 0 */
-  SPI_CTRL_PRSC1        =  8, /**< SPI control register(8)  (r/w): Clock prescaler select bit 1 */
-  SPI_CTRL_PRSC2        =  9, /**< SPI control register(9)  (r/w): Clock prescaler select bit 2 */
-  SPI_CTRL_CDIV0        = 10, /**< SPI control register(10) (r/w): Clock divider bit 0 */
-  SPI_CTRL_CDIV1        = 11, /**< SPI control register(11) (r/w): Clock divider bit 1 */
-  SPI_CTRL_CDIV2        = 12, /**< SPI control register(12) (r/w): Clock divider bit 2 */
-  SPI_CTRL_CDIV3        = 13, /**< SPI control register(13) (r/w): Clock divider bit 3 */
-  SPI_CTRL_HIGHSPEED    = 14, /**< SPI control register(14) (r/w): High-speed mode */
+  SPI_CTRL_PRSC0        =  3, /**< SPI control register(3)  (r/w): Clock prescaler select bit 0 */
+  SPI_CTRL_PRSC1        =  4, /**< SPI control register(4)  (r/w): Clock prescaler select bit 1 */
+  SPI_CTRL_PRSC2        =  5, /**< SPI control register(5)  (r/w): Clock prescaler select bit 2 */
+  SPI_CTRL_CDIV0        =  6, /**< SPI control register(6)  (r/w): Clock divider bit 0 */
+  SPI_CTRL_CDIV1        =  7, /**< SPI control register(7)  (r/w): Clock divider bit 1 */
+  SPI_CTRL_CDIV2        =  8, /**< SPI control register(8)  (r/w): Clock divider bit 2 */
+  SPI_CTRL_CDIV3        =  9, /**< SPI control register(9)  (r/w): Clock divider bit 3 */
+  SPI_CTRL_HIGHSPEED    = 10, /**< SPI control register(10) (r/w): High-speed mode */
 
   SPI_CTRL_RX_AVAIL     = 16, /**< SPI control register(16) (r/-): RX FIFO data available (RX FIFO not empty) */
   SPI_CTRL_TX_EMPTY     = 17, /**< SPI control register(17) (r/-): TX FIFO empty */
@@ -65,7 +61,16 @@ enum NEORV32_SPI_CTRL_enum {
   SPI_CTRL_FIFO_LSB     = 24, /**< SPI control register(24) (r/-): log2(FIFO size), lsb */
   SPI_CTRL_FIFO_MSB     = 27, /**< SPI control register(27) (r/-): log2(FIFO size), msb */
 
+  SPI_CS_ACTIVE         = 30, /**< SPI control register(30) (r/-): At least one CS line is active when set */
   SPI_CTRL_BUSY         = 31  /**< SPI control register(31) (r/-): SPI busy flag */
+};
+
+/** SPI data register bits */
+enum NEORV32_SPI_DATA_enum {
+  SPI_DATA_LSB  =  0, /**< SPI data register(0)  (r/w): Data byte LSB */
+  SPI_DATA_MSB  =  1, /**< SPI data register(1)  (r/w): Data byte LSB */
+  SPI_DATA_CSEN =  3, /**< SPI data register(3)  (-/w): Chip select enable (command-mode only) */
+  SPI_DATA_CMD  = 31  /**< SPI data register(31) (-/w): Command (=1) / data (=0) select */
 };
 /**@}*/
 
@@ -87,6 +92,9 @@ void     neorv32_spi_cs_dis(void);
 uint8_t  neorv32_spi_trans(uint8_t tx_data);
 void     neorv32_spi_put_nonblocking(uint8_t tx_data);
 uint8_t  neorv32_spi_get_nonblocking(void);
+void     neorv32_spi_cs_en_nonblocking(int cs);
+void     neorv32_spi_cs_dis_nonblocking(void);
+int      neorv32_spi_check_cs(void);
 int      neorv32_spi_busy(void);
 /**@}*/
 
