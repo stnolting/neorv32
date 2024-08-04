14ef5396fb43aa92d5eb3c02b995b8c546aa4f6a
stnolting <stnolting@gmail.com>
2024-08-04 19:39:53 +0200

    [sim] use file lists for simple GHDL setup
    experimental!

--- sim/simple/ghdl.setup.sh
+++ sim/simple/ghdl.setup.sh
@@ -6,11 +6,13 @@ cd $(dirname "$0")
 
 NEORV32_LOCAL_RTL=${NEORV32_LOCAL_RTL:-../../rtl}
 
+FILE_LIST=`cat $NEORV32_LOCAL_RTL/file_list_soc.f`
+CORE_SRCS="${FILE_LIST//NEORV32_RTL_PATH_PLACEHOLDER/"$NEORV32_LOCAL_RTL"}"
+
 mkdir -p build
 
 ghdl -i --work=neorv32 --workdir=build \
-  "$NEORV32_LOCAL_RTL"/core/*.vhd \
-  "$NEORV32_LOCAL_RTL"/core/mem/*.vhd \
+  $CORE_SRCS \
   "$NEORV32_LOCAL_RTL"/processor_templates/*.vhd \
   "$NEORV32_LOCAL_RTL"/system_integration/*.vhd \
   "$NEORV32_LOCAL_RTL"/test_setups/*.vhd \
