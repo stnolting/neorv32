a07666251b35f3bd7d3ea55483f6839feb058b6d
stnolting <stnolting@gmail.com>
2024-09-27 21:38:01 +0200

    [mxisa csr] add Zbkc flag

--- rtl/core/neorv32_cpu_control.vhd
+++ rtl/core/neorv32_cpu_control.vhd
@@ -41,7 +41,8 @@ entity neorv32_cpu_control is
     CPU_EXTENSION_RISCV_E      : boolean; -- implement embedded-class register file extension?
     CPU_EXTENSION_RISCV_M      : boolean; -- implement mul/div extension?
     CPU_EXTENSION_RISCV_U      : boolean; -- implement user mode extension?
-    CPU_EXTENSION_RISCV_Zbkb   : boolean; -- implement bit-manipulation instructions for cryptography
+    CPU_EXTENSION_RISCV_Zbkb   : boolean; -- implement bit-manipulation instructions for cryptography?
+    CPU_EXTENSION_RISCV_Zbkc   : boolean; -- implement carry-less multiplication instructions?
     CPU_EXTENSION_RISCV_Zbkx   : boolean; -- implement cryptography crossbar permutation extension?
     CPU_EXTENSION_RISCV_Zfinx  : boolean; -- implement 32-bit floating-point extension (using INT regs)?
     CPU_EXTENSION_RISCV_Zicntr : boolean; -- implement base counters?
@@ -1896,6 +1897,7 @@ begin
             csr.rdata(14) <= bool_to_ulogic_f(CPU_EXTENSION_RISCV_Zkne);   -- Zkne: cryptography NIST AES encryption
             csr.rdata(15) <= bool_to_ulogic_f(CPU_EXTENSION_RISCV_Zknh);   -- Zknh: cryptography NIST hash functions
             csr.rdata(16) <= bool_to_ulogic_f(CPU_EXTENSION_RISCV_Zbkb);   -- Zbkb: bit manipulation instructions for cryptography
+            csr.rdata(17) <= bool_to_ulogic_f(CPU_EXTENSION_RISCV_Zbkc);   -- Zbkc: carry-less multiplication for cryptography
             -- misc --
             csr.rdata(20) <= bool_to_ulogic_f(is_simulation_c);            -- is this a simulation?
             -- tuning options --
--- sw/lib/include/neorv32_cpu_csr.h
+++ sw/lib/include/neorv32_cpu_csr.h
@@ -324,6 +324,7 @@ enum NEORV32_CSR_XISA_enum {
   CSR_MXISA_ZKNE      = 14, /**< CPU mxisa CSR (14): scalar cryptography - NIST AES encryption (r/-)*/
   CSR_MXISA_ZKNH      = 15, /**< CPU mxisa CSR (15): scalar cryptography - NIST hash functions (r/-)*/
   CSR_MXISA_ZBKB      = 16, /**< CPU mxisa CSR (16): scalar cryptography - bit manipulation instructions (r/-)*/
+  CSR_MXISA_ZBKC      = 17, /**< CPU mxisa CSR (17): scalar cryptography - carry-less multiplication instructions (r/-)*/
 
   // Misc
   CSR_MXISA_IS_SIM    = 20, /**< CPU mxisa CSR (20): this might be a simulation when set (r/-)*/
