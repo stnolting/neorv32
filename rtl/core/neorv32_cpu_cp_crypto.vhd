120910cb623ca75d0b4d7e0cdb433e1bf13b955b
stnolting <stnolting@gmail.com>
2024-09-28 20:23:05 +0200

    [crypto-cp] add ShangMi extensions

--- rtl/core/neorv32_cpu_cp_crypto.vhd
+++ rtl/core/neorv32_cpu_cp_crypto.vhd
@@ -23,10 +23,12 @@ use neorv32.neorv32_package.all;
 
 entity neorv32_cpu_cp_crypto is
   generic (
-    EN_ZBKX : boolean; -- enable crossbar permutation extension
-    EN_ZKNH : boolean; -- enable NIST hash extension
-    EN_ZKNE : boolean; -- enable NIST AES encryption
-    EN_ZKND : boolean  -- enable NIST AES decryption
+    EN_ZBKX  : boolean; -- enable crossbar permutation extension
+    EN_ZKNH  : boolean; -- enable NIST hash extension
+    EN_ZKNE  : boolean; -- enable NIST AES encryption extension
+    EN_ZKND  : boolean; -- enable NIST AES decryption extension
+    EN_ZKSED : boolean; -- enable ShangMi hash extension
+    EN_ZKSH  : boolean  -- enable ShangMi block cypher extension
   );
   port (
     -- global control --
@@ -48,44 +50,67 @@ architecture neorv32_cpu_cp_crypto_rtl of neorv32_cpu_cp_crypto is
   -- look-up tables (ROMs)
   -- ----------------------------------------------------------------------------------------
 
-  -- s-box --
-  type sbox_rom_t is array (0 to 511) of std_ulogic_vector(7 downto 0);
-  constant sbox_c : sbox_rom_t := (
+  type rom512x8_t is array (0 to 511) of std_ulogic_vector(7 downto 0);
+  type rom256x8_t is array (0 to 255) of std_ulogic_vector(7 downto 0);
+
+  -- AES s-box --
+  constant aes_sbox_c : rom512x8_t := (
     -- forward --
-    x"63", x"7c", x"77", x"7b", x"f2", x"6b", x"6f", x"c5", x"30", x"01", x"67", x"2b", x"fe", x"d7", x"ab", x"76",
-    x"ca", x"82", x"c9", x"7d", x"fa", x"59", x"47", x"f0", x"ad", x"d4", x"a2", x"af", x"9c", x"a4", x"72", x"c0",
-    x"b7", x"fd", x"93", x"26", x"36", x"3f", x"f7", x"cc", x"34", x"a5", x"e5", x"f1", x"71", x"d8", x"31", x"15",
-    x"04", x"c7", x"23", x"c3", x"18", x"96", x"05", x"9a", x"07", x"12", x"80", x"e2", x"eb", x"27", x"b2", x"75",
-    x"09", x"83", x"2c", x"1a", x"1b", x"6e", x"5a", x"a0", x"52", x"3b", x"d6", x"b3", x"29", x"e3", x"2f", x"84",
-    x"53", x"d1", x"00", x"ed", x"20", x"fc", x"b1", x"5b", x"6a", x"cb", x"be", x"39", x"4a", x"4c", x"58", x"cf",
-    x"d0", x"ef", x"aa", x"fb", x"43", x"4d", x"33", x"85", x"45", x"f9", x"02", x"7f", x"50", x"3c", x"9f", x"a8",
-    x"51", x"a3", x"40", x"8f", x"92", x"9d", x"38", x"f5", x"bc", x"b6", x"da", x"21", x"10", x"ff", x"f3", x"d2",
-    x"cd", x"0c", x"13", x"ec", x"5f", x"97", x"44", x"17", x"c4", x"a7", x"7e", x"3d", x"64", x"5d", x"19", x"73",
-    x"60", x"81", x"4f", x"dc", x"22", x"2a", x"90", x"88", x"46", x"ee", x"b8", x"14", x"de", x"5e", x"0b", x"db",
-    x"e0", x"32", x"3a", x"0a", x"49", x"06", x"24", x"5c", x"c2", x"d3", x"ac", x"62", x"91", x"95", x"e4", x"79",
-    x"e7", x"c8", x"37", x"6d", x"8d", x"d5", x"4e", x"a9", x"6c", x"56", x"f4", x"ea", x"65", x"7a", x"ae", x"08",
-    x"ba", x"78", x"25", x"2e", x"1c", x"a6", x"b4", x"c6", x"e8", x"dd", x"74", x"1f", x"4b", x"bd", x"8b", x"8a",
-    x"70", x"3e", x"b5", x"66", x"48", x"03", x"f6", x"0e", x"61", x"35", x"57", x"b9", x"86", x"c1", x"1d", x"9e",
-    x"e1", x"f8", x"98", x"11", x"69", x"d9", x"8e", x"94", x"9b", x"1e", x"87", x"e9", x"ce", x"55", x"28", x"df",
-    x"8c", x"a1", x"89", x"0d", x"bf", x"e6", x"42", x"68", x"41", x"99", x"2d", x"0f", x"b0", x"54", x"bb", x"16",
+    x"63", x"7C", x"77", x"7B", x"F2", x"6B", x"6F", x"C5", x"30", x"01", x"67", x"2B", x"FE", x"D7", x"AB", x"76",
+    x"CA", x"82", x"C9", x"7D", x"FA", x"59", x"47", x"F0", x"AD", x"D4", x"A2", x"AF", x"9C", x"A4", x"72", x"C0",
+    x"B7", x"FD", x"93", x"26", x"36", x"3F", x"F7", x"CC", x"34", x"A5", x"E5", x"F1", x"71", x"D8", x"31", x"15",
+    x"04", x"C7", x"23", x"C3", x"18", x"96", x"05", x"9A", x"07", x"12", x"80", x"E2", x"EB", x"27", x"B2", x"75",
+    x"09", x"83", x"2C", x"1A", x"1B", x"6E", x"5A", x"A0", x"52", x"3B", x"D6", x"B3", x"29", x"E3", x"2F", x"84",
+    x"53", x"D1", x"00", x"ED", x"20", x"FC", x"B1", x"5B", x"6A", x"CB", x"BE", x"39", x"4A", x"4C", x"58", x"CF",
+    x"D0", x"EF", x"AA", x"FB", x"43", x"4D", x"33", x"85", x"45", x"F9", x"02", x"7F", x"50", x"3C", x"9F", x"A8",
+    x"51", x"A3", x"40", x"8F", x"92", x"9D", x"38", x"F5", x"BC", x"B6", x"DA", x"21", x"10", x"FF", x"F3", x"D2",
+    x"CD", x"0C", x"13", x"EC", x"5F", x"97", x"44", x"17", x"C4", x"A7", x"7E", x"3D", x"64", x"5D", x"19", x"73",
+    x"60", x"81", x"4F", x"DC", x"22", x"2A", x"90", x"88", x"46", x"EE", x"B8", x"14", x"DE", x"5E", x"0B", x"DB",
+    x"E0", x"32", x"3A", x"0A", x"49", x"06", x"24", x"5C", x"C2", x"D3", x"AC", x"62", x"91", x"95", x"E4", x"79",
+    x"E7", x"C8", x"37", x"6D", x"8D", x"D5", x"4E", x"A9", x"6C", x"56", x"F4", x"EA", x"65", x"7A", x"AE", x"08",
+    x"BA", x"78", x"25", x"2E", x"1C", x"A6", x"B4", x"C6", x"E8", x"DD", x"74", x"1F", x"4B", x"BD", x"8B", x"8A",
+    x"70", x"3E", x"B5", x"66", x"48", x"03", x"F6", x"0E", x"61", x"35", x"57", x"B9", x"86", x"C1", x"1D", x"9E",
+    x"E1", x"F8", x"98", x"11", x"69", x"D9", x"8E", x"94", x"9B", x"1E", x"87", x"E9", x"CE", x"55", x"28", x"DF",
+    x"8C", x"A1", x"89", x"0D", x"BF", x"E6", x"42", x"68", x"41", x"99", x"2D", x"0F", x"B0", x"54", x"BB", x"16",
     -- inverse --
-    x"52", x"09", x"6a", x"d5", x"30", x"36", x"a5", x"38", x"bf", x"40", x"a3", x"9e", x"81", x"f3", x"d7", x"fb",
-    x"7c", x"e3", x"39", x"82", x"9b", x"2f", x"ff", x"87", x"34", x"8e", x"43", x"44", x"c4", x"de", x"e9", x"cb",
-    x"54", x"7b", x"94", x"32", x"a6", x"c2", x"23", x"3d", x"ee", x"4c", x"95", x"0b", x"42", x"fa", x"c3", x"4e",
-    x"08", x"2e", x"a1", x"66", x"28", x"d9", x"24", x"b2", x"76", x"5b", x"a2", x"49", x"6d", x"8b", x"d1", x"25",
-    x"72", x"f8", x"f6", x"64", x"86", x"68", x"98", x"16", x"d4", x"a4", x"5c", x"cc", x"5d", x"65", x"b6", x"92",
-    x"6c", x"70", x"48", x"50", x"fd", x"ed", x"b9", x"da", x"5e", x"15", x"46", x"57", x"a7", x"8d", x"9d", x"84",
-    x"90", x"d8", x"ab", x"00", x"8c", x"bc", x"d3", x"0a", x"f7", x"e4", x"58", x"05", x"b8", x"b3", x"45", x"06",
-    x"d0", x"2c", x"1e", x"8f", x"ca", x"3f", x"0f", x"02", x"c1", x"af", x"bd", x"03", x"01", x"13", x"8a", x"6b",
-    x"3a", x"91", x"11", x"41", x"4f", x"67", x"dc", x"ea", x"97", x"f2", x"cf", x"ce", x"f0", x"b4", x"e6", x"73",
-    x"96", x"ac", x"74", x"22", x"e7", x"ad", x"35", x"85", x"e2", x"f9", x"37", x"e8", x"1c", x"75", x"df", x"6e",
-    x"47", x"f1", x"1a", x"71", x"1d", x"29", x"c5", x"89", x"6f", x"b7", x"62", x"0e", x"aa", x"18", x"be", x"1b",
-    x"fc", x"56", x"3e", x"4b", x"c6", x"d2", x"79", x"20", x"9a", x"db", x"c0", x"fe", x"78", x"cd", x"5a", x"f4",
-    x"1f", x"dd", x"a8", x"33", x"88", x"07", x"c7", x"31", x"b1", x"12", x"10", x"59", x"27", x"80", x"ec", x"5f",
-    x"60", x"51", x"7f", x"a9", x"19", x"b5", x"4a", x"0d", x"2d", x"e5", x"7a", x"9f", x"93", x"c9", x"9c", x"ef",
-    x"a0", x"e0", x"3b", x"4d", x"ae", x"2a", x"f5", x"b0", x"c8", x"eb", x"bb", x"3c", x"83", x"53", x"99", x"61",
-    x"17", x"2b", x"04", x"7e", x"ba", x"77", x"d6", x"26", x"e1", x"69", x"14", x"63", x"55", x"21", x"0c", x"7d"
+    x"52", x"09", x"6A", x"D5", x"30", x"36", x"A5", x"38", x"BF", x"40", x"A3", x"9E", x"81", x"F3", x"D7", x"FB",
+    x"7C", x"E3", x"39", x"82", x"9B", x"2F", x"FF", x"87", x"34", x"8E", x"43", x"44", x"C4", x"DE", x"E9", x"CB",
+    x"54", x"7B", x"94", x"32", x"A6", x"C2", x"23", x"3D", x"EE", x"4C", x"95", x"0B", x"42", x"FA", x"C3", x"4E",
+    x"08", x"2E", x"A1", x"66", x"28", x"D9", x"24", x"B2", x"76", x"5B", x"A2", x"49", x"6D", x"8B", x"D1", x"25",
+    x"72", x"F8", x"F6", x"64", x"86", x"68", x"98", x"16", x"D4", x"A4", x"5C", x"CC", x"5D", x"65", x"B6", x"92",
+    x"6C", x"70", x"48", x"50", x"FD", x"ED", x"B9", x"DA", x"5E", x"15", x"46", x"57", x"A7", x"8D", x"9D", x"84",
+    x"90", x"D8", x"AB", x"00", x"8C", x"BC", x"D3", x"0A", x"F7", x"E4", x"58", x"05", x"B8", x"B3", x"45", x"06",
+    x"D0", x"2C", x"1E", x"8F", x"CA", x"3F", x"0F", x"02", x"C1", x"AF", x"BD", x"03", x"01", x"13", x"8A", x"6B",
+    x"3A", x"91", x"11", x"41", x"4F", x"67", x"DC", x"EA", x"97", x"F2", x"CF", x"CE", x"F0", x"B4", x"E6", x"73",
+    x"96", x"AC", x"74", x"22", x"E7", x"AD", x"35", x"85", x"E2", x"F9", x"37", x"E8", x"1C", x"75", x"DF", x"6E",
+    x"47", x"F1", x"1A", x"71", x"1D", x"29", x"C5", x"89", x"6F", x"B7", x"62", x"0E", x"AA", x"18", x"BE", x"1B",
+    x"FC", x"56", x"3E", x"4B", x"C6", x"D2", x"79", x"20", x"9A", x"DB", x"C0", x"FE", x"78", x"CD", x"5A", x"F4",
+    x"1F", x"DD", x"A8", x"33", x"88", x"07", x"C7", x"31", x"B1", x"12", x"10", x"59", x"27", x"80", x"EC", x"5F",
+    x"60", x"51", x"7F", x"A9", x"19", x"B5", x"4A", x"0D", x"2D", x"E5", x"7A", x"9F", x"93", x"C9", x"9C", x"EF",
+    x"A0", x"E0", x"3B", x"4D", x"AE", x"2A", x"F5", x"B0", x"C8", x"EB", x"BB", x"3C", x"83", x"53", x"99", x"61",
+    x"17", x"2B", x"04", x"7E", x"BA", x"77", x"D6", x"26", x"E1", x"69", x"14", x"63", x"55", x"21", x"0C", x"7D"
   );
+
+  -- ShangMi s-box --
+  constant sm4_sbox_c : rom256x8_t := (
+    x"D6", x"90", x"E9", x"FE", x"CC", x"E1", x"3D", x"B7", x"16", x"B6", x"14", x"C2", x"28", x"FB", x"2C", x"05",
+    x"2B", x"67", x"9A", x"76", x"2A", x"BE", x"04", x"C3", x"AA", x"44", x"13", x"26", x"49", x"86", x"06", x"99",
+    x"9C", x"42", x"50", x"F4", x"91", x"EF", x"98", x"7A", x"33", x"54", x"0B", x"43", x"ED", x"CF", x"AC", x"62",
+    x"E4", x"B3", x"1C", x"A9", x"C9", x"08", x"E8", x"95", x"80", x"DF", x"94", x"FA", x"75", x"8F", x"3F", x"A6",
+    x"47", x"07", x"A7", x"FC", x"F3", x"73", x"17", x"BA", x"83", x"59", x"3C", x"19", x"E6", x"85", x"4F", x"A8",
+    x"68", x"6B", x"81", x"B2", x"71", x"64", x"DA", x"8B", x"F8", x"EB", x"0F", x"4B", x"70", x"56", x"9D", x"35",
+    x"1E", x"24", x"0E", x"5E", x"63", x"58", x"D1", x"A2", x"25", x"22", x"7C", x"3B", x"01", x"21", x"78", x"87",
+    x"D4", x"00", x"46", x"57", x"9F", x"D3", x"27", x"52", x"4C", x"36", x"02", x"E7", x"A0", x"C4", x"C8", x"9E",
+    x"EA", x"BF", x"8A", x"D2", x"40", x"C7", x"38", x"B5", x"A3", x"F7", x"F2", x"CE", x"F9", x"61", x"15", x"A1",
+    x"E0", x"AE", x"5D", x"A4", x"9B", x"34", x"1A", x"55", x"AD", x"93", x"32", x"30", x"F5", x"8C", x"B1", x"E3",
+    x"1D", x"F6", x"E2", x"2E", x"82", x"66", x"CA", x"60", x"C0", x"29", x"23", x"AB", x"0D", x"53", x"4E", x"6F",
+    x"D5", x"DB", x"37", x"45", x"DE", x"FD", x"8E", x"2F", x"03", x"FF", x"6A", x"72", x"6D", x"6C", x"5B", x"51",
+    x"8D", x"1B", x"AF", x"92", x"BB", x"DD", x"BC", x"7F", x"11", x"D9", x"5C", x"41", x"1F", x"10", x"5A", x"D8",
+    x"0A", x"C1", x"31", x"88", x"A5", x"CD", x"7B", x"BD", x"2D", x"74", x"D0", x"12", x"B8", x"E5", x"B4", x"B0",
+    x"89", x"69", x"97", x"4A", x"0C", x"96", x"77", x"7E", x"65", x"B9", x"F1", x"09", x"C5", x"6E", x"C6", x"84",
+    x"18", x"F0", x"7D", x"EC", x"3A", x"DC", x"4D", x"20", x"79", x"EE", x"5F", x"3E", x"D7", x"CB", x"39", x"48"
+  );
+
 
   -- ----------------------------------------------------------------------------------------
   -- helper functions
@@ -153,6 +178,14 @@ architecture neorv32_cpu_cp_crypto_rtl of neorv32_cpu_cp_crypto is
     return res_v;
   end function ror_f;
 
+  -- rotate left --
+  function rol_f(data : std_ulogic_vector(31 downto 0); shamt : natural range 0 to 31) return std_ulogic_vector is
+    variable res_v : std_ulogic_vector(31 downto 0);
+  begin
+    res_v := std_ulogic_vector(rotate_left(unsigned(data), shamt));
+    return res_v;
+  end function rol_f;
+
   -- multiply by 2 in Galois field (2^8) --
   function xt2_f(a : std_ulogic_vector(7 downto 0)) return std_ulogic_vector is
     variable res_v : std_ulogic_vector(7 downto 0);
@@ -191,8 +224,10 @@ architecture neorv32_cpu_cp_crypto_rtl of neorv32_cpu_cp_crypto is
   constant cmd_sha512_c : natural := 2;
   constant cmd_aesenc_c : natural := 3;
   constant cmd_aesdec_c : natural := 4;
+  constant cmd_sm3_c    : natural := 5;
+  constant cmd_sm4_c    : natural := 6;
   --
-  signal cmd : std_ulogic_vector(4 downto 0);
+  signal cmd       : std_ulogic_vector(6 downto 0);
   signal cmd_valid : std_ulogic;
 
   -- controller --
@@ -203,7 +238,7 @@ architecture neorv32_cpu_cp_crypto_rtl of neorv32_cpu_cp_crypto is
   signal rs2     : std_ulogic_vector(31 downto 0);
   signal funct12 : std_ulogic_vector(11 downto 0);
   signal funct3  : std_ulogic_vector(2 downto 0);
-  signal out_sel : std_ulogic_vector(1 downto 0);
+  signal out_sel : std_ulogic_vector(2 downto 0);
 
   -- aes core --
   type aes_t is record
@@ -219,11 +254,20 @@ architecture neorv32_cpu_cp_crypto_rtl of neorv32_cpu_cp_crypto is
   end record;
   signal aes : aes_t;
 
-  -- permutation core --
-  signal xperm_res, xperm4_res, xperm8_res : std_ulogic_vector(31 downto 0);
+  -- permutation core, sha core, sm3 core --
+  signal xperm_res, xperm4_res, xperm8_res, sha_res, sm3_res : std_ulogic_vector(31 downto 0);
 
-  -- sha core --
-  signal sha_res : std_ulogic_vector(31 downto 0);
+  -- ShangMi core --
+  type sm4_t is record
+    bs  : std_ulogic_vector(1 downto 0);
+    si  : std_ulogic_vector(7 downto 0);
+    so1 : std_ulogic_vector(7 downto 0);
+    so2 : std_ulogic_vector(31 downto 0);
+    rnd : std_ulogic_vector(31 downto 0);
+    rot : std_ulogic_vector(31 downto 0);
+    res : std_ulogic_vector(31 downto 0);
+  end record;
+  signal sm4 : sm4_t;
 
 begin
 
@@ -243,6 +287,12 @@ begin
 
   cmd(cmd_aesdec_c) <= '1' when EN_ZKND and (ctrl_i.ir_opcode(5) = '1') and (ctrl_i.ir_funct3 = "000") and
                                 (ctrl_i.ir_funct12(9 downto 7) = "101") and (ctrl_i.ir_funct12(5) = '1') else '0';
+
+  cmd(cmd_sm3_c)    <= '1' when EN_ZKSH and (ctrl_i.ir_opcode(5) = '0') and (ctrl_i.ir_funct3 = "001") and
+                                (ctrl_i.ir_funct12(11 downto 1) = "00010000100") else '0';
+
+  cmd(cmd_sm4_c)    <= '1' when EN_ZKSED and (ctrl_i.ir_opcode(5) = '1') and (ctrl_i.ir_funct3 = "000") and
+                                (ctrl_i.ir_funct12(9 downto 7) = "110") and (ctrl_i.ir_funct12(5) = '0') else '0';
 
   -- valid instruction? --
   cmd_valid <= '1' when (ctrl_i.alu_cp_alu = '1') and (or_reduce_f(cmd) = '1') else '0';
@@ -273,9 +323,9 @@ begin
         -- wait for operation trigger --
         when S_IDLE =>
           if (cmd_valid = '1') then -- trigger new operation
-            if (cmd(cmd_aesenc_c) = '1') or (cmd(cmd_aesdec_c) = '1') then
+            if (cmd(cmd_aesenc_c) = '1') or (cmd(cmd_aesdec_c) = '1') or (cmd(cmd_sm3_c) = '1') or (cmd(cmd_sm4_c) = '1') then
               state <= S_BUSY;
-            else -- super fast
+            else
               done  <= '1';
               state <= S_IDLE;
             end if;
@@ -305,16 +355,22 @@ begin
       res_o <= (others => '0'); -- default
       if (done = '1') then
         case out_sel is
-          when "00"   => res_o <= xperm_res;
-          when "01"   => res_o <= aes.res;
-          when others => res_o <= sha_res;
+          when "100"  => res_o <= xperm_res;
+          when "101"  => res_o <= aes.res;
+          when "110"  => res_o <= sm4.res;
+          when others =>
+            if EN_ZKSH and (ctrl_i.ir_opcode(5) = '0') and (funct12(3) = '1') then
+              res_o <= sm3_res;
+            else
+              res_o <= sha_res;
+            end if;
         end case;
       end if;
     end if;
   end process result;
 
   -- function unit select --
-  out_sel <= funct12(8) & funct12(5);
+  out_sel <= funct12(9 downto 8) & funct12(5);
 
 
   -- Crossbar Permutations ------------------------------------------------------------------
@@ -395,15 +451,15 @@ begin
       rs2(31 downto 24) when others;
 
     -- s-box look-up --
-    sbox_lookup: process(clk_i)
+    aes_sbox_lookup: process(clk_i)
     begin
       if rising_edge(clk_i) then -- ROM access; try to infer memory primitives
-        aes.so <= sbox_c(to_integer(unsigned(aes.dec & aes.si))); -- aes.dec = 0 -> fwd-s-box, aes.dec = 1 -> inv-s-box
+        aes.so <= aes_sbox_c(to_integer(unsigned(aes.dec & aes.si))); -- aes.dec = 0 -> fwd-s-box, aes.dec = 1 -> inv-s-box
       end if;
-    end process sbox_lookup;
+    end process aes_sbox_lookup;
 
     -- mix columns --
-    mix_columns: process(rstn_i, clk_i)
+    aes_mix_columns: process(rstn_i, clk_i)
     begin
       if (rstn_i = '0') then
         aes.mix1 <= (others => '0');
@@ -420,7 +476,7 @@ begin
           aes.mix1(07 downto 00) <= gfmul_f(aes.so, x"2");
         end if;
       end if;
-    end process mix_columns;
+    end process aes_mix_columns;
 
     -- middle / final round --
     aes.mix2 <= aes.mix1 when (aes.mid = '1') else x"000000" & aes.so;
@@ -446,6 +502,86 @@ begin
     aes.mix1 <= (others => '0');
     aes.mix2 <= (others => '0');
     aes.res  <= (others => '0');
+  end generate;
+
+
+  -- ShangMi Hash Functions -----------------------------------------------------------------
+  -- -------------------------------------------------------------------------------------------
+  sm3_enabled:
+  if EN_ZKSH generate
+    sm3_res <= (rs1 xor rol_f(rs1,  9) xor rol_f(rs1, 17)) when (funct12(0) = '0') else
+               (rs1 xor rol_f(rs1, 15) xor rol_f(rs1, 23));
+  end generate;
+
+  sm3_disabled:
+  if not EN_ZKSH generate
+    sm3_res <= (others => '0');
+  end generate;
+
+
+  -- ShangMi Encryption/Decryption ----------------------------------------------------------
+  -- -------------------------------------------------------------------------------------------
+  sm4_enabled:
+  if EN_ZKSED generate
+
+    -- operation select --
+    sm4.bs <= funct12(11 downto 10); -- byte select
+
+    -- select byte from rs2 --
+    with sm4.bs select sm4.si <=
+      rs2(07 downto 00) when "00",
+      rs2(15 downto 08) when "01",
+      rs2(23 downto 16) when "10",
+      rs2(31 downto 24) when others;
+
+    -- s-box look-up --
+    sm4_sbox_lookup: process(clk_i)
+    begin
+      if rising_edge(clk_i) then -- ROM access; try to infer memory primitives
+        sm4.so1 <= sm4_sbox_c(to_integer(unsigned(sm4.si)));
+      end if;
+    end process sm4_sbox_lookup;
+
+    -- zero-extend --
+    sm4.so2 <= x"000000" & sm4.so1;
+
+    -- encrypt/decrypt or key schedule round --
+    sm4_schedule: process(rstn_i, clk_i)
+    begin
+      if (rstn_i = '0') then
+        sm4.rnd <= (others => '0');
+      elsif rising_edge(clk_i) then
+        if (funct12(6) = '0') then -- encrypt/decrypt
+          sm4.rnd <= sm4.so2 xor lsl_f(sm4.so2, 8) xor lsl_f(sm4.so2, 2) xor lsl_f(sm4.so2, 18) xor
+                                 lsl_f((sm4.so2 and x"0000003F"), 26) xor lsl_f((sm4.so2 and x"000000C0"), 10);
+        else -- key schedule
+          sm4.rnd <= sm4.so2 xor lsl_f((sm4.so2 and x"00000007"), 29) xor lsl_f((sm4.so2 and x"000000FE"),  7) xor
+                                 lsl_f((sm4.so2 and x"00000001"), 23) xor lsl_f((sm4.so2 and x"000000F8"), 13);
+        end if;
+      end if;
+    end process sm4_schedule;
+
+    -- rotate left by multiples of 8 --
+    with sm4.bs select sm4.rot <=
+      sm4.rnd(31 downto 0)                         when "00",
+      sm4.rnd(23 downto 0) & sm4.rnd(31 downto 24) when "01",
+      sm4.rnd(15 downto 0) & sm4.rnd(31 downto 16) when "10",
+      sm4.rnd(07 downto 0) & sm4.rnd(31 downto 08) when others;
+
+    -- final XOR --
+    sm4.res <= rs1 xor sm4.rot;
+
+  end generate;
+
+  sm4_disabled:
+  if not EN_ZKSED generate
+    sm4.bs  <= (others => '0');
+    sm4.si  <= (others => '0');
+    sm4.so1 <= (others => '0');
+    sm4.so2 <= (others => '0');
+    sm4.rnd <= (others => '0');
+    sm4.rot <= (others => '0');
+    sm4.res <= (others => '0');
   end generate;
 
 
