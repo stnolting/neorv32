f0611c4e436b76e595c600c1fe2c80688cbe0261
stnolting <stnolting@gmail.com>
2024-10-02 20:53:41 +0200

    :warning: [cfu] rework CFU handshake interface

--- rtl/core/neorv32_cpu_cp_cfu.vhd
+++ rtl/core/neorv32_cpu_cp_cfu.vhd
@@ -1,9 +1,9 @@
 -- ================================================================================ --
 -- NEORV32 CPU - Co-Processor: Custom (RISC-V Instructions) Functions Unit (CFU)    --
 -- -------------------------------------------------------------------------------- --
--- For custom/user-defined RISC-V instructions See the  CPU's documentation for     --
--- more information. Also take a look at the "software-counterpart" this default    --
--- CFU hardware in 'sw/example/demo_cfu'.                                           --
+-- For custom/user-defined RISC-V instructions. See the CPU's documentation for     --
+-- more information. Also take a look at the "software-counterpart" of this default --
+-- CFU hardware example in 'sw/example/demo_cfu'.                                   --
 -- -------------------------------------------------------------------------------- --
 -- The NEORV32 RISC-V Processor - https://github.com/stnolting/neorv32              --
 -- Copyright (c) NEORV32 contributors.                                              --
@@ -26,22 +26,21 @@ entity neorv32_cpu_cp_cfu is
     active_i    : in std_ulogic; -- operation in progress, CPU is waiting for CFU
     -- CSR interface --
     csr_we_i    : in  std_ulogic; -- write enable
-    csr_addr_i  : in  std_ulogic_vector(1 downto 0); -- address
+    csr_addr_i  : in  std_ulogic_vector(1 downto 0); -- address (CSR address 0x800 to 0x803)
     csr_wdata_i : in  std_ulogic_vector(31 downto 0); -- write data
-    csr_rdata_o : out std_ulogic_vector(31 downto 0) := (others => '0'); -- read data
-    -- operands --
-    rtype_i     : in std_ulogic; -- instruction type (R3-type or R4-type)
+    csr_rdata_o : out std_ulogic_vector(31 downto 0); -- read data
+    -- operands (form/via custom instruction word) --
+    rtype_i     : in std_ulogic; -- instruction type (R3-type or R4-type); from instruction word's "opcode[5]" bit
     funct3_i    : in std_ulogic_vector(2 downto 0); -- "funct3" bit-field from custom instruction word
     funct7_i    : in std_ulogic_vector(6 downto 0); -- "funct7" bit-field from custom instruction word
-    rs1_i       : in  std_ulogic_vector(31 downto 0); -- rf source 1
-    rs2_i       : in  std_ulogic_vector(31 downto 0); -- rf source 2
-    rs3_i       : in  std_ulogic_vector(31 downto 0); -- rf source 3
+    rs1_i       : in  std_ulogic_vector(31 downto 0); -- rf source 1 via "rs1" bit-field from custom instruction word
+    rs2_i       : in  std_ulogic_vector(31 downto 0); -- rf source 2 via "rs2" bit-field from custom instruction word
+    rs3_i       : in  std_ulogic_vector(31 downto 0); -- rf source 3 via "rs3" bit-field from custom instruction word
     -- result and status --
-    result_o    : out std_ulogic_vector(31 downto 0) := (others => '0'); -- operation result
-    valid_o     : out std_ulogic := '0' -- result valid, operation done; set one cycle before result_o is valid
+    result_o    : out std_ulogic_vector(31 downto 0); -- operation result
+    valid_o     : out std_ulogic -- result valid, operation done; set one cycle before result_o is valid
   );
 end neorv32_cpu_cp_cfu;
-
 
   -- **************************************************************************************************************************
   -- CFU Interface Documentation
@@ -81,30 +80,28 @@ end neorv32_cpu_cp_cfu;
   -- > active_i (input,   1-bit): operation in progress while (optional signal)
   --
   -- > result_o (output, 32-bit): processing result
-  -- > valid_o  (output,  1-bit): set high when processing is done
+  -- > valid_o  (output,  1-bit): set high (for one cycle) when processing is done
   --
   -- The start of a new CFU operation is indicated by <start_i> being high for exactly one cycle. The CFU may operate while
   -- <active_i> is high and should terminate (and reset) all internal operations when it clears again. However, using
   -- <active_i> is optional. When the CFU has completed processing, the data returned via <result_o> will be written to the
   -- CPU's register file (indexed by the 'rd' bit-field).
   --
-  -- [TIP] For complex CFU designs it is highly recommended to register <result_o> in order to keep the CPU's critical
-  --       path as short as possible.
-  --
   -- The <valid_o> signal is used to signal the completion of the CFU operation. For pure-combinatorial instructions
   -- (completing within 1 clock cycle) <valid_o> can be hardwired to 1. If the CFU requires several clock cycles for
-  -- completion the <valid_o> signal has to be set exactly ONE CYCLE BEFORE <result_o> is valid.
+  -- completion the <valid_o> signal has to be set when <result_o> is valid. Otherwise, <valid_o> should be cleared.
+  -- However, the CPU execution logic will evaluate <valid_o> only while <active_i> is high.
   --
-  -- Example interface timing for a multi-cycle CFU operation:
-  -- clk_i    ____/----\____/----\____/----\____/----\____/----\____
-  -- start_i  ____/---------\_______________________________________ trigger is high for one cycle
-  -- active_i ____/-----------------------------\___________________ cease processing when low
-  -- valid_o  ....\___________________/---------\................... set one cycle before output phase, zero during processing
-  -- result_o ..................................|DDDDDDDDD|......... don't care ('.') except for output phase ('D')
+  -- Example interface timing for a multi-cycle CFU operation (with 2 cycles processing latency):
+  -- clk_i    ____/----\____/----\____/----\____/----\____
+  -- start_i  ____/---------\_____________________________ trigger is high for one cycle
+  -- active_i ____/-----------------------------\_________ cease processing when low
+  -- valid_o  ________________________/---------\_________ set for one cycle when operation is done / result is valid
+  -- result_o ........................|DDDDDDDDD|......... don't care ('.') except for valid-output phase ('D')
   --
-  -- [NOTE] If the <valid_o> signal is not set within a bound time window (default = 512 cycles; see "monitor_mc_tmo_c"
-  --        constant in the main NEORV32 package file) the CFU operation is automatically terminated by the hardware
-  --        (clearing <active_i>) and an illegal instruction exception is raised.
+  -- [NOTE] If the <valid_o> signal is not set within a bound time window after the CFU has been triggered via <start_i>
+  --        (default = 512 cycles; see "monitor_mc_tmo_c" constant in the main NEORV32 package file) the CFU operation
+  --        is automatically terminated by the hardware (clearing <active_i>) and an illegal instruction exception is raised.
 
   -- ----------------------------------------------------------------------------------------
   -- CFU-Internal Control and Status Registers (CFU-CSRs)
@@ -115,12 +112,12 @@ end neorv32_cpu_cp_cfu;
   -- > csr_rdata_i (output, 32-bit): CSR read data
   --
   -- The CFU provides four directly accessible CSR addresses for implementing custom control and status register inside the CFU.
-  --  These registers can be used to pass further operands, to check the unit's status or to configure operation modes.
+  -- These are accessed via the global CSR addresses 0x800 to 0x803. The CFU-exclusive CSRs can be used to pass further operands,
+  -- to check the unit's status or to configure additional operation modes.
   --
   -- [TIP] If more than four CFU-internal CSRs are required the designer can implement an "indirect access mechanism" based
   --       on just two of the default CSRs: one CSR is used to configure the index while the other is used as an alias to
   --       exchange data with the indexed CFU-internal CSR.
-
 
   -- **************************************************************************************************************************
   -- Actual CFU User Logic Example: XTEA - Extended Tiny Encryption Algorithm (replace this with your custom logic)
@@ -156,7 +153,7 @@ architecture neorv32_cpu_cp_cfu_rtl of neorv32_cpu_cp_cfu is
 
   -- processing logic --
   type xtea_t is record
-    done : std_ulogic; -- multi-cycle done shift register
+    done : std_ulogic_vector(1 downto 0); -- multi-cycle done shift register; 2 stages = 2 cyles latency
     opa  : std_ulogic_vector(31 downto 0); -- input operand a
     opb  : std_ulogic_vector(31 downto 0); -- input operand b
     sum  : std_ulogic_vector(31 downto 0); -- round key buffer
@@ -192,23 +189,25 @@ begin
   xtea_core: process(rstn_i, clk_i)
   begin
     if (rstn_i = '0') then
-      xtea.done <= '0';
+      xtea.done <= (others => '0');
       xtea.opa  <= (others => '0');
       xtea.opb  <= (others => '0');
       xtea.sum  <= (others => '0');
       xtea.res  <= (others => '0');
     elsif rising_edge(clk_i) then
+      -- "operation-done" shift register: module has 2 cycles latency --
+      xtea.done(0) <= '0'; -- default: no operation trigger
+      xtea.done(1) <= xtea.done(0);
 
       -- trigger new operation --
-      xtea.done <= '0'; -- default
       if (start_i = '1') and (rtype_i = r3type_c) then -- execution trigger and correct instruction type
-        xtea.opa  <= rs1_i; -- buffer input operand rs1 (for improved physical timing)
-        xtea.opb  <= rs2_i; -- buffer input operand rs2 (for improved physical timing)
-        xtea.done <= '1'; -- result is available in the 2nd cycle
+        xtea.opa     <= rs1_i; -- buffer input operand rs1 (for improved physical timing)
+        xtea.opb     <= rs2_i; -- buffer input operand rs2 (for improved physical timing)
+        xtea.done(0) <= '1'; -- trigger operation
       end if;
 
       -- data processing --
-      if (xtea.done = '1') then -- second-stage execution trigger
+      if (xtea.done(0) = '1') then -- second-stage execution trigger
         -- update "sum" round key --
         if (funct3_i(2) = '1') then -- initialize
           xtea.sum <= xtea.opa; -- set initial round key
@@ -247,7 +246,7 @@ begin
       case funct3_i is -- just check "funct3" here; "funct7" bit-field is ignored in this example
         when xtea_enc_v0_c | xtea_enc_v1_c | xtea_dec_v0_c | xtea_dec_v1_c => -- xtea encryption/decryption
           result_o <= xtea.res; -- processing result
-          valid_o  <= xtea.done; -- multi-cycle processing done when set
+          valid_o  <= xtea.done(1); -- multi-cycle processing done when set
         when xtea_init_c => -- xtea initialization
           result_o <= (others => '0'); -- just output zero
           valid_o  <= '1'; -- pure-combinatorial, so we are done "immediately"
