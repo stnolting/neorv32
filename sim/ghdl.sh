9644bce38a0440bf0941889ca7bd920a6f637796
stnolting <stnolting@gmail.com>
2024-11-01 07:31:05 +0100

    [sim] adjust sub-script calling

--- sim/ghdl.sh
+++ sim/ghdl.sh
@@ -6,7 +6,7 @@ set -e
 cd $(dirname "$0")
 
 # Setup simulation
-./ghdl.setup.sh
+/bin/bash ghdl.setup.sh
 
 # Run simulation (pass down more than 1 parameter to GHDL)
-./ghdl.run.sh $@
+/bin/bash ghdl.run.sh $@
