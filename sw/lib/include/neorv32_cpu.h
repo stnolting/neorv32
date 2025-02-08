3c9461d79ab53ebc3ffd1ae1f1e4863a003a3c66
stnolting <stnolting@gmail.com>
2025-02-08 09:55:04 +0100

    [sw/lib] add LR/SC wrapper functions

--- sw/lib/include/neorv32_cpu.h
+++ sw/lib/include/neorv32_cpu.h
@@ -1,7 +1,7 @@
 // ================================================================================ //
 // The NEORV32 RISC-V Processor - https://github.com/stnolting/neorv32              //
 // Copyright (c) NEORV32 contributors.                                              //
-// Copyright (c) 2020 - 2024 Stephan Nolting. All rights reserved.                  //
+// Copyright (c) 2020 - 2025 Stephan Nolting. All rights reserved.                  //
 // Licensed under the BSD-3-Clause license, see LICENSE for details.                //
 // SPDX-License-Identifier: BSD-3-Clause                                            //
 // ================================================================================ //
@@ -9,8 +9,6 @@
 /**
  * @file neorv32_cpu.h
  * @brief CPU Core Functions HW driver header file.
- *
- * @see https://stnolting.github.io/neorv32/sw/files.html
  */
 
 #ifndef neorv32_cpu_h
@@ -227,6 +225,61 @@ inline void __attribute__ ((always_inline)) neorv32_cpu_csr_clr(const int csr_id
 inline void __attribute__ ((always_inline)) neorv32_cpu_sleep(void) {
 
   asm volatile ("wfi");
+}
+
+
+/**********************************************************************//**
+ * Atomic memory access: load-reservate word.
+ *
+ * @note The address has to be word-aligned - otherwise an alignment exception will be raised.
+ * @warning This function requires the A/Zalrsc ISA extension.
+ *
+ * @param[in] addr Address (32-bit).
+ * @return Read data word (32-bit).
+ **************************************************************************/
+inline uint32_t __attribute__ ((always_inline)) neorv32_cpu_amolr(uint32_t addr) {
+
+#if defined __riscv_atomic
+  uint32_t amo_addr = addr;
+  uint32_t amo_rdata;
+
+  asm volatile ("lr.w %[dst], 0(%[addr])" : [dst] "=r" (amo_rdata) : [addr] "r" (amo_addr));
+
+  return amo_rdata;
+#else
+  (void)addr;
+
+  return 0;
+#endif
+}
+
+
+/**********************************************************************//**
+ * Atomic memory access: store-conditional word.
+ *
+ * @note The address has to be word-aligned - otherwise an alignment exception will be raised.
+ * @warning This function requires the A/Zalrsc ISA extension.
+ *
+ * @param[in] addr Address (32-bit).
+ * @param[in] wdata Data word to-be-written conditionally (32-bit).
+ * @return Status: 0 = ok, 1 = failed (32-bit).
+ **************************************************************************/
+inline uint32_t __attribute__ ((always_inline)) neorv32_cpu_amosc(uint32_t addr, uint32_t wdata) {
+
+#if defined __riscv_atomic
+  uint32_t amo_addr  = addr;
+  uint32_t amo_wdata = wdata;
+  uint32_t amo_status;
+
+  asm volatile ("sc.w %[dst], %[src], (%[addr])" : [dst] "=r" (amo_status) : [src] "r" (amo_wdata), [addr] "r" (amo_addr));
+
+  return amo_status;
+#else
+  (void)addr;
+  (void)wdata;
+
+  return 1; // always fail
+#endif
 }
 
 
