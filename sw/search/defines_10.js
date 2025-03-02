8eafc23c8fef2fd15d822df26c171ff3cdfd7c87
GHA <push@gha>
2025-03-02 14:33:14 +0000

    update 449dd877662c25df08064fbf82a2488e7c5e8c80

---
+++ sw/search/defines_10.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['xstr_0',['xstr',['../bootloader_8c.html#aff6b53dd58cea43d24393a9a2f79e772',1,'bootloader.c']]],
+  ['xtea_5frounds_1',['XTEA_ROUNDS',['../demo__cfu_2main_8c.html#a36801eebef583b6c3438709144834e3e',1,'main.c']]]
+];
---
+++ sw/search/defines_5.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['gen_5fdelay_0',['GEN_DELAY',['../game__of__life_2main_8c.html#a3f33bf77abbf35c245956489f8f7e3a7',1,'main.c']]],
+  ['gpio_5ffirq_5fenable_1',['GPIO_FIRQ_ENABLE',['../neorv32_8h.html#a94f61a4b2f6624a3b9bcc65e75ec3c46',1,'neorv32.h']]],
+  ['gpio_5ffirq_5fpending_2',['GPIO_FIRQ_PENDING',['../neorv32_8h.html#a777e69dd3df03bbe21d1c307e0f76d30',1,'neorv32.h']]],
+  ['gpio_5frte_5fid_3',['GPIO_RTE_ID',['../neorv32_8h.html#a85fe61bb25e4c5161e9df3671d12735b',1,'neorv32.h']]],
+  ['gpio_5ftrap_5fcode_4',['GPIO_TRAP_CODE',['../neorv32_8h.html#ae15802149fc0ad8be82b6d6127b44d63',1,'neorv32.h']]],
+  ['gptmr_5ffirq_5fenable_5',['GPTMR_FIRQ_ENABLE',['../neorv32_8h.html#a0a59186908b498cb92273b7767966e26',1,'neorv32.h']]],
+  ['gptmr_5ffirq_5fpending_6',['GPTMR_FIRQ_PENDING',['../neorv32_8h.html#a4a3164d123ddddd07750f868e67d82d8',1,'neorv32.h']]],
+  ['gptmr_5frte_5fid_7',['GPTMR_RTE_ID',['../neorv32_8h.html#aac38deeef36de0a474ab238a3054f860',1,'neorv32.h']]],
+  ['gptmr_5ftrap_5fcode_8',['GPTMR_TRAP_CODE',['../neorv32_8h.html#a445543c36d1f78477aa8ea4841c720a1',1,'neorv32.h']]]
+];
