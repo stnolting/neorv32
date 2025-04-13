6088aeeaf30aff91a33644211940ba1b871f5de3
stnolting <stnolting@gmail.com>
2025-04-13 08:06:33 +0200

    [processor_check] add build info

--- sw/example/processor_check/main.c
+++ sw/example/processor_check/main.c
@@ -174,6 +174,7 @@ int main() {
   // -----------------------------------------------
   neorv32_aux_print_logo(); // show NEORV32 ASCII logo
   neorv32_aux_print_about(); // show project credits
+  PRINT_STANDARD("Build: %s "__DATE__" "__TIME__"\n", NEORV32_GIT_TAG);
   neorv32_aux_print_hw_config(); // show full hardware configuration report
 
 
