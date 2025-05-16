c2749cd6b27f42c54969127ad8ff9ef0c958a479
stnolting <stnolting@gmail.com>
2025-05-16 20:00:21 +0200

    [smp] rework dual-core boot

--- sw/lib/include/neorv32_smp.h
+++ sw/lib/include/neorv32_smp.h
@@ -14,71 +14,19 @@
 #ifndef NEORV32_SMP_H
 #define NEORV32_SMP_H
 
-
 /**********************************************************************//**
  * @name Prototypes
  **************************************************************************/
 /**@{*/
-int      neorv32_smp_launch(int (*entry_point)(void), uint8_t* stack_memory, size_t stack_size_bytes);
-void     neorv32_smp_icc_push(uint32_t data);
-uint32_t neorv32_smp_icc_pop(void);
+int neorv32_smp_launch(int (*entry_point)(void), uint8_t* stack_memory, size_t stack_size_bytes);
 /**@}*/
-
 
 /**********************************************************************//**
  * Get core/hart ID of the CPU that is executing this function.
- *
  * @return Core ID from mhartid CSR.
  **************************************************************************/
 inline uint32_t __attribute__ ((always_inline)) neorv32_smp_whoami(void) {
-
   return neorv32_cpu_csr_read(CSR_MHARTID);
-}
-
-
-/**********************************************************************//**
- * Get data from other core via ICC link.
- * Check link status before #neorv32_smp_icc_avail().
- *
- * @return Data word (32-bit) received from other core.
- **************************************************************************/
-inline uint32_t __attribute__ ((always_inline)) neorv32_smp_icc_get(void) {
-
-  return neorv32_cpu_csr_read(CSR_MXICCDATA);
-}
-
-
-/**********************************************************************//**
- * Send data to other core via ICC link.
- * Check link status before #neorv32_smp_icc_free().
- *
- * @param[in] data Data word (32-bit) to be send to other core.
- **************************************************************************/
-inline void __attribute__ ((always_inline)) neorv32_smp_icc_put(uint32_t data) {
-
-  neorv32_cpu_csr_write(CSR_MXICCDATA, data);
-}
-
-
-/**********************************************************************//**
- * Check if ICC link data is available.
- *
- * @return 0 = no data available, nonzero = data available.
- **************************************************************************/
-inline int __attribute__ ((always_inline)) neorv32_smp_icc_avail(void) {
-
-  return neorv32_cpu_csr_read(CSR_MXICCSREG) & (1 << CSR_MXICCSREG_RX_AVAIL);
-}
-
-
-/**********************************************************************//**
- * Check if free space in ICC link.
- *
- * @return 0 = no free space available, nonzero = free space available.
- **************************************************************************/
-inline int __attribute__ ((always_inline)) neorv32_smp_icc_free(void) {
-
-  return neorv32_cpu_csr_read(CSR_MXICCSREG) & (1 << CSR_MXICCSREG_TX_FREE);
 }
 
 #endif // NEORV32_SMP_H
