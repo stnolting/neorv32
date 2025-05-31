8297c976e106a512db13cbf8aee5e752de643367
GHA <push@gha>
2025-05-31 17:31:04 +0000

    update 433178b204dca9876789b5ee2a1095f41dc5dbee

---
+++ sw/search/variables_b.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['seconds_0',['seconds',['../structdate__t.html#a1071948e7754cb415da9714d59a315bf',1,'date_t']]],
+  ['soc_1',['SOC',['../structneorv32__sysinfo__t.html#ac43abe3b416da3a2895d1d06537a14c3',1,'neorv32_sysinfo_t']]]
+];
