c80bde87929a33ec760d78f6f47078e7cd0487a2
GHA <push@gha>
2025-06-07 19:55:35 +0000

    update 124975646396ab9e3ec441b49ab0aacaaa6a2fb3

---
+++ sw/search/enums_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['spi_5fflash_5fcmd_5fenum_0',['SPI_FLASH_CMD_enum',['../spi__flash_8c.html#aa5e5723e4cbf83805c3b1197cd401fed',1,'spi_flash.c']]],
+  ['spi_5fflash_5fsreg_5fenum_1',['SPI_FLASH_SREG_enum',['../spi__flash_8c.html#a97299f563ebc1fca4c663b025a15136f',1,'spi_flash.c']]]
+];
---
+++ sw/search/enumvalues_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['flash_5fsreg_5fbusy_0',['FLASH_SREG_BUSY',['../spi__flash_8c.html#a97299f563ebc1fca4c663b025a15136fa133cd87673ce9a612da494583e03dae9',1,'spi_flash.c']]],
+  ['flash_5fsreg_5fwel_1',['FLASH_SREG_WEL',['../spi__flash_8c.html#a97299f563ebc1fca4c663b025a15136fa22e8c983d74430ea0930e37781e88bf9',1,'spi_flash.c']]]
+];
