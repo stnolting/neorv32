6e9c351b2d18d128aecfad71ec973a5a9e0fe316
stnolting <stnolting@gmail.com>
2025-06-18 21:51:24 +0200

    [coremark] add experimental multi-threading configuration

--- sw/example/coremark/include/core_portme.h
+++ sw/example/coremark/include/core_portme.h
@@ -19,61 +19,58 @@ Original Author: Shay Gal-on
 /* Modified for the NEORV32 Processor - 2025, Stephan Nolting */
 
 /* Topic : Description
-        This file contains configuration constants required to execute on
-   different platforms
-*/
+ * This file contains configuration constants required to execute on different platforms
+ */
 #ifndef CORE_PORTME_H
 #define CORE_PORTME_H
 
 #include <stdlib.h>
 #include <neorv32.h>
 
-/************************/
-/* NEORV32-specific */
-/************************/
 #define BAUD_RATE  (19200)
 #define ITERATIONS (2000)
 
-/************************/
-/* Data types and settings */
-/************************/
 /* Configuration : HAS_FLOAT
-        Define to 1 if the platform supports floating point.
-*/
+ * Define to 1 if the platform supports floating point.
+ */
 #ifndef HAS_FLOAT
 #define HAS_FLOAT 1
 #endif
+
 /* Configuration : HAS_TIME_H
-        Define to 1 if platform has the time.h header file,
-        and implementation of functions thereof.
-*/
+ *Define to 1 if platform has the time.h header file,
+ *and implementation of functions thereof.
+ */
 #ifndef HAS_TIME_H
 #define HAS_TIME_H 1
 #endif
+
 /* Configuration : USE_CLOCK
-        Define to 1 if platform has the time.h header file,
-        and implementation of functions thereof.
-*/
+ * Define to 1 if platform has the time.h header file,
+ * and implementation of functions thereof.
+ */
 #ifndef USE_CLOCK
 #define USE_CLOCK 1
 #endif
+
 /* Configuration : HAS_STDIO
-        Define to 1 if the platform has stdio.h.
-*/
+ * Define to 1 if the platform has stdio.h.
+ */
 #ifndef HAS_STDIO
 #define HAS_STDIO 1
 #endif
+
 /* Configuration : HAS_PRINTF
-        Define to 1 if the platform has stdio.h and implements the printf
-   function.
-*/
+ * Define to 1 if the platform has stdio.h and implements the printf
+ * function.
+ */
 #ifndef HAS_PRINTF
 #define HAS_PRINTF 1
 #endif
 
 /* Definitions : COMPILER_VERSION, COMPILER_FLAGS, MEM_LOCATION
-        Initialize these strings per platform
-*/
+ * Initialize these strings per platform
+ */
 #ifndef COMPILER_VERSION
 #ifdef __GNUC__
 #define COMPILER_VERSION "GCC"__VERSION__
@@ -89,13 +86,11 @@ Original Author: Shay Gal-on
 #endif
 
 /* Data Types :
-        To avoid compiler issues, define the data types that need ot be used for
-   8b, 16b and 32b in <core_portme.h>.
-
-        *Important* :
-        ee_ptr_int needs to be the data type used to hold pointers, otherwise
-   coremark may fail!!!
-*/
+ * To avoid compiler issues, define the data types that need to be used for 8b, 16b and 32b in <core_portme.h>.
+ *
+ * *Important* :
+ * ee_ptr_int needs to be the data type used to hold pointers, otherwise coremark may fail!!!
+ */
 typedef int16_t       ee_s16;
 typedef uint16_t      ee_u16;
 typedef int32_t       ee_s32;
@@ -106,101 +101,100 @@ typedef uint64_t      ee_u64;
 typedef ee_u32        ee_ptr_int;
 typedef size_t        ee_size_t;
 #define NULL ((void *)0)
+
 /* align_mem :
-        This macro is used to align an offset to point to a 32b value. It is
-   used in the Matrix algorithm to initialize the input memory blocks.
-*/
+ * This macro is used to align an offset to point to a 32b value. It is
+ * used in the Matrix algorithm to initialize the input memory blocks.
+ */
 #define align_mem(x) (void *)(4 + (((ee_ptr_int)(x)-1) & ~3))
 
 /* Configuration : CORE_TICKS
-        Define type of return from the timing functions.
+ * Define type of return from the timing functions.
  */
 #define CORETIMETYPE ee_u64
 typedef ee_u64 CORE_TICKS;
 
 /* Configuration : SEED_METHOD
-        Defines method to get seed values that cannot be computed at compile
-   time.
-
-        Valid values :
-        SEED_ARG - from command line.
-        SEED_FUNC - from a system function.
-        SEED_VOLATILE - from volatile variables.
-*/
+ * Defines method to get seed values that cannot be computed at compile time.
+ *
+ * Valid values :
+ * SEED_ARG - from command line.
+ * SEED_FUNC - from a system function.
+ * SEED_VOLATILE - from volatile variables.
+ */
 #ifndef SEED_METHOD
 #define SEED_METHOD SEED_VOLATILE
 #endif
 
 /* Configuration : MEM_METHOD
-        Defines method to get a block of memory.
-
-        Valid values :
-        MEM_MALLOC - for platforms that implement malloc and have malloc.h.
-        MEM_STATIC - to use a static memory array.
-        MEM_STACK - to allocate the data block on the stack (NYI).
-*/
+ * Defines method to get a block of memory.
+ *
+ * Valid values :
+ * MEM_MALLOC - for platforms that implement malloc and have malloc.h.
+ * MEM_STATIC - to use a static memory array.
+ * MEM_STACK - to allocate the data block on the stack (NYI).
+ */
 #ifndef MEM_METHOD
-#define MEM_METHOD MEM_STATIC
+#define MEM_METHOD MEM_MALLOC
 #endif
 
 /* Configuration : MULTITHREAD
-        Define for parallel execution
-
-        Valid values :
-        1 - only one context (default).
-        N>1 - will execute N copies in parallel.
-
-        Note :
-        If this flag is defined to more then 1, an implementation for launching
-   parallel contexts must be defined.
-
-        Two sample implementations are provided. Use <USE_PTHREAD> or <USE_FORK>
-   to enable them.
-
-        It is valid to have a different implementation of <core_start_parallel>
-   and <core_end_parallel> in <core_portme.c>, to fit a particular architecture.
-*/
+ * Define for parallel execution
+ *
+ * Valid values :
+ * 1 - only one context (default).
+ * N>1 - will execute N copies in parallel.
+ *
+ * Note :
+ * If this flag is defined to more then 1, an implementation for launching
+ * parallel contexts must be defined.
+ *
+ * Two sample implementations are provided. Use <USE_PTHREAD> or <USE_FORK> to enable them.
+  *
+ * It is valid to have a different implementation of <core_start_parallel>
+ *and <core_end_parallel> in <core_portme.c>, to fit a particular architecture.
+ */
 #ifndef MULTITHREAD
 #define MULTITHREAD 1
-#define USE_PTHREAD 0
-#define USE_FORK    0
-#define USE_SOCKET  0
 #endif
 
 /* Configuration : MAIN_HAS_NOARGC
-        Needed if platform does not support getting arguments to main.
-
-        Valid values :
-        0 - argc/argv to main is supported
-        1 - argc/argv to main is not supported
-
-        Note :
-        This flag only matters if MULTITHREAD has been defined to a value
-   greater then 1.
-*/
+ * Needed if platform does not support getting arguments to main.
+ *
+ * Valid values :
+ * 0 - argc/argv to main is supported
+ * 1 - argc/argv to main is not supported
+ *
+ * Note :
+ * This flag only matters if MULTITHREAD has been defined to a value greater then 1.
+ */
 #ifndef MAIN_HAS_NOARGC
 #define MAIN_HAS_NOARGC 0
 #endif
 
 /* Configuration : MAIN_HAS_NORETURN
-        Needed if platform does not support returning a value from main.
-
-        Valid values :
-        0 - main returns an int, and return value will be 0.
-        1 - platform does not support returning a value from main
-*/
+ * Needed if platform does not support returning a value from main.
+ *
+ * Valid values :
+ * 0 - main returns an int, and return value will be 0.
+ * 1 - platform does not support returning a value from main
+ */
 #ifndef MAIN_HAS_NORETURN
 #define MAIN_HAS_NORETURN 0
 #endif
 
 /* Variable : default_num_contexts
-        Not used for this simple port, must contain the value 1.
-*/
-extern ee_u32 default_num_contexts;
+ * Not used for this simple port, must contain the value 1.
+ */
+extern int default_num_contexts;
+
+#if (MULTITHREAD > 1)
+#define PARALLEL_METHOD "Proprietary"
+#endif
 
 typedef struct CORE_PORTABLE_S
 {
-    ee_u8 portable_id;
+  ee_u8 portable_id;
 } core_portable;
 
 /* target specific init/fini */
