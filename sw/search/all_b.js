a8c31961d333a17d5fa5cda73544b22b2ffc422a
GHA <push@gha>
2025-06-27 18:50:13 +0000

    update 90537fa2a1bba6e0f02990ee677f50555dbf976c

---
+++ sw/search/all_b.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['ebreak_5ftrap_5fhandler_0',['ebreak_trap_handler',['../example_2demo__semihosting_2main_8c.html#a0cc832639eb287d48e47a4da1a359c09',1,'main.c']]],
+  ['ecosystem_1',[':floppy_disk: &lt;a href=&quot;https://stnolting.github.io/neorv32/#_software_framework&quot; &gt;Software Framework&lt;/a&gt; - The Software Ecosystem',['../md_README.html#autotoc_md11',1,'']]]
+];
---
+++ sw/search/functions_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['ebreak_5ftrap_5fhandler_0',['ebreak_trap_handler',['../example_2demo__semihosting_2main_8c.html#a0cc832639eb287d48e47a4da1a359c09',1,'main.c']]]
+];
