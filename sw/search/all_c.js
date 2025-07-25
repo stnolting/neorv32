6cd4ea7234a592c51f1778e7f06fe6f45106cc6b
GHA <push@gha>
2025-07-25 17:35:15 +0000

    update 31bc7080b998802146f140b4751d028e74ad6ad0

---
+++ sw/search/all_c.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['legal_0',[':copyright: Legal',['../md_README.html#autotoc_md12',1,'']]],
+  ['load_5fexe_1',['load_exe',['../bootloader_2main_8c.html#a9526f702d7a434d6c6c8dbb26ce53bf9',1,'main.c']]]
+];
---
+++ sw/search/defines_9.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['sdi_5ffirq_5fenable_0',['SDI_FIRQ_ENABLE',['../neorv32_8h.html#a30cf20b6d704c50cc5c46238bb43646f',1,'neorv32.h']]],
+  ['sdi_5ffirq_5fpending_1',['SDI_FIRQ_PENDING',['../neorv32_8h.html#a68611c0dfbe331739eff10230ac3fede',1,'neorv32.h']]],
+  ['sdi_5ftrap_5fcode_2',['SDI_TRAP_CODE',['../neorv32_8h.html#a68925ca3f482f148caedd36118fd8d79',1,'neorv32.h']]],
+  ['silent_5fmode_3',['SILENT_MODE',['../example_2float__corner__test_2main_8c.html#a87f296d7594bb8178cf76f0c045b7013',1,'SILENT_MODE:&#160;main.c'],['../example_2floating__point__test_2main_8c.html#a87f296d7594bb8178cf76f0c045b7013',1,'SILENT_MODE:&#160;main.c']]],
+  ['slink_5ffirq_5fenable_4',['SLINK_FIRQ_ENABLE',['../neorv32_8h.html#a64a92fa91268599d601cd56ab2b84b55',1,'neorv32.h']]],
+  ['slink_5ffirq_5fpending_5',['SLINK_FIRQ_PENDING',['../neorv32_8h.html#a523d2a0ce4a1589a91414b89ca3cd2c0',1,'neorv32.h']]],
+  ['slink_5ftrap_5fcode_6',['SLINK_TRAP_CODE',['../neorv32_8h.html#a5e11d4944a892054ad26d2289c8dadb2',1,'neorv32.h']]],
+  ['spi_5ffirq_5fenable_7',['SPI_FIRQ_ENABLE',['../neorv32_8h.html#ab8fcd3f80c373eb0f9d0fc6bf64ca626',1,'neorv32.h']]],
+  ['spi_5ffirq_5fpending_8',['SPI_FIRQ_PENDING',['../neorv32_8h.html#ab2f58f746febb29a9828447c733f8043',1,'neorv32.h']]],
+  ['spi_5ftrap_5fcode_9',['SPI_TRAP_CODE',['../neorv32_8h.html#a83c212f1f5d8ee2a5921b3b2b19e9cc4',1,'neorv32.h']]]
+];
