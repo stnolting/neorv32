5299364ac9f67c399a25a54dcc239a7c32373e51
GHA <push@gha>
2025-08-24 08:45:12 +0000

    update 72273df61eb820731407df6ee162d047f514d99f

---
+++ sw/search/files_3.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['spi_5fflash_2ec_0',['spi_flash.c',['../spi__flash_8c.html',1,'']]],
+  ['spi_5fflash_2eh_1',['spi_flash.h',['../spi__flash_8h.html',1,'']]],
+  ['spinlock_2ec_2',['spinlock.c',['../demo__dual__core_2spinlock_8c.html',1,'(Global Namespace)'],['../demo__dual__core__rte_2spinlock_8c.html',1,'(Global Namespace)']]],
+  ['spinlock_2eh_3',['spinlock.h',['../demo__dual__core_2spinlock_8h.html',1,'(Global Namespace)'],['../demo__dual__core__rte_2spinlock_8h.html',1,'(Global Namespace)']]]
+];
---
+++ sw/search/functions_8.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['irq_5ftest_0',['irq_test',['../example_2demo__trng_2main_8c.html#a78c48f34ade1e4fecb1352d94f386a8c',1,'main.c']]],
+  ['is_5fprime_1',['is_prime',['../example_2demo__dual__core__primes_2main_8c.html#a4bb904556a9270d49ad91d7086487c99',1,'main.c']]]
+];
