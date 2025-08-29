e25d304056e6c533555710741706ed989a1ab92f
GHA <push@gha>
2025-08-29 04:40:32 +0000

    update 3560f91cfb57ac783d08615d49bab31faea411f4

---
+++ sw/search/functions_3.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['clear_5funiverse_0',['clear_universe',['../example_2game__of__life_2main_8c.html#a819f10c5b644a07eeb069b6e7f12050e',1,'main.c']]],
+  ['compute_5frate_1',['compute_rate',['../example_2demo__trng_2main_8c.html#abbde1237eed617466fb971a78c3acd1e',1,'main.c']]],
+  ['core1_5fentry_2',['core1_entry',['../example_2demo__dual__core__primes_2main_8c.html#a8418d6ec460e756538b293a039b96e10',1,'main.c']]],
+  ['core1_5fmain_3',['core1_main',['../example_2processor__check_2main_8c.html#ae1b0d6148e3c4d1635b43ee9544b98ef',1,'main.c']]],
+  ['count_5fprimes_4',['count_primes',['../example_2demo__dual__core__primes_2main_8c.html#a81027a8425c6f30e76e79cd72ea62e13',1,'main.c']]]
+];
---
+++ sw/search/functions_9.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['load_5fexe_0',['load_exe',['../bootloader_2main_8c.html#a9526f702d7a434d6c6c8dbb26ce53bf9',1,'main.c']]]
+];
