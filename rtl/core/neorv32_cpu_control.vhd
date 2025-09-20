d661ad8aa7be5abdfce1106868048a728cb4ff0f
stnolting <stnolting@gmail.com>
2025-09-20 11:45:30 +0200

    [control] do not read CSR if environment instruction
    actually, this is irrelevant as environment instructions like MRET do not write any x register, but this reduces switching activity in the CSR file and also simplifies execution tracing

--- rtl/core/neorv32_cpu_control.vhd
+++ rtl/core/neorv32_cpu_control.vhd
@@ -473,8 +473,9 @@ begin
 
           -- environment/CSR operation or ILLEGAL opcode --
           when others =>
-            if ((funct3_v = funct3_csrrw_c) or (funct3_v = funct3_csrrwi_c)) and (exe_engine.ir(instr_rd_msb_c downto instr_rd_lsb_c) = "00000") then
-              csr.re_nxt <= '0'; -- no read if CSRRW[I] and rd = 0
+            if (funct3_v = funct3_env_c) or
+               (((funct3_v = funct3_csrrw_c) or (funct3_v = funct3_csrrwi_c)) and (exe_engine.ir(instr_rd_msb_c downto instr_rd_lsb_c) = "00000")) then
+              csr.re_nxt <= '0'; -- no read if CSRRW[I] and rd = 0 OR if environment instruction 
             else
               csr.re_nxt <= '1';
             end if;
