d3c0c3fabb86e3c01bdb12092c545a8abc684ff9
vogma <vogel.marco@mailbox.org>
2025-10-07 16:40:27 +0200

    added self checking test for mvsa01

--- sw/example/zcmp/cm_mvsa01.c
+++ sw/example/zcmp/cm_mvsa01.c
@@ -1,7 +1,69 @@
 #include <cm_mvsa01.h>
 
-#define test_mvsa01(instr_bits, instr_name) \
-	neorv32_uart0_printf("Test: " instr_name "\n"); \
+// Test output extracted from spike:
+uint32_t cmvsa01s0s1[10] = {0xa500000a,0xa500000b,0xa500000a,0xa500000b,0xa5000012,0xa5000013,0xa5000014,0xa5000015,0xa5000016,0xa5000017};
+uint32_t cmvsa01s0s2[10] = {0xa500000a,0xa500000b,0xa500000a,0xa5000009,0xa500000b,0xa5000013,0xa5000014,0xa5000015,0xa5000016,0xa5000017};
+uint32_t cmvsa01s0s3[10] = {0xa500000a,0xa500000b,0xa500000a,0xa5000009,0xa5000012,0xa500000b,0xa5000014,0xa5000015,0xa5000016,0xa5000017};
+uint32_t cmvsa01s0s4[10] = {0xa500000a,0xa500000b,0xa500000a,0xa5000009,0xa5000012,0xa5000013,0xa500000b,0xa5000015,0xa5000016,0xa5000017};
+uint32_t cmvsa01s0s5[10] = {0xa500000a,0xa500000b,0xa500000a,0xa5000009,0xa5000012,0xa5000013,0xa5000014,0xa500000b,0xa5000016,0xa5000017};
+uint32_t cmvsa01s0s6[10] = {0xa500000a,0xa500000b,0xa500000a,0xa5000009,0xa5000012,0xa5000013,0xa5000014,0xa5000015,0xa500000b,0xa5000017};
+uint32_t cmvsa01s0s7[10] = {0xa500000a,0xa500000b,0xa500000a,0xa5000009,0xa5000012,0xa5000013,0xa5000014,0xa5000015,0xa5000016,0xa500000b};
+uint32_t cmvsa01s1s0[10] = {0xa500000a,0xa500000b,0xa500000b,0xa500000a,0xa5000012,0xa5000013,0xa5000014,0xa5000015,0xa5000016,0xa5000017};
+uint32_t cmvsa01s1s2[10] = {0xa500000a,0xa500000b,0xa5000008,0xa500000a,0xa500000b,0xa5000013,0xa5000014,0xa5000015,0xa5000016,0xa5000017};
+uint32_t cmvsa01s1s3[10] = {0xa500000a,0xa500000b,0xa5000008,0xa500000a,0xa5000012,0xa500000b,0xa5000014,0xa5000015,0xa5000016,0xa5000017};
+uint32_t cmvsa01s1s4[10] = {0xa500000a,0xa500000b,0xa5000008,0xa500000a,0xa5000012,0xa5000013,0xa500000b,0xa5000015,0xa5000016,0xa5000017};
+uint32_t cmvsa01s1s5[10] = {0xa500000a,0xa500000b,0xa5000008,0xa500000a,0xa5000012,0xa5000013,0xa5000014,0xa500000b,0xa5000016,0xa5000017};
+uint32_t cmvsa01s1s6[10] = {0xa500000a,0xa500000b,0xa5000008,0xa500000a,0xa5000012,0xa5000013,0xa5000014,0xa5000015,0xa500000b,0xa5000017};
+uint32_t cmvsa01s1s7[10] = {0xa500000a,0xa500000b,0xa5000008,0xa500000a,0xa5000012,0xa5000013,0xa5000014,0xa5000015,0xa5000016,0xa500000b};
+uint32_t cmvsa01s2s0[10] = {0xa500000a,0xa500000b,0xa500000b,0xa5000009,0xa500000a,0xa5000013,0xa5000014,0xa5000015,0xa5000016,0xa5000017};
+uint32_t cmvsa01s2s1[10] = {0xa500000a,0xa500000b,0xa5000008,0xa500000b,0xa500000a,0xa5000013,0xa5000014,0xa5000015,0xa5000016,0xa5000017};
+uint32_t cmvsa01s2s3[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa500000a,0xa500000b,0xa5000014,0xa5000015,0xa5000016,0xa5000017};
+uint32_t cmvsa01s2s4[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa500000a,0xa5000013,0xa500000b,0xa5000015,0xa5000016,0xa5000017};
+uint32_t cmvsa01s2s5[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa500000a,0xa5000013,0xa5000014,0xa500000b,0xa5000016,0xa5000017};
+uint32_t cmvsa01s2s6[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa500000a,0xa5000013,0xa5000014,0xa5000015,0xa500000b,0xa5000017};
+uint32_t cmvsa01s2s7[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa500000a,0xa5000013,0xa5000014,0xa5000015,0xa5000016,0xa500000b};
+uint32_t cmvsa01s3s0[10] = {0xa500000a,0xa500000b,0xa500000b,0xa5000009,0xa5000012,0xa500000a,0xa5000014,0xa5000015,0xa5000016,0xa5000017};
+uint32_t cmvsa01s3s1[10] = {0xa500000a,0xa500000b,0xa5000008,0xa500000b,0xa5000012,0xa500000a,0xa5000014,0xa5000015,0xa5000016,0xa5000017};
+uint32_t cmvsa01s3s2[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa500000b,0xa500000a,0xa5000014,0xa5000015,0xa5000016,0xa5000017};
+uint32_t cmvsa01s3s4[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa5000012,0xa500000a,0xa500000b,0xa5000015,0xa5000016,0xa5000017};
+uint32_t cmvsa01s3s5[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa5000012,0xa500000a,0xa5000014,0xa500000b,0xa5000016,0xa5000017};
+uint32_t cmvsa01s3s6[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa5000012,0xa500000a,0xa5000014,0xa5000015,0xa500000b,0xa5000017};
+uint32_t cmvsa01s3s7[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa5000012,0xa500000a,0xa5000014,0xa5000015,0xa5000016,0xa500000b};
+uint32_t cmvsa01s4s0[10] = {0xa500000a,0xa500000b,0xa500000b,0xa5000009,0xa5000012,0xa5000013,0xa500000a,0xa5000015,0xa5000016,0xa5000017};
+uint32_t cmvsa01s4s1[10] = {0xa500000a,0xa500000b,0xa5000008,0xa500000b,0xa5000012,0xa5000013,0xa500000a,0xa5000015,0xa5000016,0xa5000017};
+uint32_t cmvsa01s4s2[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa500000b,0xa5000013,0xa500000a,0xa5000015,0xa5000016,0xa5000017};
+uint32_t cmvsa01s4s3[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa5000012,0xa500000b,0xa500000a,0xa5000015,0xa5000016,0xa5000017};
+uint32_t cmvsa01s4s5[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa5000012,0xa5000013,0xa500000a,0xa500000b,0xa5000016,0xa5000017};
+uint32_t cmvsa01s4s6[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa5000012,0xa5000013,0xa500000a,0xa5000015,0xa500000b,0xa5000017};
+uint32_t cmvsa01s4s7[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa5000012,0xa5000013,0xa500000a,0xa5000015,0xa5000016,0xa500000b};
+uint32_t cmvsa01s5s0[10] = {0xa500000a,0xa500000b,0xa500000b,0xa5000009,0xa5000012,0xa5000013,0xa5000014,0xa500000a,0xa5000016,0xa5000017};
+uint32_t cmvsa01s5s1[10] = {0xa500000a,0xa500000b,0xa5000008,0xa500000b,0xa5000012,0xa5000013,0xa5000014,0xa500000a,0xa5000016,0xa5000017};
+uint32_t cmvsa01s5s2[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa500000b,0xa5000013,0xa5000014,0xa500000a,0xa5000016,0xa5000017};
+uint32_t cmvsa01s5s3[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa5000012,0xa500000b,0xa5000014,0xa500000a,0xa5000016,0xa5000017};
+uint32_t cmvsa01s5s4[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa5000012,0xa5000013,0xa500000b,0xa500000a,0xa5000016,0xa5000017};
+uint32_t cmvsa01s5s6[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa5000012,0xa5000013,0xa5000014,0xa500000a,0xa500000b,0xa5000017};
+uint32_t cmvsa01s5s7[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa5000012,0xa5000013,0xa5000014,0xa500000a,0xa5000016,0xa500000b};
+uint32_t cmvsa01s6s0[10] = {0xa500000a,0xa500000b,0xa500000b,0xa5000009,0xa5000012,0xa5000013,0xa5000014,0xa5000015,0xa500000a,0xa5000017};
+uint32_t cmvsa01s6s1[10] = {0xa500000a,0xa500000b,0xa5000008,0xa500000b,0xa5000012,0xa5000013,0xa5000014,0xa5000015,0xa500000a,0xa5000017};
+uint32_t cmvsa01s6s2[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa500000b,0xa5000013,0xa5000014,0xa5000015,0xa500000a,0xa5000017};
+uint32_t cmvsa01s6s3[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa5000012,0xa500000b,0xa5000014,0xa5000015,0xa500000a,0xa5000017};
+uint32_t cmvsa01s6s4[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa5000012,0xa5000013,0xa500000b,0xa5000015,0xa500000a,0xa5000017};
+uint32_t cmvsa01s6s5[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa5000012,0xa5000013,0xa5000014,0xa500000b,0xa500000a,0xa5000017};
+uint32_t cmvsa01s6s7[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa5000012,0xa5000013,0xa5000014,0xa5000015,0xa500000a,0xa500000b};
+uint32_t cmvsa01s7s0[10] = {0xa500000a,0xa500000b,0xa500000b,0xa5000009,0xa5000012,0xa5000013,0xa5000014,0xa5000015,0xa5000016,0xa500000a};
+uint32_t cmvsa01s7s1[10] = {0xa500000a,0xa500000b,0xa5000008,0xa500000b,0xa5000012,0xa5000013,0xa5000014,0xa5000015,0xa5000016,0xa500000a};
+uint32_t cmvsa01s7s2[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa500000b,0xa5000013,0xa5000014,0xa5000015,0xa5000016,0xa500000a};
+uint32_t cmvsa01s7s3[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa5000012,0xa500000b,0xa5000014,0xa5000015,0xa5000016,0xa500000a};
+uint32_t cmvsa01s7s4[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa5000012,0xa5000013,0xa500000b,0xa5000015,0xa5000016,0xa500000a};
+uint32_t cmvsa01s7s5[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa5000012,0xa5000013,0xa5000014,0xa500000b,0xa5000016,0xa500000a};
+uint32_t cmvsa01s7s6[10] = {0xa500000a,0xa500000b,0xa5000008,0xa5000009,0xa5000012,0xa5000013,0xa5000014,0xa5000015,0xa500000b,0xa500000a};
+
+uint8_t local_result = 0;
+uint8_t global_result = 0;
+
+#define test_mvsa01(instr_bits, instr_name, arr) \
+	neorv32_uart0_printf(instr_name); \
+	local_result = 0; \
 	asm volatile ( \
 		/* Save all clobbered registers onto the stack */ \
 		"addi sp, sp, -48\n" \
@@ -55,75 +117,81 @@
 		"lw ra, 40(sp)\n" \
 		"addi sp, sp, 48\n" \
 	); \
-	neorv32_uart0_printf("a0: %x\n", test_results[0]); \
-	neorv32_uart0_printf("a1: %x\n", test_results[1]); \
-	neorv32_uart0_printf("s0: %x\n", test_results[2]); \
-	neorv32_uart0_printf("s1: %x\n", test_results[3]); \
-	neorv32_uart0_printf("s2: %x\n", test_results[4]); \
-	neorv32_uart0_printf("s3: %x\n", test_results[5]); \
-	neorv32_uart0_printf("s4: %x\n", test_results[6]); \
-	neorv32_uart0_printf("s5: %x\n", test_results[7]); \
-	neorv32_uart0_printf("s6: %x\n", test_results[8]); \
-	neorv32_uart0_printf("s7: %x\n", test_results[9]); \
-	neorv32_uart0_printf("\n");
+	for(int i=0; i<10; i++) { \
+		if (test_results[i] != arr[i]) { \
+			local_result = 1; \
+			global_result = 1; \
+		} \
+	} \
+	if (local_result == 0) { \
+		neorv32_uart0_printf(" - OK\n"); \
+	} else { \
+		neorv32_uart0_printf(" - FAIL\n"); \
+	} 
 
 int cm_mvsa01() {
-	test_mvsa01(0xac26, "cm.mvsa01 s0,s1");
-	test_mvsa01(0xac2a, "cm.mvsa01 s0,s2");
-	test_mvsa01(0xac2e, "cm.mvsa01 s0,s3");
-	test_mvsa01(0xac32, "cm.mvsa01 s0,s4");
-	test_mvsa01(0xac36, "cm.mvsa01 s0,s5");
-	test_mvsa01(0xac3a, "cm.mvsa01 s0,s6");
-	test_mvsa01(0xac3e, "cm.mvsa01 s0,s7");
-	test_mvsa01(0xaca2, "cm.mvsa01 s1,s0");
-	test_mvsa01(0xacaa, "cm.mvsa01 s1,s2");
-	test_mvsa01(0xacae, "cm.mvsa01 s1,s3");
-	test_mvsa01(0xacb2, "cm.mvsa01 s1,s4");
-	test_mvsa01(0xacb6, "cm.mvsa01 s1,s5");
-	test_mvsa01(0xacba, "cm.mvsa01 s1,s6");
-	test_mvsa01(0xacbe, "cm.mvsa01 s1,s7");
-	test_mvsa01(0xad22, "cm.mvsa01 s2,s0");
-	test_mvsa01(0xad26, "cm.mvsa01 s2,s1");
-	test_mvsa01(0xad2e, "cm.mvsa01 s2,s3");
-	test_mvsa01(0xad32, "cm.mvsa01 s2,s4");
-	test_mvsa01(0xad36, "cm.mvsa01 s2,s5");
-	test_mvsa01(0xad3a, "cm.mvsa01 s2,s6");
-	test_mvsa01(0xad3e, "cm.mvsa01 s2,s7");
-	test_mvsa01(0xada2, "cm.mvsa01 s3,s0");
-	test_mvsa01(0xada6, "cm.mvsa01 s3,s1");
-	test_mvsa01(0xadaa, "cm.mvsa01 s3,s2");
-	test_mvsa01(0xadb2, "cm.mvsa01 s3,s4");
-	test_mvsa01(0xadb6, "cm.mvsa01 s3,s5");
-	test_mvsa01(0xadba, "cm.mvsa01 s3,s6");
-	test_mvsa01(0xadbe, "cm.mvsa01 s3,s7");
-	test_mvsa01(0xae22, "cm.mvsa01 s4,s0");
-	test_mvsa01(0xae26, "cm.mvsa01 s4,s1");
-	test_mvsa01(0xae2a, "cm.mvsa01 s4,s2");
-	test_mvsa01(0xae2e, "cm.mvsa01 s4,s3");
-	test_mvsa01(0xae36, "cm.mvsa01 s4,s5");
-	test_mvsa01(0xae3a, "cm.mvsa01 s4,s6");
-	test_mvsa01(0xae3e, "cm.mvsa01 s4,s7");
-	test_mvsa01(0xaea2, "cm.mvsa01 s5,s0");
-	test_mvsa01(0xaea6, "cm.mvsa01 s5,s1");
-	test_mvsa01(0xaeaa, "cm.mvsa01 s5,s2");
-	test_mvsa01(0xaeae, "cm.mvsa01 s5,s3");
-	test_mvsa01(0xaeb2, "cm.mvsa01 s5,s4");
-	test_mvsa01(0xaeba, "cm.mvsa01 s5,s6");
-	test_mvsa01(0xaebe, "cm.mvsa01 s5,s7");
-	test_mvsa01(0xaf22, "cm.mvsa01 s6,s0");
-	test_mvsa01(0xaf26, "cm.mvsa01 s6,s1");
-	test_mvsa01(0xaf2a, "cm.mvsa01 s6,s2");
-	test_mvsa01(0xaf2e, "cm.mvsa01 s6,s3");
-	test_mvsa01(0xaf32, "cm.mvsa01 s6,s4");
-	test_mvsa01(0xaf36, "cm.mvsa01 s6,s5");
-	test_mvsa01(0xaf3e, "cm.mvsa01 s6,s7");
-	test_mvsa01(0xafa2, "cm.mvsa01 s7,s0");
-	test_mvsa01(0xafa6, "cm.mvsa01 s7,s1");
-	test_mvsa01(0xafaa, "cm.mvsa01 s7,s2");
-	test_mvsa01(0xafae, "cm.mvsa01 s7,s3");
-	test_mvsa01(0xafb2, "cm.mvsa01 s7,s4");
-	test_mvsa01(0xafb6, "cm.mvsa01 s7,s5");
-	test_mvsa01(0xafba, "cm.mvsa01 s7,s6");
+	test_mvsa01(0xac26, "cm.mvsa01 s0,s1",cmvsa01s0s1);
+	test_mvsa01(0xac2a, "cm.mvsa01 s0,s2",cmvsa01s0s2);
+	test_mvsa01(0xac2e, "cm.mvsa01 s0,s3",cmvsa01s0s3);
+	test_mvsa01(0xac32, "cm.mvsa01 s0,s4",cmvsa01s0s4);
+	test_mvsa01(0xac36, "cm.mvsa01 s0,s5",cmvsa01s0s5);
+	test_mvsa01(0xac3a, "cm.mvsa01 s0,s6",cmvsa01s0s6);
+	test_mvsa01(0xac3e, "cm.mvsa01 s0,s7",cmvsa01s0s7);
+	test_mvsa01(0xaca2, "cm.mvsa01 s1,s0",cmvsa01s1s0);
+	test_mvsa01(0xacaa, "cm.mvsa01 s1,s2",cmvsa01s1s2);
+	test_mvsa01(0xacae, "cm.mvsa01 s1,s3",cmvsa01s1s3);
+	test_mvsa01(0xacb2, "cm.mvsa01 s1,s4",cmvsa01s1s4);
+	test_mvsa01(0xacb6, "cm.mvsa01 s1,s5",cmvsa01s1s5);
+	test_mvsa01(0xacba, "cm.mvsa01 s1,s6",cmvsa01s1s6);
+	test_mvsa01(0xacbe, "cm.mvsa01 s1,s7",cmvsa01s1s7);
+	test_mvsa01(0xad22, "cm.mvsa01 s2,s0",cmvsa01s2s0);
+	test_mvsa01(0xad26, "cm.mvsa01 s2,s1",cmvsa01s2s1);
+	test_mvsa01(0xad2e, "cm.mvsa01 s2,s3",cmvsa01s2s3);
+	test_mvsa01(0xad32, "cm.mvsa01 s2,s4",cmvsa01s2s4);
+	test_mvsa01(0xad36, "cm.mvsa01 s2,s5",cmvsa01s2s5);
+	test_mvsa01(0xad3a, "cm.mvsa01 s2,s6",cmvsa01s2s6);
+	test_mvsa01(0xad3e, "cm.mvsa01 s2,s7",cmvsa01s2s7);
+	test_mvsa01(0xada2, "cm.mvsa01 s3,s0",cmvsa01s3s0);
+	test_mvsa01(0xada6, "cm.mvsa01 s3,s1",cmvsa01s3s1);
+	test_mvsa01(0xadaa, "cm.mvsa01 s3,s2",cmvsa01s3s2);
+	test_mvsa01(0xadb2, "cm.mvsa01 s3,s4",cmvsa01s3s4);
+	test_mvsa01(0xadb6, "cm.mvsa01 s3,s5",cmvsa01s3s5);
+	test_mvsa01(0xadba, "cm.mvsa01 s3,s6",cmvsa01s3s6);
+	test_mvsa01(0xadbe, "cm.mvsa01 s3,s7",cmvsa01s3s7);
+	test_mvsa01(0xae22, "cm.mvsa01 s4,s0",cmvsa01s4s0);
+	test_mvsa01(0xae26, "cm.mvsa01 s4,s1",cmvsa01s4s1);
+	test_mvsa01(0xae2a, "cm.mvsa01 s4,s2",cmvsa01s4s2);
+	test_mvsa01(0xae2e, "cm.mvsa01 s4,s3",cmvsa01s4s3);
+	test_mvsa01(0xae36, "cm.mvsa01 s4,s5",cmvsa01s4s5);
+	test_mvsa01(0xae3a, "cm.mvsa01 s4,s6",cmvsa01s4s6);
+	test_mvsa01(0xae3e, "cm.mvsa01 s4,s7",cmvsa01s4s7);
+	test_mvsa01(0xaea2, "cm.mvsa01 s5,s0",cmvsa01s5s0);
+	test_mvsa01(0xaea6, "cm.mvsa01 s5,s1",cmvsa01s5s1);
+	test_mvsa01(0xaeaa, "cm.mvsa01 s5,s2",cmvsa01s5s2);
+	test_mvsa01(0xaeae, "cm.mvsa01 s5,s3",cmvsa01s5s3);
+	test_mvsa01(0xaeb2, "cm.mvsa01 s5,s4",cmvsa01s5s4);
+	test_mvsa01(0xaeba, "cm.mvsa01 s5,s6",cmvsa01s5s6);
+	test_mvsa01(0xaebe, "cm.mvsa01 s5,s7",cmvsa01s5s7);
+	test_mvsa01(0xaf22, "cm.mvsa01 s6,s0",cmvsa01s6s0);
+	test_mvsa01(0xaf26, "cm.mvsa01 s6,s1",cmvsa01s6s1);
+	test_mvsa01(0xaf2a, "cm.mvsa01 s6,s2",cmvsa01s6s2);
+	test_mvsa01(0xaf2e, "cm.mvsa01 s6,s3",cmvsa01s6s3);
+	test_mvsa01(0xaf32, "cm.mvsa01 s6,s4",cmvsa01s6s4);
+	test_mvsa01(0xaf36, "cm.mvsa01 s6,s5",cmvsa01s6s5);
+	test_mvsa01(0xaf3e, "cm.mvsa01 s6,s7",cmvsa01s6s7);
+	test_mvsa01(0xafa2, "cm.mvsa01 s7,s0",cmvsa01s7s0);
+	test_mvsa01(0xafa6, "cm.mvsa01 s7,s1",cmvsa01s7s1);
+	test_mvsa01(0xafaa, "cm.mvsa01 s7,s2",cmvsa01s7s2);
+	test_mvsa01(0xafae, "cm.mvsa01 s7,s3",cmvsa01s7s3);
+	test_mvsa01(0xafb2, "cm.mvsa01 s7,s4",cmvsa01s7s4);
+	test_mvsa01(0xafb6, "cm.mvsa01 s7,s5",cmvsa01s7s5);
+	test_mvsa01(0xafba, "cm.mvsa01 s7,s6",cmvsa01s7s6);
+
+	if (global_result == 0) {
+		neorv32_uart0_printf("\nAll tests passed successfully.\n");
+	} else {
+		neorv32_uart0_printf("\nSome tests failed.\n");
+	}
 
 	return 0;
 }
