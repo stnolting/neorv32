9cf3288539ae34da51e65f4f68c5d46fae93d440
stnolting <stnolting@gmail.com>
2025-11-01 19:01:23 +0100

    ✨ [cpu] add support for Zibi ISA extension

--- sw/lib/include/neorv32_csr.h
+++ sw/lib/include/neorv32_csr.h
@@ -316,7 +316,7 @@ enum NEORV32_CSR_MXISA_enum {
   CSR_MXISA_ZIFENCEI =  1, /**< CPU mxisa CSR  (1): instruction stream sync (r/-)*/
   CSR_MXISA_ZMMUL    =  2, /**< CPU mxisa CSR  (2): hardware mul/div (r/-)*/
   CSR_MXISA_ZXCFU    =  3, /**< CPU mxisa CSR  (3): custom RISC-V instructions (r/-)*/
-  CSR_MXISA_ZKT      =  4, /**< CPU mxisa CSR  (4): data independent execution time (of cryptographic operations) (r/-)*/
+  CSR_MXISA_ZKT      =  4, /**< CPU mxisa CSR  (4): data independent execution time (of cryptography operations) (r/-)*/
   CSR_MXISA_ZFINX    =  5, /**< CPU mxisa CSR  (5): FPU using x registers (r/-)*/
   CSR_MXISA_ZICOND   =  6, /**< CPU mxisa CSR  (6): integer conditional operations (r/-)*/
   CSR_MXISA_ZICNTR   =  7, /**< CPU mxisa CSR  (7): standard instruction, cycle and time counter CSRs (r/-)*/
@@ -340,7 +340,8 @@ enum NEORV32_CSR_MXISA_enum {
   CSR_MXISA_ZAAMO    = 25, /**< CPU mxisa CSR (25): atomic read-modify-write operations (r/-)*/
   CSR_MXISA_ZALRSC   = 26, /**< CPU mxisa CSR (26): atomic reservation-set operations (r/-)*/
   CSR_MXISA_ZCB      = 27, /**< CPU mxisa CSR (27): additional code size reduction instruction (r/-)*/
-  CSR_MXISA_ZCA      = 28  /**< CPU mxisa CSR (28): compressed instructions without floating-point (r/-)*/
+  CSR_MXISA_ZCA      = 28, /**< CPU mxisa CSR (28): compressed instructions without floating-point (r/-)*/
+  CSR_MXISA_ZIBI     = 29  /**< CPU mxisa CSR (29): branch with immediate-comparison (r/-)*/
 };
 
 
