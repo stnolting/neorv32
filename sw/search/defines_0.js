ebc9896a5bfd6d8b61f910df20833e8690df3986
GHA <push@gha>
2025-11-22 11:20:17 +0000

    update eb3a841b3c6130970cb98c8a4c4db144623d816c

---
+++ sw/search/defines_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['cfs_5ffirq_5fenable_0',['CFS_FIRQ_ENABLE',['../neorv32_8h.html#ace79ee46daf3a1b689d9fe79dfcd6627',1,'neorv32.h']]],
+  ['cfs_5ffirq_5fpending_1',['CFS_FIRQ_PENDING',['../neorv32_8h.html#aae182648a2c23d070b9db0295388e001',1,'neorv32.h']]],
+  ['cfs_5ftrap_5fcode_2',['CFS_TRAP_CODE',['../neorv32_8h.html#aae6b2ae3fba20ff80bfb59735f179c26',1,'neorv32.h']]]
+];
---
+++ sw/search/variables_b.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['weekday_0',['weekday',['../structdate__t.html#a44167cd71416886e55048310af6d4e1b',1,'date_t']]],
+  ['word_1',['WORD',['../structneorv32__gptmr__t.html#a4c2bc2076a21ee717680c31ccf5564f7',1,'neorv32_gptmr_t']]]
+];
