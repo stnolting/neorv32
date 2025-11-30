f327855439330b5740f62b1460f47feb257a2cc4
GHA <push@gha>
2025-11-30 07:53:16 +0000

    update 3ce9ff80424263b8eb924cdf2a1ebacb071ccbfc

---
+++ sw/search/variables_6.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['minutes_0',['minutes',['../structdate__t.html#a2ab79834ebae42979e359bf30ad5f67a',1,'date_t']]],
+  ['misc_1',['MISC',['../structneorv32__sysinfo__t.html#ab75cd8f3b25cf14b61014e2d19d43765',1,'neorv32_sysinfo_t']]],
+  ['mode_2',['MODE',['../structneorv32__gptmr__t.html#aa09662c619ff5df11fde78d5fd205cee',1,'neorv32_gptmr_t']]],
+  ['month_3',['month',['../structdate__t.html#a1a97f6a755cd4209a0880cf811d52c7b',1,'date_t']]],
+  ['mswi_4',['MSWI',['../structneorv32__clint__t.html#a7b49826af73aae2035cd697cb4b2666b',1,'neorv32_clint_t']]],
+  ['mtime_5',['MTIME',['../structneorv32__clint__t.html#aea25562a01a789da8b6c5ee82c6a9ef0',1,'neorv32_clint_t']]],
+  ['mtimecmp_6',['MTIMECMP',['../structneorv32__clint__t.html#ac198a6ec62ea154d2533aebdd7b0df36',1,'neorv32_clint_t']]]
+];
