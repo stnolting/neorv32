6c6866faac3e6d3a18ad8f5275dbaeba09bc4fa8
stnolting <stnolting@gmail.com>
2025-12-27 20:23:21 +0100

    [cpu] minor comment edit

--- rtl/core/neorv32_cpu.vhd
+++ rtl/core/neorv32_cpu.vhd
@@ -22,7 +22,7 @@ entity neorv32_cpu is
   generic (
     -- General --
     HART_ID             : natural range 0 to 1023;        -- hardware thread ID
-    VENDOR_ID           : std_ulogic_vector(31 downto 0); -- vendor ID (JEDEC bank ID + offset)
+    VENDOR_ID           : std_ulogic_vector(31 downto 0); -- vendor ID
     BOOT_ADDR           : std_ulogic_vector(31 downto 0); -- CPU boot address
     DEBUG_PARK_ADDR     : std_ulogic_vector(31 downto 0); -- CPU debug mode parking loop entry address
     DEBUG_EXC_ADDR      : std_ulogic_vector(31 downto 0); -- CPU debug mode exception entry address
@@ -229,7 +229,7 @@ begin
   generic map (
     -- General --
     HART_ID          => HART_ID,          -- hardware thread ID
-    VENDOR_ID        => VENDOR_ID,        -- vendor ID (JEDEC bank ID + offset)
+    VENDOR_ID        => VENDOR_ID,        -- vendor ID
     BOOT_ADDR        => BOOT_ADDR,        -- CPU boot address
     DEBUG_PARK_ADDR  => DEBUG_PARK_ADDR,  -- CPU debug mode parking loop entry address
     DEBUG_EXC_ADDR   => DEBUG_EXC_ADDR,   -- CPU debug mode exception entry address
