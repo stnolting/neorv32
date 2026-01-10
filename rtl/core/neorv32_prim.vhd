66e59b17bf9efb4000597f3fb584a026537a1833
stnolting <stnolting@gmail.com>
2026-01-10 07:44:36 +0100

    [prim] remove SDPRAM

--- rtl/core/neorv32_prim.vhd
+++ rtl/core/neorv32_prim.vhd
@@ -224,75 +224,18 @@ begin
     data_o <= rdata;
   end generate;
 
-end neorv32_prim_spram_rtl;
-
-
--- ================================================================================ --
--- NEORV32 - Primitives - Generic Simple Dual-Port RAM (SDPRAM)                     --
--- -------------------------------------------------------------------------------- --
--- Provides a single read/write port and a read-only port.                          --
--- Can be mapped to blockRAM primitives.                                            --
--- -------------------------------------------------------------------------------- --
--- The NEORV32 RISC-V Processor - https://github.com/stnolting/neorv32              --
--- Copyright (c) NEORV32 contributors.                                              --
--- Copyright (c) 2020 - 2025 Stephan Nolting. All rights reserved.                  --
--- Licensed under the BSD-3-Clause license, see LICENSE for details.                --
--- SPDX-License-Identifier: BSD-3-Clause                                            --
--- ================================================================================ --
-
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
-entity neorv32_prim_sdpram is
-  generic (
-    AWIDTH : natural; -- address width (number of bits)
-    DWIDTH : natural; -- data width (number of bits)
-    OUTREG : boolean  -- add output register stage
-  );
-  port (
-    -- global control --
-    clk_i    : in  std_ulogic;                           -- global clock
-    -- read/write port --
-    a_en_i   : in  std_ulogic;                           -- access enable
-    a_rw_i   : in  std_ulogic;                           -- 0=read, 1=write
-    a_addr_i : in  std_ulogic_vector(AWIDTH-1 downto 0); -- read/write address
-    a_data_i : in  std_ulogic_vector(DWIDTH-1 downto 0); -- write data
-    a_data_o : out std_ulogic_vector(DWIDTH-1 downto 0); -- read data
-    -- read port --
-    b_en_i   : in  std_ulogic;                           -- access enable
-    b_addr_i : in  std_ulogic_vector(AWIDTH-1 downto 0); -- read address
-    b_data_o : out std_ulogic_vector(DWIDTH-1 downto 0)  -- read data
-  );
-end neorv32_prim_sdpram;
-
-architecture neorv32_prim_sdpram_rtl of neorv32_prim_sdpram is
-
-  type ram_t is array ((2**AWIDTH)-1 downto 0) of std_ulogic_vector(DWIDTH-1 downto 0);
-  signal sdpram : ram_t;
-  signal a_rdata, b_rdata : std_ulogic_vector(DWIDTH-1 downto 0);
-
-begin
-
-  -- Memory Core ----------------------------------------------------------------------------
-  -- -------------------------------------------------------------------------------------------
-  memory_core: process(clk_i)
-  begin
-    if rising_edge(clk_i) then
-      -- port A: read/write --
-      if (a_en_i = '1') then
-        if (a_rw_i = '1') then
-          sdpram(to_integer(unsigned(a_addr_i))) <= a_data_i;
-        else
-          a_rdata <= sdpram(to_integer(unsigned(a_addr_i)));
+  -- single entry only --
+  memory_small:
+  if (AWIDTH = 0) generate
+    memory_core: process(clk_i)
+    begin
+      if rising_edge(clk_i) then
+        if (en_i = '1') and (rw_i = '1') then
+          rdata <= data_i;
         end if;
       end if;
-      -- port B: read-only --
-      if (b_en_i = '1') then
-        b_rdata <= sdpram(to_integer(unsigned(b_addr_i)));
-      end if;
-    end if;
-  end process memory_core;
+    end process memory_core;
+  end generate;
 
   -- Output Register ------------------------------------------------------------------------
   -- -------------------------------------------------------------------------------------------
@@ -301,8 +244,7 @@ begin
     read_outreg: process(clk_i)
     begin
       if rising_edge(clk_i) then
-        a_data_o <= a_rdata;
-        b_data_o <= b_rdata;
+        data_o <= rdata;
       end if;
     end process read_outreg;
   end generate;
@@ -310,11 +252,10 @@ begin
   -- no output register --
   output_register_disabled:
   if not OUTREG generate
-    a_data_o <= a_rdata;
-    b_data_o <= b_rdata;
+    data_o <= rdata;
   end generate;
 
-end neorv32_prim_sdpram_rtl;
+end neorv32_prim_spram_rtl;
 
 
 -- ================================================================================ --
