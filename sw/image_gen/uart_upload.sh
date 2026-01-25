877460750fc030d1193be47e0da675d19d1b766c
stnolting <stnolting@gmail.com>
2026-01-25 16:02:25 +0100

    [uart_upload] rework bootloader uplaod script
    to make it more robust

--- sw/image_gen/uart_upload.sh
+++ sw/image_gen/uart_upload.sh
@@ -1,64 +1,75 @@
 #!/usr/bin/env bash
 
+# Simple script to upload an executable to the default NEORV32 bootloader
+
 set -e
 
-# Simple script to upload executable via bootloader
+# Default baud rate
+BAUD=19200
 
+# check arguments; show help text
 if [ $# -ne 2 ]
 then
-  printf "Upload and execute application image via serial port (UART) to the NEORV32 bootloader.\n"
+  printf "Upload an executable via serial port (UART) to the NEORV32 bootloader.\n"
+  printf "If the upload is successful, the executable will be run automatically.\n"
   printf "Reset processor before starting the upload.\n\n"
-  printf "Usage:   sh uart_upload.sh <serial port> <NEORV32 executable>\n"
-  printf "Example: sh uart_upload.sh /dev/ttyS6 path/to/project/neorv32_exe.bin\n"
+  printf "Usage:   ./uart_upload.sh <serial port> <NEORV32 executable>\n"
+  printf "Example: ./uart_upload.sh /dev/ttyS6 path/to/neorv32_exe.bin\n"
   exit 0
 fi
 
-# configure serial port
-stty -F "$1" 19200 -hup raw -echo -echoe -echok -echoctl -echoke -ixon cs8 -cstopb noflsh clocal cread
+# timeout to make sure there is enough time for the file to be sent in background
+# "BAUD" bits per second; 10 bits per byte (incl START and STOP bits); plus safety margin
+FILESIZE=$(stat -c%s "$2")
+TIMEOUT=$(( (FILESIZE / (BAUD/10)) + 2 ))
 
-# abort autoboot sequence
-printf " " > $1 # send any char that triggers no command
+# setup serial port
+printf "Opening serial port ($1)... "
+stty -F $1 $BAUD raw -echo -ixon -ixoff -icrnl -onlcr
+exec 3<>$1
+printf "OK\n"
 
-# execute upload command and get response
-exec 3<$1                            # redirect serial output to fd 3
-cat <&3 > uart_upload.response.tmp & # redirect serial output to file
-PID=$!                               # save pid to kill cat later
-printf "u" > $1                      # send upload command to serial port
-sleep 0.5s                           # wait for bootloader response
-kill $PID                            # kill cat process
+# send executable
+printf "Uploading executable ($FILESIZE bytes)... "
+printf " " >&3 # skip auto-boot
+printf "u" >&3 # start upload
+cat $2 >&3     # send executable
 
-exec 3<&- # free fd 3
+# wait for upload to complete and check bootloader response
+UPOLOAD_OK=0
+EXPIRED=$(( SECONDS + TIMEOUT ))
+PATTERN="OK"
+BUFFER=""
+while [ $SECONDS -lt $EXPIRED ]; do
+  if read -r -n 1 -u 3 -t 1 ch; then
+    BUFFER+="$ch"
+    UPOLOAD_OK=-1
+    if [[ "$BUFFER" == *"$PATTERN"* ]]; then
+      UPOLOAD_OK=1
+      break
+    fi
+  fi
+done
 
-# check response
-if ! grep -Fq "Awaiting neorv32_exe.bin" uart_upload.response.tmp;
-then
-  printf "Bootloader response error!\n"
-  printf "Reset processor before starting the upload.\n"
-  rm -f uart_upload.response.tmp
-  exit 1
-fi
-
-# send executable and get response
-printf "Uploading executable..."
-exec 3<$1                            # redirect serial output to fd 3
-cat <&3 > uart_upload.response.tmp & # redirect serial output to file
-PID=$!                               # save pid to kill cat later
-cat "$2" > "$1"                      # send executable to serial port
-sleep 3s                             # wait for bootloader response
-kill $PID                            # kill cat process
-
-exec 3<&- # free fd 3
-
-# check response
-if ! grep -Fq "OK" uart_upload.response.tmp;
-then
-  printf " FAILED!\n"
-  rm -f uart_upload.response.tmp
-  exit 1
+if [ $UPOLOAD_OK -eq 1 ]; then
+  printf "OK\n" >&2
+elif [ $UPOLOAD_OK -eq -1 ]; then
+  printf "\nERROR! Invalid bootloader response.\n" >&2
 else
-  printf " OK\n"
-  echo "Starting application..."
-  printf "e" > $1
-  rm -f uart_upload.response.tmp
+  printf "\nERROR! Bootloader response timeout ($TIMEOUT seconds).\n" >&2
+fi
+
+# start executable
+if [ $UPOLOAD_OK -eq 1 ]; then
+  printf "Booting executable...\n"
+  printf "e" >&3
+fi
+
+# done
+exec 3>&-
+if [ $UPOLOAD_OK -eq 1 ]; then
   exit 0
+else
+  printf "Check configuration, reset processor and try again.\n" >&2
+  exit 1;
 fi
