2afec3af9bc0cd73b2d6c44c1787cb026eb11c93
GHA <push@gha>
2026-02-12 04:10:58 +0000

    update 6873ca936f65e3dec15d932648acd339b4da5f36

---
+++ sw/search/defines_7.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['sdi_5ffirq_5fenable_0',['SDI_FIRQ_ENABLE',['../neorv32_8h.html#a30cf20b6d704c50cc5c46238bb43646f',1,'neorv32.h']]],
+  ['sdi_5ffirq_5fpending_1',['SDI_FIRQ_PENDING',['../neorv32_8h.html#a68611c0dfbe331739eff10230ac3fede',1,'neorv32.h']]],
+  ['sdi_5ftrap_5fcode_2',['SDI_TRAP_CODE',['../neorv32_8h.html#a68925ca3f482f148caedd36118fd8d79',1,'neorv32.h']]],
+  ['slink_5ffirq_5fenable_3',['SLINK_FIRQ_ENABLE',['../neorv32_8h.html#a64a92fa91268599d601cd56ab2b84b55',1,'neorv32.h']]],
+  ['slink_5ffirq_5fpending_4',['SLINK_FIRQ_PENDING',['../neorv32_8h.html#a523d2a0ce4a1589a91414b89ca3cd2c0',1,'neorv32.h']]],
+  ['slink_5ftrap_5fcode_5',['SLINK_TRAP_CODE',['../neorv32_8h.html#a5e11d4944a892054ad26d2289c8dadb2',1,'neorv32.h']]],
+  ['spi_5ffirq_5fenable_6',['SPI_FIRQ_ENABLE',['../neorv32_8h.html#ab8fcd3f80c373eb0f9d0fc6bf64ca626',1,'neorv32.h']]],
+  ['spi_5ffirq_5fpending_7',['SPI_FIRQ_PENDING',['../neorv32_8h.html#ab2f58f746febb29a9828447c733f8043',1,'neorv32.h']]],
+  ['spi_5ftrap_5fcode_8',['SPI_TRAP_CODE',['../neorv32_8h.html#a83c212f1f5d8ee2a5921b3b2b19e9cc4',1,'neorv32.h']]]
+];
---
+++ sw/search/searchdata.js
@@ -0,0 +1,36 @@
+var indexSectionsWithContent =
+{
+  0: "_cdeghimnoprstuwy",
+  1: "dns",
+  2: "n",
+  3: "_n",
+  4: "_cdehimprstwy",
+  5: "ns",
+  6: "cdhnopstuw",
+  7: "cdginorstuw"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "enums",
+  6: "enumvalues",
+  7: "defines"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Data Structures",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Enumerations",
+  6: "Enumerator",
+  7: "Macros"
+};
+
