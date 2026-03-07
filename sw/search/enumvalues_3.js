8a20053fe65d0bded867096d3d480fd4ebf0497d
GHA <push@gha>
2026-03-07 03:52:01 +0000

    update 051e5a036d4706dcc3dab45f195968e630cd25ed

---
+++ sw/search/enumvalues_3.js
@@ -0,0 +1,17 @@
+var searchData=
+[
+  ['neoled_5fctrl_5fen_0',['NEOLED_CTRL_EN',['../neorv32__neoled_8h.html#ab99c7fed050816eae0fa0315562e0412af97ab77b33a250d87025c3dd3a21fea7',1,'neorv32_neoled.h']]],
+  ['neoled_5fctrl_5ffifo_5flsb_1',['NEOLED_CTRL_FIFO_LSB',['../neorv32__neoled_8h.html#ab99c7fed050816eae0fa0315562e0412a5e32ef67de37338164120b4156a49143',1,'neorv32_neoled.h']]],
+  ['neoled_5fctrl_5ffifo_5fmsb_2',['NEOLED_CTRL_FIFO_MSB',['../neorv32__neoled_8h.html#ab99c7fed050816eae0fa0315562e0412a1b6754786094a5b3f839f4c7d4a9bded',1,'neorv32_neoled.h']]],
+  ['neoled_5fctrl_5fprsc_5flsb_3',['NEOLED_CTRL_PRSC_LSB',['../neorv32__neoled_8h.html#ab99c7fed050816eae0fa0315562e0412afbf7ddcea8d03a6dbe62071bc30225db',1,'neorv32_neoled.h']]],
+  ['neoled_5fctrl_5fprsc_5fmsb_4',['NEOLED_CTRL_PRSC_MSB',['../neorv32__neoled_8h.html#ab99c7fed050816eae0fa0315562e0412a67e6989ab6b29657c36f0cc5293448ad',1,'neorv32_neoled.h']]],
+  ['neoled_5fctrl_5ft_5f0h_5flsb_5',['NEOLED_CTRL_T_0H_LSB',['../neorv32__neoled_8h.html#ab99c7fed050816eae0fa0315562e0412a93ed85dbc92120fdb2131a5b955d8d46',1,'neorv32_neoled.h']]],
+  ['neoled_5fctrl_5ft_5f0h_5fmsb_6',['NEOLED_CTRL_T_0H_MSB',['../neorv32__neoled_8h.html#ab99c7fed050816eae0fa0315562e0412a00b5673de0a314b567ca03024d1da08d',1,'neorv32_neoled.h']]],
+  ['neoled_5fctrl_5ft_5f1h_5flsb_7',['NEOLED_CTRL_T_1H_LSB',['../neorv32__neoled_8h.html#ab99c7fed050816eae0fa0315562e0412a3e7cfd957649e9da6bd9ca607f3e902e',1,'neorv32_neoled.h']]],
+  ['neoled_5fctrl_5ft_5f1h_5fmsb_8',['NEOLED_CTRL_T_1H_MSB',['../neorv32__neoled_8h.html#ab99c7fed050816eae0fa0315562e0412acadbd161833117726a1d8413d6324644',1,'neorv32_neoled.h']]],
+  ['neoled_5fctrl_5ft_5ftot_5flsb_9',['NEOLED_CTRL_T_TOT_LSB',['../neorv32__neoled_8h.html#ab99c7fed050816eae0fa0315562e0412a6ee1404652f8478b63f6be116b52b7bb',1,'neorv32_neoled.h']]],
+  ['neoled_5fctrl_5ft_5ftot_5fmsb_10',['NEOLED_CTRL_T_TOT_MSB',['../neorv32__neoled_8h.html#ab99c7fed050816eae0fa0315562e0412aefe3c83a1ce10837b64a4ada0f6960a0',1,'neorv32_neoled.h']]],
+  ['neoled_5fctrl_5ftx_5fbusy_11',['NEOLED_CTRL_TX_BUSY',['../neorv32__neoled_8h.html#ab99c7fed050816eae0fa0315562e0412a55da98b018c7cc32cd1e80087ed9018e',1,'neorv32_neoled.h']]],
+  ['neoled_5fctrl_5ftx_5fempty_12',['NEOLED_CTRL_TX_EMPTY',['../neorv32__neoled_8h.html#ab99c7fed050816eae0fa0315562e0412a23937f0a728da197590e1d383507cb23',1,'neorv32_neoled.h']]],
+  ['neoled_5fctrl_5ftx_5ffull_13',['NEOLED_CTRL_TX_FULL',['../neorv32__neoled_8h.html#ab99c7fed050816eae0fa0315562e0412acfac5269688b2a03794dc17b11f1b983',1,'neorv32_neoled.h']]]
+];
