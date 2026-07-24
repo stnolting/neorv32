ddc281e7d5dbb3ec95db6f009a8189cfda2d84ba
GHA <push@gha>
2026-07-24 20:13:26 +0000

    update 7467212abf02c8035a967652949fb43a19647b2a

---
+++ sw/search/all_b.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['reg_0',['REG',['../structneorv32__cfs__t.html#a9bfa2b6f4938f42fb1ee3beaeb8cc32e',1,'neorv32_cfs_t']]],
+  ['reserved_1',['reserved',['../structneorv32__gpio__t.html#ac2eb24eef93c1544227f6332a03b88db',1,'neorv32_gpio_t::reserved'],['../structneorv32__pwm__t.html#a3615c23a4d86914958087f1d3acb6517',1,'neorv32_pwm_t::reserved']]],
+  ['reset_2',['RESET',['../structneorv32__wdt__t.html#a0a273e8ca2bd92547bf7727ec65a9449',1,'neorv32_wdt_t']]],
+  ['riscv_5finstr_5fi_5ftype_3',['RISCV_INSTR_I_TYPE',['../neorv32__intrinsics_8h.html#a66610638a0aecbb9d049561973b7f77d',1,'neorv32_intrinsics.h']]],
+  ['riscv_5finstr_5fr_5ftype_4',['RISCV_INSTR_R_TYPE',['../neorv32__intrinsics_8h.html#aa83541c1d6b505d61703d811d5580788',1,'neorv32_intrinsics.h']]],
+  ['route_5',['ROUTE',['../structneorv32__slink__t.html#a0b2417a1a587da45909ce2a948f90e22',1,'neorv32_slink_t']]]
+];
