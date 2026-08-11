08f5db2934c0f6e630cc923d500fd328c7dcfb00
stnolting <stnolting@gmail.com>
2026-08-11 23:28:19 +0200

    [sw/lib/uart] minor fixes

--- sw/lib/source/neorv32_uart.c
+++ sw/lib/source/neorv32_uart.c
@@ -74,7 +74,7 @@ void neorv32_uart_setup(neorv32_uart_t *UARTx, uint32_t baudrate, uint32_t irq_m
 #endif
 
   // find baud prescaler (10-bit wide))
-  while (baud_div >= 0x3ffU) {
+  while (baud_div >= 0x400U) {
     if ((prsc_sel == 2) || (prsc_sel == 4))
       baud_div >>= 3;
     else
@@ -86,7 +86,7 @@ void neorv32_uart_setup(neorv32_uart_t *UARTx, uint32_t baudrate, uint32_t irq_m
   tmp |= (uint32_t)(1              & 1U)     << UART_CTRL_EN;
   tmp |= (uint32_t)(prsc_sel       & 7U)     << UART_CTRL_PRSC_LSB;
   tmp |= (uint32_t)((baud_div - 1) & 0x3ffU) << UART_CTRL_BAUD_LSB;
-  tmp |= (uint32_t)(irq_mask       & (0xfu   << UART_CTRL_IRQ_RX_NEMPTY));
+  tmp |= (uint32_t)(irq_mask       & (0xfU   << UART_CTRL_IRQ_RX_NEMPTY));
 
 #ifdef UART0_SIM_MODE
 #warning UART0_SIM_MODE (primary UART) enabled! \
@@ -192,8 +192,13 @@ void neorv32_uart_rtscts_disable(neorv32_uart_t *UARTx) {
  **************************************************************************/
 void neorv32_uart_putc(neorv32_uart_t *UARTx, char c) {
 
+#ifdef UART_SEMIHOSTING
+  (void)UARTx;
+  neorv32_semihosting_putc(c);
+#else
   while ((UARTx->CTRL & (1<<UART_CTRL_TX_NFULL)) == 0); // wait for free space in TX FIFO
   neorv32_uart_tx_put(UARTx, c);
+#endif
 }
 
 
@@ -340,7 +345,7 @@ void neorv32_uart_vprintf(neorv32_uart_t *UARTx, const char *format, va_list arg
 
   while ((c = *format++)) {
     if (c == '%') {
-      c = tolower(*format++);
+      c = tolower((unsigned char)(*format++));
       switch (c) {
 
         case 's': // string
