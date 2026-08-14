4dbb95c57378c10b65b8cbcf8f4b4e4e40c684e3
stnolting <stnolting@gmail.com>
2026-08-14 10:32:59 +0200

    [sim] remove "nvc" note

--- sim/neorv32_tb.vhd
+++ sim/neorv32_tb.vhd
@@ -58,7 +58,7 @@ entity neorv32_tb is
     RISCV_ISA_Xcfu    : boolean                        := true;        -- custom (instr.) functions unit
     CPU_CONSTT_BR_EN  : boolean                        := false;       -- constant-time branches
     CPU_FAST_MUL_EN   : boolean                        := true;        -- use DSPs for M extension's multiplier
-    CPU_FAST_MUL_REGS : natural range 1 to 3           := 1;           -- number of fast multiplier register stages (override with nvc -g)
+    CPU_FAST_MUL_REGS : natural range 1 to 3           := 1;           -- number of fast multiplier register stages
     CPU_FAST_SHIFT_EN : boolean                        := true;        -- use barrel shifter for shift operations
     CPU_RF_ARCH_SEL   : natural range 0 to 3           := 0;           -- register file implementation style select
     IMEM_EN           : boolean                        := true;        -- implement processor-internal instruction memory
