0620e89bc9c873c2ada5feb67921767e86d5e1db
stnolting <stnolting@gmail.com>
2026-08-14 19:49:57 +0200

    [vivado_ip] remove  CPU_FAST_MUL_REG value validation

--- rtl/system_integration/neorv32_vivado_ip.tcl
+++ rtl/system_integration/neorv32_vivado_ip.tcl
@@ -314,8 +314,6 @@ proc setup_ip_gui {} {
     { CPU_FAST_SHIFT_EN {Barrel shifter}         {Use full-parallel shifters instead of of bit-serial shifters} }
     { CPU_RF_ARCH_SEL   {Register file style}    {Select implementation style of CPU register file} }
   }
-  set_property value_validation_range_minimum 1 [ipx::get_user_parameters CPU_FAST_MUL_REGS -of_objects [ipx::current_core]]
-  set_property value_validation_range_maximum 3 [ipx::get_user_parameters CPU_FAST_MUL_REGS -of_objects [ipx::current_core]]
   set_property widget {comboBox} [ipgui::get_guiparamspec -name "CPU_RF_ARCH_SEL" -component [ipx::current_core] ]
   set_property value_validation_type pairs [ipx::get_user_parameters CPU_RF_ARCH_SEL -of_objects [ipx::current_core]]
   set_property value_validation_pairs {{Block RAM} 0 {Distributed RAM} 1 {FFs with reset} 2 {Latches} 3} [ipx::get_user_parameters CPU_RF_ARCH_SEL -of_objects [ipx::current_core]]
