5f0d3360ee1928a8e90251357e959914f5c200c1
GHA <push@gha>
2026-08-22 05:01:34 +0000

    update b3f390870f77b9bdc4ac282c242742b10408dc53

---
+++ sw/search/all_5.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['hours_0',['hours',['../structdate__t.html#a39a23695634a2814d827984f4eea54eb',1,'date_t']]],
+  ['hpmcnt_5fevent_5fci_1',['HPMCNT_EVENT_CI',['../neorv32__csr_8h.html#a5cb925a3e41efd5e767bb39b3ee60cfea5e93e893522c760cb099fd285872de05',1,'neorv32_csr.h']]],
+  ['hpmcnt_5fevent_5fcy_2',['HPMCNT_EVENT_CY',['../neorv32__csr_8h.html#a5cb925a3e41efd5e767bb39b3ee60cfea2b683572525adc02ce0910031cfea914',1,'neorv32_csr.h']]],
+  ['hpmcnt_5fevent_5fdelta_3',['HPMCNT_EVENT_DELTA',['../neorv32__csr_8h.html#a5cb925a3e41efd5e767bb39b3ee60cfeabf130d29c8229bc883770c008cdb25a8',1,'neorv32_csr.h']]],
+  ['hpmcnt_5fevent_5fir_4',['HPMCNT_EVENT_IR',['../neorv32__csr_8h.html#a5cb925a3e41efd5e767bb39b3ee60cfeaab438295e5f00ffe4c2457a54ee9bfc4',1,'neorv32_csr.h']]],
+  ['hpmcnt_5fevent_5fload_5',['HPMCNT_EVENT_LOAD',['../neorv32__csr_8h.html#a5cb925a3e41efd5e767bb39b3ee60cfea1a06f216e1a12afa0b90d6e1218b3c91',1,'neorv32_csr.h']]],
+  ['hpmcnt_5fevent_5fstore_6',['HPMCNT_EVENT_STORE',['../neorv32__csr_8h.html#a5cb925a3e41efd5e767bb39b3ee60cfea6a942852e3cd85616a63addb8e9d76f2',1,'neorv32_csr.h']]],
+  ['hpmcnt_5fevent_5fwait_5falu_7',['HPMCNT_EVENT_WAIT_ALU',['../neorv32__csr_8h.html#a5cb925a3e41efd5e767bb39b3ee60cfeaffbb04d0053ebd079c6cacb4e5e7ed6b',1,'neorv32_csr.h']]],
+  ['hpmcnt_5fevent_5fwait_5fdis_8',['HPMCNT_EVENT_WAIT_DIS',['../neorv32__csr_8h.html#a5cb925a3e41efd5e767bb39b3ee60cfeabcb123b5b150b4413e5fd48475faab27',1,'neorv32_csr.h']]],
+  ['hpmcnt_5fevent_5fwait_5flsu_9',['HPMCNT_EVENT_WAIT_LSU',['../neorv32__csr_8h.html#a5cb925a3e41efd5e767bb39b3ee60cfea67a59934bc66bc7ccf82d24fe8a18a3b',1,'neorv32_csr.h']]]
+];
---
+++ sw/search/variables_9.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['seconds_0',['seconds',['../structdate__t.html#a1071948e7754cb415da9714d59a315bf',1,'date_t']]],
+  ['slice_1',['SLICE',['../structneorv32__gptmr__t.html#a29adfe7931c80845de22b68cc085e9ff',1,'neorv32_gptmr_t']]],
+  ['soc_2',['SOC',['../structneorv32__sysinfo__t.html#ac43abe3b416da3a2895d1d06537a14c3',1,'neorv32_sysinfo_t']]],
+  ['stop_5faddr_3',['STOP_ADDR',['../structneorv32__tracer__t.html#a31fde434661391ebc5b00c8d779ca358',1,'neorv32_tracer_t']]],
+  ['strobe_4',['STROBE',['../structneorv32__neoled__t.html#a76c7f73a97a89d6a4fd0b391f7f35d48',1,'neorv32_neoled_t']]]
+];
