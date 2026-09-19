ca8516369c0837130bbd594322acfde5448391ca
GHA <push@gha>
2026-09-19 12:43:21 +0000

    update da5f595a3528c524eaabf712ec8823cc43375b96

---
+++ sw/search/all_6.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['io_5fbase_5faddress_0',['IO_BASE_ADDRESS',['../neorv32_8h.html#a1eb0cb7fed7e154e15cb4009880a879c',1,'neorv32.h']]],
+  ['irq_1',['IRQ',['../structneorv32__gptmr__t.html#ab6b81b1eeb5227555d5538f54e6f3e67',1,'neorv32_gptmr_t']]],
+  ['irq_5fenable_2',['IRQ_ENABLE',['../structneorv32__gpio__t.html#a666113da2d16d02acf65bb2d0197d99a',1,'neorv32_gpio_t']]],
+  ['irq_5fpending_3',['IRQ_PENDING',['../structneorv32__gpio__t.html#a27a1d5fa04dd4d32065c2ca4ca4f60c5',1,'neorv32_gpio_t']]],
+  ['irq_5fpolarity_4',['IRQ_POLARITY',['../structneorv32__gpio__t.html#af3b50a8ac4416b6020eaa70069fc53b6',1,'neorv32_gpio_t']]],
+  ['irq_5ftype_5',['IRQ_TYPE',['../structneorv32__gpio__t.html#aba631107cf34fc04f0f167f96606c5e0',1,'neorv32_gpio_t']]]
+];
---
+++ sw/search/defines_1.js
@@ -0,0 +1,15 @@
+var searchData=
+[
+  ['dma_5fbswap_0',['DMA_BSWAP',['../neorv32__dma_8h.html#a6c3a68eed2cc81ac700c31ebd75edff0',1,'neorv32_dma.h']]],
+  ['dma_5fdst_5fconst_5fbyte_1',['DMA_DST_CONST_BYTE',['../neorv32__dma_8h.html#abb2f750655e3b8799a798cb1cc22f863',1,'neorv32_dma.h']]],
+  ['dma_5fdst_5fconst_5fword_2',['DMA_DST_CONST_WORD',['../neorv32__dma_8h.html#a1e920c201b09c772b89bf41e11a5f730',1,'neorv32_dma.h']]],
+  ['dma_5fdst_5finc_5fbyte_3',['DMA_DST_INC_BYTE',['../neorv32__dma_8h.html#a31f3ebeb5bd1f6c60ba81d2d66c63fc5',1,'neorv32_dma.h']]],
+  ['dma_5fdst_5finc_5fword_4',['DMA_DST_INC_WORD',['../neorv32__dma_8h.html#a995f79c20eb8edba5cdc2ce87ddf8177',1,'neorv32_dma.h']]],
+  ['dma_5ffirq_5fenable_5',['DMA_FIRQ_ENABLE',['../neorv32_8h.html#ace46f95dec73b33bf2535eb653bf8f65',1,'neorv32.h']]],
+  ['dma_5ffirq_5fpending_6',['DMA_FIRQ_PENDING',['../neorv32_8h.html#a1fa2b25383e6d05cf26f3517091ddbbf',1,'neorv32.h']]],
+  ['dma_5fsrc_5fconst_5fbyte_7',['DMA_SRC_CONST_BYTE',['../neorv32__dma_8h.html#aae260c7bb09e88ce5ea5fd2a8be723a4',1,'neorv32_dma.h']]],
+  ['dma_5fsrc_5fconst_5fword_8',['DMA_SRC_CONST_WORD',['../neorv32__dma_8h.html#ad07c778b0867f2a1dcc2f1de6209d73e',1,'neorv32_dma.h']]],
+  ['dma_5fsrc_5finc_5fbyte_9',['DMA_SRC_INC_BYTE',['../neorv32__dma_8h.html#a5d284d89e545d4f7058cfe49c3735b30',1,'neorv32_dma.h']]],
+  ['dma_5fsrc_5finc_5fword_10',['DMA_SRC_INC_WORD',['../neorv32__dma_8h.html#a960c30f00c824dc0a9cb0cd575bac06e',1,'neorv32_dma.h']]],
+  ['dma_5ftrap_5fcode_11',['DMA_TRAP_CODE',['../neorv32_8h.html#ad2e81443af8a4ce2db3a4e3c1a8fb784',1,'neorv32.h']]]
+];
